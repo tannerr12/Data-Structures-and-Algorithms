e2435f71adba304433164c16ebd535d40ee2047c
tannerr12 <tanner.01@hotmail.com>
2022-09-21 15:53:43 -0400

    Time: 71 ms (6.37%), Space: 13.9 MB (21.19%) - LeetHub

---
+++ 1291-sequential-digits/1291-sequential-digits.py
@@ -0,0 +1,51 @@
+class Solution:
+    def sequentialDigits(self, low: int, high: int) -> List[int]:
+        
+        mn, mx = len(str(low)), len(str(high))
+        
+        res = deque()
+        
+        s = ''
+        for i in range(1,mn +1):
+            if i == 10:
+                        return res
+            s+=str(i)
+        if int(s) > high:
+            return []
+        if int(s) >= low:
+            res.append(s)
+        while True:
+            temp = ''
+            increase = False
+            for char in s:
+                
+                if char != '9':
+                    val = int(char) +1
+                    temp += str(val)
+            
+                else:
+                    increase = True
+                    break
+            
+            if increase:
+                r = len(s)
+                s= ''
+                for i in range(1,r+2):
+                    if i == 10:
+                        return res
+                    s+=str(i)
+                
+                temp = s
+            if int(temp) > high:
+                break
+            s = temp
+            if int(temp) >= low:
+                res.append(temp)
+            
+            
+        while res and int(res[0]) < low:
+            res.popleft()
+        return res
+            
+            
+            
