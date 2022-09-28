b60f7e98afb6c571b7d8a35cb0b4de91bf34b3c0
tannerr12 <tanner.01@hotmail.com>
2022-09-28 13:04:36 -0400

    Time: 4975 ms (18.79%), Space: 60.1 MB (5.72%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.py
+++ 2055-plates-between-candles/2055-plates-between-candles.py
@@ -10,7 +10,7 @@ class Solution:
         memoy = {}
         
         for i in range(len(s)-1,-1,-1):
-            
+            memox[i] = prev
             if check ==False and s[i] == '*':
                 continue
             if check== False and s[i] == '|':
@@ -21,49 +21,27 @@ class Solution:
                 starCount+=1
             
             elif s[i] == '|':
-                if prev == 0:
-                    barMap[i] = starCount
-                    prev = i
-                else:
-                    barMap[i] = starCount
-                    prev = i
-            
-        #print(barMap)
-        
-        for x,y in queries:
-            countx = []
-            county = []
-            cx = x
-            cy = y
-            
-            
-            
-            
-            if cx not in memox:
-                countx.append(cx)
-                while cx < y-1 and cx not in barMap:
-                    cx +=1
-                    countx.append(cx)
-            else:
-                cx = memox[cx]
-            if cy not in memoy:
-                county.append(cy)
-                while cy > x+1 and cy not in barMap:
-                    cy -=1
-                    county.append(cy)
-            else:
-                cy = memoy[cy]
 
+                barMap[i] = starCount
+                prev = i
+            memox[i] = prev
+        for i in range(len(s)):
+
+            if s[i] == '|':
+                prev = i
+            memoy[i] = prev
+        print(memox)
+        print(memoy)
+        for x,y in queries:
+            cx = memox[x]
+            cy = memoy[y]
             
-            if cx in barMap and cy in barMap:
+            if cy >= x and cx <= y and cx in barMap and cy in barMap:
                 if barMap[cx] - barMap[cy] < 0:
                     res.append(0)
                     continue
                 res.append(barMap[cx] - barMap[cy])
-                for val in countx:
-                    memox[val] = cx
-                for val in county:
-                    memoy[val] = cy
+             
             else:
                 res.append(0)
          
