6c1bbf1c0852f6f5f81be88c27c93d342a741081
tannerr12 <tanner.01@hotmail.com>
2022-10-12 23:16:39 -0400

    Time: 138 ms (5.32%), Space: 14.6 MB (20.45%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.py
@@ -2,39 +2,52 @@ class Solution:
     def search(self, nums: List[int], target: int) -> bool:
         
         
-       
-        return target in nums
-        
-        def binSearch():
-            minL = nums[0]
-            maxL = nums[0]
-            maxr = nums[-1]
-            minr = nums[-1]
+            
             l = 0
             r = len(nums) -1
-            pivot = -1
+            if nums[0] == target:
+                return True
             
             while l < r:
                 
                 curr = (l + r) // 2
+
+                if nums[curr] < nums[r]:
+                    r = curr
                 
-                if nums[curr -1] > nums[curr] or (curr != len(nums) -1 and nums[curr + 1] < nums[curr]) or (nums[0] < nums[curr]):
-                    pivot = curr
-                    minr = nums[curr+1] if curr < len(nums)-1 else nums[0]
-                    maxl = nums[curr-1]
+                elif nums[curr] > nums[r]:
                     
+                    l = curr +1
+                else:
+                    while l < r and nums[r-1] == nums[r]:
+                        r -=1
+                    while l< r and nums[l +1] == nums[l]:
+                        l+=1
                 
-                if nums[curr] == target:
+
+     
+            pivot = l
+            l = 0
+            r = len(nums) -1
+      
+            
+            while l <= r:
+                
+                curr = (l + r) // 2
+                curr2  = (curr + pivot) % len(nums)
+
+                if nums[curr2] == target:
                     return True
                 
-                elif target < minL:
-                    r = curr + 1
+                elif target < nums[curr2]:
+                    r = curr - 1
                 
-                elif target > minL:
-                    l = curr - 1
+                else:
+                    l = curr + 1
+            
+            
             
             
             return False
         
         
-        return binSearch()
