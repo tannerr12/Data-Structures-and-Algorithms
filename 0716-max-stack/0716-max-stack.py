241e9e1823184b4bfc27143fae636b660700fb61
tannerr12 <tanner.01@hotmail.com>
2022-10-13 22:01:45 -0400

    Time: 1422 ms (38.93%), Space: 64 MB (26.01%) - LeetHub

--- 0716-max-stack/0716-max-stack.py
+++ 0716-max-stack/0716-max-stack.py
@@ -1,36 +1,74 @@
+class Node:
+    def __init__(self):
+        self.prev = None
+        self.next = None
+        self.val = float('inf')
+        
 class MaxStack:
 
     def __init__(self):
-        self.soft_deleted = set()
-        self.max_heap = []
-        self.recency_stack = []
-        self.next_id = 0
+        self.h = OrderedDict()
+        self.stack = []
+        self.count = 0
+        self.heap = []
+        self.removed = set()
         
-    def push(self, x: int) -> None:
-        heapq.heappush(self.max_heap, (-x, self.next_id))
-        self.recency_stack.append((x, self.next_id))
-        self.next_id -= 1
 
-    def _clean_up(self):
-        while self.recency_stack and self.recency_stack[-1][1] in self.soft_deleted:
-            self.soft_deleted.remove(self.recency_stack.pop()[1])
-        while self.max_heap and self.max_heap[0][1] in self.soft_deleted:
-            self.soft_deleted.remove(heapq.heappop(self.max_heap)[1])
-    
-    def pop(self) -> int:
-        entry_to_return = self.recency_stack.pop()
-        self.soft_deleted.add(entry_to_return[1])
-        self._clean_up()
-        return entry_to_return[0]
+    def push(self, x: int) -> None:
+        self.count +=1
+        #print(self.count)
+        self.h[self.count] = x
+        heapq.heappush(self.heap, (-x,-self.count))
         
+
+    def pop(self) -> int:
+        if len(self.h) == 0:
+            return 0
+
+        val = 0
+
+        c = self.count *1
+        while c not in self.h:
+            c -=1
+        val = self.h[c]
+        del self.h[c]
+        self.removed.add(-c)
+        return val
+    
+
     def top(self) -> int:
-        return self.recency_stack[-1][0]
+        c = self.count
+        while c not in self.h:
+            c-=1
+        
+        
+        return self.h[c]
 
     def peekMax(self) -> int:
-        return -self.max_heap[0][0]
+      
+        val,key = self.heap[0]
+        while self.heap[0][1] in self.removed:
+            val,key = heapq.heappop(self.heap)
         
+        return self.heap[0][0] * -1
+
     def popMax(self) -> int:
-        value, time = heapq.heappop(self.max_heap)
-        self.soft_deleted.add(time)
-        self._clean_up()
-        return value * -1
+        val,key = self.heap[0]
+
+        while self.heap[0][1] in self.removed:
+            val,key = heapq.heappop(self.heap)
+        
+        val, key = heapq.heappop(self.heap)
+      
+        del self.h[key * -1]
+        self.removed.add(key)
+        return val * -1
+
+
+# Your MaxStack object will be instantiated and called as such:
+# obj = MaxStack()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.peekMax()
+# param_5 = obj.popMax()
