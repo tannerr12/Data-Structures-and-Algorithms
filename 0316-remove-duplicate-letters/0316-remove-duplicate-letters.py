b6ad47ef3af1eb9b5cfc676f6fd6b9a62119ab04
tannerr12 <tanner.01@hotmail.com>
2022-10-16 13:18:01 -0400

    Time: 73 ms (36.41%), Space: 13.9 MB (37.85%) - LeetHub

--- 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.py
@@ -5,17 +5,20 @@ class Solution:
         
         #alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
         stack = []
+        seen = set()
         for i in range(len(s)):
             
             #if not stack or ord(nums[i]) > stack[-1] and nums[i] not in stack:
              #   stack.append(nums[i])
             h[s[i]] -=1
-            while stack and ord(s[i]) < ord(stack[-1]) and h[stack[-1]] != 0 and s[i] not in stack:
-                stack.pop()
+            while stack and ord(s[i]) < ord(stack[-1]) and h[stack[-1]] != 0 and s[i] not in seen:
+                v = stack.pop()
+                seen.remove(v)
             
             
-            if s[i] not in stack:
+            if s[i] not in seen:
                 stack.append(s[i])
+                seen.add(s[i])
             
             
             
