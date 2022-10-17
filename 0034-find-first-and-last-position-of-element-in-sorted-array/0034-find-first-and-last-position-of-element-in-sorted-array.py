7a2d9b2eec4df0306011d09493b271c8f0937275
tannerr12 <tanner.01@hotmail.com>
2022-10-17 19:22:03 -0400

    Time: 107 ms (79.86%), Space: 15.4 MB (49.40%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.py
@@ -2,7 +2,27 @@ class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
         
         
+        #left
         
+        l,r = 0,len(nums) -1
+        
+        left,right = -1,-1
+        
+        while l <= r:
+            
+            curr = (l+r) // 2
+            
+            
+            
+            if nums[curr] > target:
+                r = curr -1
+                
+            elif nums[curr] < target:
+                l = curr + 1
+            else:
+                left = curr
+                r = curr -1
+                
         
         l,r = 0,len(nums) -1
         
@@ -13,26 +33,22 @@ class Solution:
             curr = (l+r) // 2
             
             
-            if nums[curr] == target:
-                #gather pointers
-                x,y = curr,curr
-                
-                while x < len(nums)-1 and nums[x+1] == target:
-                    x+=1
-                
-                while y > 0 and nums[y-1] == target:
-                    y-=1
-                
-                return[y,x]
             
-            
-            elif nums[curr] > target:
+            if nums[curr] > target:
                 r = curr -1
                 
-            else:
+            elif nums[curr] < target:
                 l = curr + 1
-                
+            else:
+                right = curr
+                l = curr +1
+                        
         
         
-        return [-1,-1]
+        
+        
+        #right
+        
+        
+        return [left,right]
     
