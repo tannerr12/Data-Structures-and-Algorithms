e58c27757a0168bf1be3316f47b09f76d30512e0
tannerr12 <tanner.01@hotmail.com>
2022-10-19 17:08:47 -0400

    Time: 675 ms (5.23%), Space: 14 MB (25.21%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -3,9 +3,9 @@ class Solution:
         
         res = left
         if left > 10000 and right - left > 10:
-            left = left //10
+            left = left // (10 * len(str(left)) - 1)
         if right > 10000 and right - left > 10:
-            right = right //10
+            right = right //(10 * len(str(right)) - 1)
         
         for i in range(left,right +1):
             
