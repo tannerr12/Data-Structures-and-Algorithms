c3bd853ea0918f88de5ed652931e06877c03532c
tannerr12 <tanner.01@hotmail.com>
2022-10-19 17:11:05 -0400

    Time: 451 ms (5.23%), Space: 13.9 MB (25.21%) - LeetHub

--- 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.py
@@ -2,14 +2,12 @@ class Solution:
     def rangeBitwiseAnd(self, left: int, right: int) -> int:
         
         res = left
-        if left > 10000 and right - left > 10:
+        if left > 1000 and right - left > 10:
             left = left // (10 * len(str(left)) - 1)
-        if right > 10000 and right - left > 10:
+        if right > 1000 and right - left > 10:
             right = right //(10 * len(str(right)) - 1)
         
         for i in range(left,right +1):
-            
-            
             res &= i
             if res == 0:
                 break
