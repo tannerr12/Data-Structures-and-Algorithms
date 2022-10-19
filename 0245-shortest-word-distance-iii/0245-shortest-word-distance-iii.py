08950aa5dc92274bb842ff20acf798ee6481df2c
tannerr12 <tanner.01@hotmail.com>
2022-10-18 20:56:31 -0400

    Time: 2009 ms (27.43%), Space: 37.2 MB (27.85%) - LeetHub

--- 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
+++ 0245-shortest-word-distance-iii/0245-shortest-word-distance-iii.py
@@ -14,39 +14,24 @@ class Solution:
         wl1 = h[word1]
         wl2 = h[word2]
         dist = float('inf')
-        if word1 != word2:
+        
             
-            i,j = 0,0
+        i,j = 0,0
             
-            while i < len(wl1) and j < len(wl2):
-            
-                dist = min(dist, abs(wl1[i] - wl2[j]))
+        while i < len(wl1) and j < len(wl2):
+            if word1 == word2 and i == j:
+                i+=1
+                continue
+            dist = min(dist, abs(wl1[i] - wl2[j]))
                 
-                if wl1[i] > wl2[j]:
-                    j +=1
+            if wl1[i] > wl2[j]:
+                j +=1
                 
-                else:
-                    i +=1
+            else:
+                i +=1
             
             
-        else:
-            
-            i,j = 0,0
-            
-            while i < len(wl1) and j < len(wl2):
-                
-                if i == j:
-                    i+=1
-                    continue
-                
-                dist = min(dist, abs(wl1[i] - wl2[j]))
-                
-                if wl1[i] > wl2[j]:
-                    j +=1
-                
-                else:
-                    i +=1
-            
+
         
         
         
