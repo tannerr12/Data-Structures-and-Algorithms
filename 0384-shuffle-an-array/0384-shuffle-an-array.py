8894e50b63a5ca96ba7b292b5ea7b4ebd941f69f
tannerr12 <tanner.01@hotmail.com>
2022-10-19 20:28:33 -0400

    Time: 436 ms (20.48%), Space: 17.5 MB (97.75%) - LeetHub

--- 0384-shuffle-an-array/0384-shuffle-an-array.py
+++ 0384-shuffle-an-array/0384-shuffle-an-array.py
@@ -3,25 +3,15 @@ import random
 class Solution:
 
     def __init__(self, nums: List[int]):
-        self.arr = nums.copy()
-        self.tempArr = nums.copy()
-        
-        
-        
-        
+        self.arr = nums
 
     def reset(self) -> List[int]:
-        self.arr = self.tempArr.copy()
         return self.arr
 
     def shuffle(self) -> List[int]:
         
-        samp = (random.sample(self.arr,len(self.tempArr)))
-        for i in range(len(self.tempArr)):
-            self.arr[i] = samp[i]
-            
-        
-        return self.arr
+        return random.sample(self.arr,len(self.arr))
+       
             
         
         
