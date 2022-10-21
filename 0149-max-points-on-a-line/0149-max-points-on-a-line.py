cfa5388918010c633b6aae71a04bb7a177e26371
tannerr12 <tanner.01@hotmail.com>
2022-10-20 21:00:06 -0400

    Time: 145 ms (74.03%), Space: 14.1 MB (47.14%) - LeetHub

--- 0149-max-points-on-a-line/0149-max-points-on-a-line.py
+++ 0149-max-points-on-a-line/0149-max-points-on-a-line.py
@@ -1,13 +1,9 @@
 class Solution:
     def maxPoints(self, points: List[List[int]]) -> int:
         
-        
-        adj = collections.defaultdict(list)
-        seen = set()
+
         res = 0
         for i in range(len(points)):
-           # if i in seen:
-            #    continue
             y,x = points[i]
             upDiag = 0
             downDiag = 0
@@ -20,26 +16,20 @@ class Solution:
                     continue
                     
                 yy,xx = points[j]
-                
-                
                 if xx == x:
-                    seen.add(j)
                     right +=1
-                if yy == y:
-                    seen.add(j)
+                if yy == y: 
                     up+=1
                 if xx > x and yy > y:
-                    seen.add(j)
                     upD[(yy-y) / (xx-x)] += 1     
                 if xx < x and yy > y:
-                    seen.add(j)
                     dwD[(yy-y) / (xx-x)] +=1
             
             
             
             if len(upD) > 0:
                 upDiag = max(upD.values())
-            if len(dwD)> 0:
+            if len(dwD) > 0:
                 downDiag = max(dwD.values())
             res = max(res, max(right, up,upDiag,downDiag) +1) 
         
