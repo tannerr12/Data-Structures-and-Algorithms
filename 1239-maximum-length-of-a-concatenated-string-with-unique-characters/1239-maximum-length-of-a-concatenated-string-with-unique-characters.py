35cd633e7978875af312ad85015683d38f38840a
tannerr12 <tanner.01@hotmail.com>
2022-10-25 13:26:11 -0400

    Time: 293 ms (33.33%), Space: 14.1 MB (44.42%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.py
@@ -2,64 +2,49 @@ class Solution:
     def maxLength(self, arr: List[str]) -> int:
         
         
-        res = 0
-        memo = {}
         d = set()
-        res = 0
-        @cache
-        def permu(s,z,y):
-            nonlocal res
-            if z >= len(realArr):
-                return len(s)
+        
+        def permu(i):
+           
+            if i >= len(arr):
+                return len(d)
             
-            res = max(res,len(s))
-            a= 0
-            b = 0
             
-            h = Counter(s)
-            for j in range(z,len(realArr)):
-                
-                if j == y:
-                    continue
-                
-                valid = True
-                
-                for y in realArr[j]:
+            res = 0
+            if checkArr(d, arr[i]):
+                for char in arr[i]:
+                    d.add(char)
                     
-                    if y in h:
-                        valid = False
-                        break
-                
-                if valid:
-                    
-                    a = permu(s + realArr[j],j+1, j) 
-                if realArr[j] not in d:
-                    d.add(realArr[j])
-                    b = permu(realArr[j], 0, j)
-                
-                
-                    
+                res = permu(i+1)
             
+                for char in arr[i]:
+                    d.remove(char)
             
-            return max(a,b)
-        
-        
-        realArr = []
-        for i in range(len(arr)):
-            temp =Counter(arr[i])
-            if max(temp.values()) == 1:
-                realArr.append(arr[i])
+            res = max(res,permu(i+1))
+               
+            
+            return res
         
         
         
-        if len(realArr) == 0:
-            return 0
-        val = permu(realArr[0], 0, 0)
-        return res
-        if val == 0:
-            return len(max(realArr))
-        else:
-            val
+        def checkArr(char,s):
+                
+            tempSet = set()
+            for i in range(len(s)):
+
+                if s[i] in tempSet or s[i] in char:
+                    return False
+                tempSet.add(s[i])
+
+
+            return True
+     
+        
+        
+       
+        return permu(0)
+     
+        
                     
                 
                 
