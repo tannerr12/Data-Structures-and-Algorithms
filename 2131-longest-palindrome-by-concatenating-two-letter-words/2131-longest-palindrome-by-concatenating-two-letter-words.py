11af0babb7d321162fadc57b5a5478f727cadcf1
tannerr12 <tanner.01@hotmail.com>
2022-11-02 20:41:31 -0400

    Time: 4193 ms (7.43%), Space: 38.3 MB (54.38%) - LeetHub

--- 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
+++ 2131-longest-palindrome-by-concatenating-two-letter-words/2131-longest-palindrome-by-concatenating-two-letter-words.py
@@ -2,13 +2,8 @@ class Solution:
     def longestPalindrome(self, words: List[str]) -> int:
         
         h = Counter(words)
-        
-        #print(h)
-        
-       # doubled = defaultdict(int)
-        
-        
-        s1, s2 = '' , ''
+
+        res = 0
         remainder = 0
         
         for i in range(len(words)):
@@ -20,33 +15,21 @@ class Solution:
             if word == back and h[word] >= 2:
                 
                 m = h[word] // 2
-                
-                s1 += word * m
-                h[word] -= m
-                s2 = (back * m) + s2
-                h[back] -= m
+                h[word] -= m * 2
+                res += 2 * (m*2)
                 if h[word] == 1:
                     remainder = 2
 
-            elif word == back and h[word] == 1:
-                           
-                    remainder = 2
+            elif word == back and h[word] == 1:  
+                remainder = 2
             elif back in h and h[back] > 0:
                 
                 m = min(h[word], h[back])
-                
-                s1 += word * m
                 h[word] -= m
-                s2 = (back * m) + s2
-                h[back] -= m 
+                h[back] -= m
+                
+                res += 2 * (m*2)
                 
             
-            
         
-        #for key 
-        
-       # print(doubled)
-        #print(s1 + s2)
-        
-        #remainder = max(doubled.values())
-        return len(s1) + len(s2) + remainder
+        return res + remainder
