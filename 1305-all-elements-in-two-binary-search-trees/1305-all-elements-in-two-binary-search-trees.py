7cb5c73681f471e3a6fe1d67840728a91c8c77dc
tannerr12 <tanner.01@hotmail.com>
2022-12-09 21:42:16 -0500

    Time: 352 ms (89.75%), Space: 23.1 MB (32.09%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.py
@@ -7,16 +7,16 @@
 class Solution:
     def getAllElements(self, root1: TreeNode, root2: TreeNode) -> List[int]:
         
-        h = defaultdict(int)
+        h = []
         mx = float('-inf')
         mn = float('inf')
         def dfs(root):
-            nonlocal mn 
+ 
             if root is None:
                 return 0
             
-            h[root.val] +=1
-            mn = min(mn,root.val)
+            h.append(root.val)
+        
             val = max(root.val,dfs(root.left),dfs(root.right))
             return val
         
@@ -25,12 +25,6 @@ class Solution:
         mx = max(mx, dfs(root1), dfs(root2))
 
         
-        res = []
-        for i in range(mn,mx +1):
-            
-            if i in h:
-                val = h[i]
-                for j in range(val):
-                    res.append(i)
-        
+        res = sorted(h,key=lambda x:x)
         return res
+        
