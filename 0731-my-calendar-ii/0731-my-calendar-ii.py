e19cdc16bfd5a15e9b2c6c5b01bc94d881ffe312
tannerr12 <tanner.01@hotmail.com>
2022-12-12 17:13:40 -0500

    Time: 8265 ms (5.06%), Space: 14.9 MB (25.70%) - LeetHub

--- 0731-my-calendar-ii/0731-my-calendar-ii.py
+++ 0731-my-calendar-ii/0731-my-calendar-ii.py
@@ -1,77 +1,34 @@
-from sortedcontainers import SortedList
 class MyCalendarTwo:
 
     def __init__(self):
-        self.arr = SortedList()
-        self.double = SortedList()
+        self.h = {}
+
     def book(self, start: int, end: int) -> bool:
-        #print(self.double)
-        res = self.binSearchDouble(start,end)
-        if not res:
-            return False
-        #l = self.binSearch(start,end)
+        if start not in self.h:
+            self.h[start] = 1
+        else:
+            self.h[start]+=1
         
-        for i in range(len(self.arr)):
-            s,e = self.arr[i]
-            #double overlap
-            if (start >= s and end <= e):
-                self.double.add([start,end])
+        if end not in self.h:
+            self.h[end] = -1
+        else:
+            self.h[end] -=1
+        s = 0
+        
+        #print(self.h)
+        for key,val in sorted(self.h.items()):
+            
+            s+=val # +1 if we meet an event 
+            
+            if s >= 3:
                 
-            #full overlap
-            elif (start <= s and end >= e):
-                self.double.add([s,e])
-            
-            #end overlap
-            elif (start < s and e > end and s < end):
-                 self.double.add([s,end])
-            
-            #start overlap
-            elif (start > s and start < e and end > e):
-                self.double.add([start,e])
-        
-        #print(self.double)
-        self.arr.add([start,end])
-        return True
-        
-        
-            
-    
-    def binSearchDouble(self,start,end):
-        
-        l= 0
-        r = len(self.double)-1
-        
-        while l<=r:
-            
-            curr = (l+r)//2
-            s, e = self.double[curr]
-            if (start >= s and end <= e) or (start <= s and end >= e) or (start < s and e > end and s < end) or (start > s and start < e and end > e):
+                self.h[start] -=1
+                self.h[end] +=1
                 return False
             
-            elif s < start:
-                l = curr +1
-            else:
-                r = curr -1
             
+        
         return True
-
-        
-    
-    def binSearch(self,start,end):
-        
-        l= 0
-        r = len(self.arr)-1
-        
-        while l<=r:
-            
-            curr = (l+r)//2
-            s, e = self.arr[curr]
-            if start >= s:
-                l = curr +1
-            else:
-                r = curr -1
-            
-        return l
 
 
 # Your MyCalendarTwo object will be instantiated and called as such:
