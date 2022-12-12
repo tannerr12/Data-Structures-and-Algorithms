b6de4c2d1dfa2a752c570e780e816ed7e40657aa
tannerr12 <tanner.01@hotmail.com>
2022-12-12 12:07:23 -0500

    Time: 770 ms (39.60%), Space: 14.8 MB (64.77%) - LeetHub

---
+++ 0729-my-calendar-i/0729-my-calendar-i.py
@@ -0,0 +1,75 @@
+"""
+class segTree:
+    leftmost, rightmost = None,None
+    segTree lChild,rChild
+    s = 0
+    
+    def __init__(self, leftmost,rightmost,a):
+        self.leftmost = leftmost
+        self.rightmost = rightmost
+        
+        if leftmost == rightmost:
+            s = a[leftmost]
+        
+        else:
+            mid = (leftmost + rightmost) //2
+            lChild = segTree(leftmost,mid,a)
+            rChild = segTree(mid + 1, rightmost, a)
+            recalc()
+    
+    
+    def reclac():
+        if leftmost == rightmost:
+            return
+        
+        s = lChild.s + rChild.s
+        
+        
+    def pointUpdate(index, newVal):
+        if (leftmost == rightmost):
+            s = newVal
+            return
+        if index <= lChild.rightmost:
+            lChild.pointUpdate(index,newVal)
+        else:
+            rChild.pointUpdate(index,newVal)
+        
+        recalc()
+        """
+class MyCalendar:
+
+    def __init__(self):
+        self.arr = []
+
+    def book(self, start: int, end: int) -> bool:
+        
+        res, val = self.binarySearch(start,end)
+        if res:
+            self.arr.insert(val, [start,end])
+            return res
+        else:
+            return res
+    
+    
+    def binarySearch(self, start,end):
+        
+        l = 0
+        r = len(self.arr) -1
+        
+        while l <= r:
+            
+            curr = (l+r)//2
+            s,e = self.arr[curr][0], self.arr[curr][1]
+            if (s <= start and end <= e) or (end >= e and start < e) or (start <= s and end > s):
+                return (False, None)
+            elif end < e:
+                l = curr +1
+            else:
+                r = curr -1
+        
+        
+        return (True,l)
+
+# Your MyCalendar object will be instantiated and called as such:
+# obj = MyCalendar()
+# param_1 = obj.book(start,end)
