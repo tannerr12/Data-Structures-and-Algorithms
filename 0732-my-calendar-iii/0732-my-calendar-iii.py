ad40a2a45577f8b4be5df1393e61168139926f8a
tannerr12 <tanner.01@hotmail.com>
2022-12-12 20:56:51 -0500

    Time: 1773 ms (67.41%), Space: 20.7 MB (5.67%) - LeetHub

--- 0732-my-calendar-iii/0732-my-calendar-iii.py
+++ 0732-my-calendar-iii/0732-my-calendar-iii.py
@@ -1,22 +1,59 @@
+class Tree:
+    def __init__(self, low, high, val=0):
+        self.low = low
+        self.high = high
+        self.val = val
+        self.left = None
+        self.right = None
+        
+    def push_down(self): 
+        self.val += 1
+        if self.left: 
+            self.left.push_down()
+        if self.right: 
+            self.right.push_down()
+    def update(self, i, j):
+        if self.low == i and self.high == j:
+            self.val += 1
+            if self.left: 
+                self.left.push_down()
+            if self.right: 
+                self.right.push_down()
+            return self.val
+        
+        
+        mid = (self.low + self.high) // 2
+        if not self.left and not self.right:
+            self.left = Tree(self.low, mid, self.val)
+            self.right = Tree(mid, self.high, self.val)
+        if mid <= i:
+            self.val = max(self.val, self.right.update(i, j))
+        elif mid >= j:
+            self.val = max(self.val, self.left.update(i, j))
+        else:
+            l = self.left.update(i, mid)
+            r = self.right.update(mid, j)
+            self.val = max(self.val, max(l, r))
+        return self.val
+
+
+
 from sortedcontainers import SortedList
 class MyCalendarThree:
 
     def __init__(self):
-        self.arr = SortedList()
+        self.low = 0
+        self.high = 10 ** 9
+        self.tree = Tree(self.low, self.high)
 
-    def book(self, startTime: int, endTime: int) -> int:
-        
-        
-        self.arr.add([startTime,1])
-        self.arr.add([endTime,-1])
-        s = 0
-        mx = 0
-        for time, val in self.arr:    
-            s += val
-            mx = max(mx,s)
-        
-        return mx
-    
+    def book(self, x: int, y: int) -> int:
+        return self.tree.update(x, y)
+
+
+
+
+
+
         
             
 
