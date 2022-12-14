3e62339c635d9ec03ae843c6a008446fe115eb7e
tannerr12 <tanner.01@hotmail.com>
2022-12-14 15:25:06 -0500

    Time: 1760 ms (92.05%), Space: 43.4 MB (59.24%) - LeetHub

--- 2050-parallel-courses-iii/2050-parallel-courses-iii.py
+++ 2050-parallel-courses-iii/2050-parallel-courses-iii.py
@@ -10,12 +10,8 @@ class Solution:
         for x,y in relations:
             
             adj[x].append(y)
-            
             indegree[y]+=1
-            
-        
-        
-    
+
         q = []
         seen = set()
         for i in range(1, n +1):
@@ -24,29 +20,25 @@ class Solution:
                 heapq.heappush(q,(time[i-1], i))
                 seen.add(i)
         
-        res = 0
+        
         while q:
+                
+            t, node = heapq.heappop(q)
+            #maxTime[node-1] = max(maxTime[node-1], t)
             
-            m = 0
-            for i in range(len(q)):
                 
-                t, node = heapq.heappop(q)
-                maxTime[node-1] = max(maxTime[node-1], t)
-                
-                
-                for x in adj[node]:
+            for x in adj[node]:
                     
-                    indegree[x] -=1
-                    maxTime[x-1] = max(maxTime[x-1], time[x-1] + maxTime[node-1])
-                    if indegree[x] == 0 and x not in seen:
-                        heapq.heappush(q, (maxTime[x-1], x))
-                        seen.add(x)
+                indegree[x] -=1
+                maxTime[x-1] = max(maxTime[x-1], time[x-1] + maxTime[node-1])
+                if indegree[x] == 0 and x not in seen:
+                    heapq.heappush(q, (maxTime[x-1], x))
+                    seen.add(x)
             
-            #res += m
+
         
         return max(maxTime)
         
-        #return res
                     
                 
         
