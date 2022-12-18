950353db06374e89808aeec3121772e0b363ab28
tannerr12 <tanner.01@hotmail.com>
2022-12-17 20:11:54 -0500

    Time: 9887 ms (44.55%), Space: 18.4 MB (36.08%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.py
+++ 0312-burst-balloons/0312-burst-balloons.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
-        
+        """ 
         nums  = [1] + nums + [1]
         
         @cache
@@ -21,3 +21,21 @@ class Solution:
         
         
         return dfs(1,len(nums) -2)
+        """
+        
+       
+        
+        
+        
+        nums = [1] + nums + [1]
+        n = len(nums)
+        dp= [[0 for j in range(len(nums))] for i in range(len(nums))]   
+        for left in range(n - 2, -1,-1):
+            for right in range(left + 2, n):
+                
+                for k in range(left+1,right):
+                    dp[left][right] = max(dp[left][right], dp[left][k] + dp[k][right] + (nums[left] * nums[k] * nums[right]))
+                
+        
+        print(dp)
+        return dp[0][-1]
