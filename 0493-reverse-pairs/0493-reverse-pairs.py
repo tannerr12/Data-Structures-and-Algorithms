5e6fa1bc2e1bc045ed231da5b4571b5ad1668c46
tannerr12 <tanner.01@hotmail.com>
2022-12-20 17:43:41 -0500

    Time: 2359 ms (80.61%), Space: 22.7 MB (24.04%) - LeetHub

--- 0493-reverse-pairs/0493-reverse-pairs.py
+++ 0493-reverse-pairs/0493-reverse-pairs.py
@@ -1,16 +1,58 @@
 from sortedcontainers import SortedList
 class Solution:
     def reversePairs(self, nums: List[int]) -> int:
-        #2,4,4,6,2
-        #1,2,2,3,1
-        l = SortedList(key =lambda x : -x)
-        res = 0
         
-        for i in range(len(nums)):
+        
+        
+        def merge(nums):
             
-            res+= l.bisect_left(nums[i]*2)
+            if len(nums) <= 1:
+                return nums
             
-            l.add(nums[i])
+            left = merge(nums[:len(nums) //2])
+            right = merge(nums[len(nums) //2:])
+            
+            
+            return partition(left,right)
         
         
+        
+        
+        res = 0 
+        def partition(left,right):
+            nonlocal res
+            l,r = 0,0
+            
+            i,j = 0,0
+            arr = []
+            
+            
+            for i in range(len(left)):
+                
+                while j < len(right) and left[i] > right[j] * 2:
+                    j+=1
+      
+                res += j        
+        
+            while l < len(left) and r < len(right):
+                
+
+                if left[l] <= right[r]:
+                    arr.append(left[l])
+                    l+=1
+                
+                else:
+                    arr.append(right[r])
+                    r+=1
+            
+                
+                
+            arr.extend(right[r:])
+            arr.extend(left[l:])
+                
+            return arr
+        
+        
+        merge(nums)
         return res
+                    
