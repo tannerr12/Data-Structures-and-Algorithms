0b4519872019a082d0df4f1855194a0c6cfe012a
tannerr12 <tanner.01@hotmail.com>
2022-12-21 01:22:35 -0500

    Time: 797 ms (77.41%), Space: 19.2 MB (99.44%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -0,0 +1,51 @@
+class Solution:
+    def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        
+        
+        npossible= defaultdict(set)
+        adj = defaultdict(list)
+        seen = set()
+        colordct = {}
+        for x,y in dislikes:
+            npossible[x].add(y)
+            npossible[y].add(x)
+            
+            adj[x].append(y)
+            adj[y].append(x)
+            
+            
+        
+        
+        
+       # print(npossible)
+        
+        
+        seen = set()
+        for i in range(1,n +1):
+            if i in seen:
+                continue
+            
+            q = deque()
+            q.append(i)
+            colordct[i] = False
+            while q:
+
+                for i in range(len(q)):
+                    node = q.popleft()
+                    
+
+                    for x in adj[node]:
+                    
+                        if x in colordct:
+                            if colordct[x] ==colordct[node]:
+                                return False
+                        else:
+                            colordct[x] = not colordct[node]
+                            
+                            q.append(x)
+                            seen.add(x)
+
+        
+                
+        return True
+                    
