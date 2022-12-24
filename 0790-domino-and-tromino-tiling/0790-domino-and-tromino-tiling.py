6fc791d94fbcf9d7ea32bb5417219521c7546587
tannerr12 <tanner.01@hotmail.com>
2022-12-24 16:17:51 -0500

    Time: 69 ms (50.08%), Space: 18.6 MB (5.01%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.py
@@ -1,20 +1,55 @@
 class Solution:
     def numTilings(self, n: int) -> int:
         
-        mod = 10 ** 9 + 7
         
         @cache
-        def p(n):
-            if n == 2:
+        def dfs(i,state1,state2):
+            
+            if i >= n:
                 return 1
-            return (p(n -1) + f(n - 2)) 
+            
+            state3, state4 = True, True
+            if i == n-1:
+                state3 = False
+                state4 = False
+            
+            res = 0
+            if state1 and state2:
+                #vertical
+                res += dfs(i+1,True,True)
+                if state3:
+                    #L shape top right
+                    res += dfs(i+1, False, True)
+                if state4:
+                    #L shape bottom right
+                    res += dfs(i+1, True, False)
+                if state3 and state4:
+                    res += dfs(i+1, False,False)
+            
+            if state1 and not state2:
+                if state3:
+                    #flat top
+                    res += dfs(i+1, False,True)
+                if state3 and state4:
+                    #L shape top left
+                    res += dfs(i+1,False,False)
+            
+            if not state1 and state2:
+                if state4:
+                    #flat bottom
+                    res += dfs(i+1,True,False)
+                if state3 and state4:
+                    # l shape bottom right
+                    res += dfs(i+1, False,False)
+            
+            
+            if not state1 and not state2:
+                res += dfs(i+1, state3, state4)
+            
+            
+            
+            return res
         
         
-        @cache
-        def f(n):
-            if n<=2:
-                return n
-            return (f(n-1) + f(n-2) + 2 * p(n-1))
-        
-        
-        return f(n) % mod
+        res = dfs(0,True, True)
+        return res % (10 ** 9 + 7)
