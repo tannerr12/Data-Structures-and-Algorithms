3b71f39a4bfa2d72be5308bb931e48b31c780527
tannerr12 <tanner.01@hotmail.com>
2022-12-30 21:42:41 -0500

    Time: 89 ms (69.37%), Space: 25.8 MB (5.47%) - LeetHub

---
+++ 0980-unique-paths-iii/0980-unique-paths-iii.py
@@ -0,0 +1,42 @@
+class Solution:
+    def uniquePathsIII(self, grid: List[List[int]]) -> int:
+        
+        countzero = 0
+        start = [0, 0]
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                
+                if grid[r][c] == 0:
+                    countzero+=1
+                elif grid[r][c] == 1:
+                    start = [r,c]
+                    
+        #memo = {}
+        @cache
+        def dfs(r,c,count,s):
+            
+            if r >= len(grid) or r < 0 or c >= len(grid[0]) or c < 0 or grid[r][c] == -1:
+                return 0
+            
+            if grid[r][c] == 2 and count == -1:
+                return 1
+            elif grid[r][c] == 2 and count != -1:
+                return 0
+            
+            #4 directions
+            
+            res = 0
+            v = grid[r][c]
+            grid[r][c] = -1
+            res += dfs(r+1,c,count -1,s + str(r) + ', ' + str(c))
+            res += dfs(r-1,c,count -1,s + str(r) + ', ' + str(c))
+            res += dfs(r,c+1,count -1,s + str(r) + ', ' + str(c))
+            res += dfs(r,c -1, count -1,s + str(r) + ', ' + str(c))
+            grid[r][c] = v
+            
+            return res
+        
+        
+        
+        
+        return dfs(start[0], start[1], countzero, '')
