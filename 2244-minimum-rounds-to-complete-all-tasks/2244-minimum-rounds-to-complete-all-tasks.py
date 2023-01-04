bb8b7327a5c45bd555c1f1fa02da843e9f13eac4
tannerr12 <tanner.01@hotmail.com>
2023-01-03 21:16:13 -0500

    Time: 2427 ms (36.73%), Space: 28.3 MB (70.66%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,8 +1,7 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
-        #tasks.sort()
-        h = Counter(tasks)
         
+        h = Counter(tasks)
         res = 0
         for key,val in h.items():
             if val < 2:
@@ -12,17 +11,7 @@ class Solution:
             if x % 1 == 0:
                 res += int(x)
             else:
-                l = 0
-                while val > 0:
-                    if val % 3 != 0:
-                        val -=2
-                        l +=1
-                    
-                    else:
-                        l+= val // 3
-                        break
-                res +=l
-        
+                res += val // 3 + 1
         return res
                         
                 
