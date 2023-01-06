b0ee63b517a550d48bfe65956af35195b35a6ff4
tannerr12 <tanner.01@hotmail.com>
2023-01-05 22:47:54 -0500

    Time: 1832 ms (52.76%), Space: 14.6 MB (68.10%) - LeetHub

--- 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
+++ 2370-longest-ideal-subsequence/2370-longest-ideal-subsequence.py
@@ -7,9 +7,8 @@ class Solution:
             
             v = ord(c) - ord('a')
             
-            for i in range(max(0,v - k), min(26, v + k +1)):
-                dp[v] = max(dp[i], dp[v])
+            dp[v] = max(dp[max(0,v - k) : min(26, v + k +1)]) + 1
             
-            dp[v] +=1
+            
         
         return max(dp)
