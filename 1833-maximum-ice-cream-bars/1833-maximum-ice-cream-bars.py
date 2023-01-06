ae788306316663103663ac01c01607f317522217
tannerr12 <tanner.01@hotmail.com>
2023-01-05 20:52:42 -0500

    Time: 907 ms (59.95%), Space: 29.9 MB (17.54%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.py
@@ -1,6 +1,6 @@
 class Solution:
     def maxIceCream(self, costs: List[int], coins: int) -> int:
-        
+        """ 
         costs.sort()
         res = 0
         for i in range(len(costs)):
@@ -12,3 +12,24 @@ class Solution:
             res +=1
         
         return res
+        """
+        
+        def Solution2(coins):
+            
+            h = Counter(costs)
+            
+            res = 0
+            for i in range(1,10**5 +1):
+                if i > coins:
+                    break
+                if i in h:
+                    
+                    v = min(coins // i, h[i])
+                    res += v
+                    coins -= v * i
+            
+            
+            return res
+        
+        return Solution2(coins)
+            
