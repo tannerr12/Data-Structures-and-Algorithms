67bcada783d8bf405a850f050707e3f2a855ffcd
tannerr12 <tanner.01@hotmail.com>
2023-01-11 17:58:48 -0500

    Time: 1496 ms (48.96%), Space: 30.5 MB (19.01%) - LeetHub

---
+++ 2017-grid-game/2017-grid-game.py
@@ -0,0 +1,31 @@
+class Solution:
+    def gridGame(self, grid: List[List[int]]) -> int:
+        
+        
+        prefixL = [grid[1][0]]
+        postfixU = [grid[0][-1]]
+        for i in range(1,len(grid[0])):
+            postfixU.append(postfixU[-1] + grid[0][-(i +1)])
+            prefixL.append(prefixL[-1] + grid[1][i])
+        
+        
+        #print(prefixL)
+        #print(postfixU)
+        
+        player1 = 0
+        player2 = 0
+        gap = 0
+        res = float('inf')
+        for i in range(len(grid[0])):
+            v = grid[0][i] + grid[1][i] + (prefixL[-1] - prefixL[i]) + (postfixU[-1] - postfixU[-(i + 1)])
+            v2 = max(postfixU[-(i + 1)] - grid[0][i], prefixL[i] - grid[1][i])
+            if v > v2 and gap <= abs(v-v2):
+                res = min(res, v2)
+
+                
+        
+        
+        #print(player1)
+        #print(player2)
+        
+        return res
