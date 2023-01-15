a505dfa185df86fcb92f01595b6cf34ec1436e2d
tannerr12 <tanner.01@hotmail.com>
2023-01-15 00:43:33 -0500

    Time: 743 ms (100.00%), Space: 31.9 MB (33.33%) - LeetHub

--- 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.py
+++ 2537-count-the-number-of-good-subarrays/2537-count-the-number-of-good-subarrays.py
@@ -4,33 +4,22 @@ class Solution:
         
         h = defaultdict(int)
         
-        stack = []
         l = 0
         pairs = 0
         res = 0
         
         for i in range(len(nums)):
             
-            
-            h[nums[i]] +=1
-            #stack.append(nums[i])            
+            h[nums[i]] +=1            
             if h[nums[i]] >= 2:
                 pairs += h[nums[i]] - 1
-            #if pairs >= k:
-            #    res += len(nums) - (i + 1) 
-                
+
             
-            while pairs >= k:
-                if h[nums[l]] > 2:
-                    pairs -= h[nums[l]] - 1
-                elif h[nums[l]] == 2:
-                    pairs -=1
+            while pairs >= k: 
                 h[nums[l]] -=1
+                pairs -= h[nums[l]]
                 l+=1    
                 res += 1 + len(nums) - (i + 1) 
                 
            
-            
-            
-                
         return res
