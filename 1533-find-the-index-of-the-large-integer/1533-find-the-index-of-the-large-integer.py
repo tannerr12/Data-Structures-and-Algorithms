a77f6925c9300b99984c35a248172eb7e9b1836b
tannerr12 <tanner.01@hotmail.com>
2023-01-15 23:39:31 -0500

    Time: 249 ms (97.85%), Space: 52.6 MB (97.85%) - LeetHub

---
+++ 1533-find-the-index-of-the-large-integer/1533-find-the-index-of-the-large-integer.py
@@ -0,0 +1,42 @@
+# """
+# This is ArrayReader's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class ArrayReader(object):
+#	 # Compares the sum of arr[l..r] with the sum of arr[x..y]
+#	 # return 1 if sum(arr[l..r]) > sum(arr[x..y])
+#	 # return 0 if sum(arr[l..r]) == sum(arr[x..y])
+#	 # return -1 if sum(arr[l..r]) < sum(arr[x..y])
+#    def compareSub(self, l: int, r: int, x: int, y: int) -> int:
+#
+#	 # Returns the length of the array
+#    def length(self) -> int:
+#
+
+
+class Solution:
+    def getIndex(self, reader: 'ArrayReader') -> int:
+        
+        
+        l,r = 0, reader.length() -1
+        
+        while l < r:
+            
+            mid = (l + r) // 2
+            #if mid == l or mid == r:
+            #    return mid
+            if (r - l +1) % 2 == 0:
+                val = reader.compareSub(l, mid, mid+1, r)
+            else:
+                val = reader.compareSub(l, mid, mid, r)
+            
+            if val == 0:
+                return mid
+            if val == 1:
+                r = mid
+            
+            else:
+                l = mid +1
+        
+        return l
+        
