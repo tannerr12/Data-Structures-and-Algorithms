5f90f1eeed218f5b57a11713cda5656ae7cb3a7b
tannerr12 <tanner.01@hotmail.com>
2023-01-17 21:02:41 -0500

    Time: 115 ms (81.02%), Space: 14.2 MB (56.20%) - LeetHub

--- 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
+++ 1671-minimum-number-of-removals-to-make-mountain-array/1671-minimum-number-of-removals-to-make-mountain-array.py
@@ -2,14 +2,19 @@ class Solution:
     def minimumMountainRemovals(self, nums: List[int]) -> int:
         def scan(arr):
             
-            dp = [1] * len(arr)
-            for i in range(1, len(arr)):
-                for j in range(i):
-                    if arr[i] > arr[j]:
-                        dp[i] = max(dp[i], dp[j] + 1)
+            dp = []
+            ls = []
+            for i in range(len(arr)):
+                
+                idx = bisect_left(ls, arr[i])
+                
+                if len(ls) > idx:
+                    ls[idx] = arr[i]
+                else:
+                    ls.append(arr[i])
+                
+                dp.append(len(ls))
             
-            
-        
             return dp
         
 
