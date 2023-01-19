2233d17e622c9f9d01d93237e341c84dc18f94c7
tannerr12 <tanner.01@hotmail.com>
2023-01-19 16:53:01 -0500

    Time: 622 ms (6.13%), Space: 18.9 MB (5.19%) - LeetHub

---
+++ 2310-sum-of-numbers-with-units-digit-k/2310-sum-of-numbers-with-units-digit-k.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumNumbers(self, num: int, k: int) -> int:
+        
+        @cache
+        def dfs(val):
+            
+            if val == 0:
+                return 0
+            
+            res =float('inf')
+            for i in range(k,val+1,10):
+                if val >= i:
+                    res = min(res,dfs(val - i) +1)
+
+            
+            return res
+        
+        
+        if k == 0:
+            k = 10
+        res = dfs(num)
+        
+        return res if res != float('inf') else -1
