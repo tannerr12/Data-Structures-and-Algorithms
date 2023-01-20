699bcfd2a4f4e94777e760f82bf9e1080692bc12
tannerr12 <tanner.01@hotmail.com>
2023-01-19 19:23:31 -0500

    Time: 2987 ms (17.88%), Space: 13.9 MB (76.49%) - LeetHub

--- 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
+++ 2305-fair-distribution-of-cookies/2305-fair-distribution-of-cookies.py
@@ -1,36 +1,23 @@
 class Solution:
     def distributeCookies(self, cookies: List[int], k: int) -> int:
         
-        dp = [0] * (k +1)
-        
-        dp[0] = cookies[0]
-        
-        
-        
-        memo = {}
-        
+        dp = [0] * k
         
         def backtrack(i):
             
             if i >= len(cookies):
+                #mn = min(dp)
+                mx = max(dp)
                 
-                return max(dp)
-
-            if (i,str(dp)) in memo:
-                return memo[(i,str(dp))]
+                return mx
             
             res = float('inf')
             for kid in range(k):
-               
                 dp[kid] += cookies[i]
                 res = min(res,backtrack(i+1))
                 dp[kid] -= cookies[i]
-               
-            memo[(i,str(dp))] = res
-            return res
-        
-        
-        return backtrack(1)
                 
             
-                
+            return res
+        dp[0] += cookies[0]
+        return backtrack(1)
