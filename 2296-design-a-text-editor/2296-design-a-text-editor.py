db2d28767e37f8e60126d43ee92b2f505354d3f6
tannerr12 <tanner.01@hotmail.com>
2023-01-20 19:00:53 -0500

    Time: 1840 ms (52.86%), Space: 27.9 MB (83.65%) - LeetHub

--- 2296-design-a-text-editor/2296-design-a-text-editor.py
+++ 2296-design-a-text-editor/2296-design-a-text-editor.py
@@ -11,42 +11,25 @@ class TextEditor:
     def deleteText(self, k: int) -> int:
 
         count = min(self.cursor,k)
-        start = self.cursor - k
-        if start < 0:
-            self.text = self.text[self.cursor:]
-            self.cursor = 0
-        else:    
+        start = max(self.cursor - k,0)
+
             
-            self.text = self.text[:self.cursor-k] + self.text[self.cursor:]
-            self.cursor = start
+        self.text = self.text[:start] + self.text[self.cursor:]
+        self.cursor = start
 
         return count
 
     def cursorLeft(self, k: int) -> str:
-
-        count = max(self.cursor - k, 0)
-        self.cursor = count
-        if self.cursor == 0:
-            return ""
+        self.cursor = max(self.cursor - k, 0)
         mnCount = max(0,self.cursor-10)
-
         return self.text[mnCount:self.cursor]
 
         
 
     def cursorRight(self, k: int) -> str:
 
-        count = min(self.cursor + k, len(self.text))
-        self.cursor = count
-        if self.cursor == len(self.text):
-          
-            if len(self.text) >= 10:
-                return self.text[len(self.text) -10:]
-            else:
-                return self.text
-        
+        self.cursor = min(self.cursor + k, len(self.text))
         mnCount = max(0,self.cursor-10)
-    
         return self.text[mnCount:self.cursor]
 
 
