9b8821c6f1ea794d610846aa4ed031e761259f52
tannerr12 <tanner.01@hotmail.com>
2023-01-30 21:31:48 -0500

    Time: 4224 ms (15.25%), Space: 29.1 MB (5.83%) - LeetHub

---
+++ 2233-maximum-product-after-k-increments/2233-maximum-product-after-k-increments.py
@@ -0,0 +1,60 @@
+class Solution:
+    def maximumProduct(self, nums: List[int], k: int) -> int:
+        MOD = 10 **9 + 7
+        res = 1
+        
+        if len(nums) == 1:
+            return nums[0] + k
+        
+        c = Counter(nums)
+        
+        nums = []
+        
+        for key,val in c.items():
+            
+            heappush(nums, [key,val])
+        
+        
+        while k and nums:
+            
+            val,mult = heappop(nums)
+            pval = val
+            if nums:
+                dist = (nums[0][0] - val) * mult
+                
+                if k >= dist:
+                    v2,m2 = heappop(nums)
+                    m2 += mult
+                    heappush(nums,[v2,m2])
+                    k-= dist
+                else:
+                    diff = dist - k
+                    level = k // mult
+                    val += level
+                    
+                    right = k - (level * mult)
+                    heappush(nums,[val+1,right])
+                    heappush(nums,[val,mult - right])
+                    k=0
+                    
+            else:
+                   
+                    level = k // mult
+                    val += level
+                    
+                    right = k - (level * mult)
+                    heappush(nums,[val+1,right])
+                    heappush(nums,[val,mult - right])
+                    k=0
+                    
+            
+        
+        
+        while nums:
+            
+            val,mult = heappop(nums)
+            res *= (val ** mult) % MOD
+            
+        
+        return res % MOD
+            
