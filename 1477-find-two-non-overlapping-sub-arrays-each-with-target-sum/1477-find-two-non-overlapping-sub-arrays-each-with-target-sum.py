ea7ddd0e4647915838345c88ed5356b25da9972b
tannerr12 <tanner.01@hotmail.com>
2023-02-05 23:06:26 -0500

    Time: 977 ms (64.74%), Space: 31.7 MB (47.98%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -1,26 +1,29 @@
 class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
-        
-        l = 0
+        n = len(arr)
+        l = n-1
         
         curr = 0
         ls = []
-        
-        for i in range(len(arr)):
+        prefix = [0] * len(arr)
+        minSeen = float('inf')
+        for i in range(len(arr)-1,-1,-1):
             
             curr += arr[i]
-            
             while curr > target:
                 curr -= arr[l]
-                l +=1
+                l -=1
             
             
             if curr == target:
-                ls.append([l, i - l +1])
-                
+                ls.append([i, l- i +1])
+                minSeen = min(minSeen, ls[-1][1])
+            
+            prefix[i] = minSeen if minSeen != float('inf') else 0
+            
         
         
-        
+        """
         prefix = []
         p = ls.copy()
         for i in range(len(arr) -1, -1, -1):
@@ -42,11 +45,11 @@ class Solution:
                     prefix.append(0)
         
         
-        prefix.reverse()
         
+        """
+      #  print(ls)
+       # print(prefix)
         res = float('inf')
-        #print(ls)
-        #print(prefix)
         for x,y in ls:
             if x + y >= len(arr) or prefix[x + y] == 0:
                 continue
