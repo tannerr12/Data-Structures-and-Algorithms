60061a388159a00adad7c15300d4460251f1f016
tannerr12 <tanner.01@hotmail.com>
2023-02-05 23:13:32 -0500

    Time: 978 ms (64.74%), Space: 27.4 MB (99.42%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -1,9 +1,8 @@
 class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
         n = len(arr)
-        l = n-1
+        r = n-1
         curr = 0
-        ls = []
         prefix = [0] * len(arr)
         minSeen = float('inf')
         res = float('inf')
@@ -11,14 +10,13 @@ class Solution:
             
             curr += arr[i]
             while curr > target:
-                curr -= arr[l]
-                l -=1
+                curr -= arr[r]
+                r -=1
             
             if curr == target:
-                ls.append([i, l- i +1])
-                if l + 1 < len(arr) and prefix and prefix[l+1] != 0:
-                    res = min(res, l-i+1 + prefix[l+1])
-                minSeen = min(minSeen, ls[-1][1])
+                if r + 1 < len(arr) and prefix and prefix[r+1] != 0:
+                    res = min(res, r-i+1 + prefix[r+1])
+                minSeen = min(minSeen, r-i+1)
             
             prefix[i] = minSeen if minSeen != float('inf') else 0
             
