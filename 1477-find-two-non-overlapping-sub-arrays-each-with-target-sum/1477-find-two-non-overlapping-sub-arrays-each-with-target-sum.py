5c0deecb1237cc3648172ae2e9ec183576b17af6
tannerr12 <tanner.01@hotmail.com>
2023-02-05 23:12:23 -0500

    Time: 1047 ms (48.56%), Space: 33.1 MB (46.82%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.py
@@ -2,11 +2,11 @@ class Solution:
     def minSumOfLengths(self, arr: List[int], target: int) -> int:
         n = len(arr)
         l = n-1
-        
         curr = 0
         ls = []
         prefix = [0] * len(arr)
         minSeen = float('inf')
+        res = float('inf')
         for i in range(len(arr)-1,-1,-1):
             
             curr += arr[i]
@@ -14,46 +14,14 @@ class Solution:
                 curr -= arr[l]
                 l -=1
             
-            
             if curr == target:
                 ls.append([i, l- i +1])
+                if l + 1 < len(arr) and prefix and prefix[l+1] != 0:
+                    res = min(res, l-i+1 + prefix[l+1])
                 minSeen = min(minSeen, ls[-1][1])
             
             prefix[i] = minSeen if minSeen != float('inf') else 0
             
         
         
-        """
-        prefix = []
-        p = ls.copy()
-        for i in range(len(arr) -1, -1, -1):
-            if p and i == p[-1][0]:
-                idx, val = p.pop()
-                if not prefix:
-                    prefix.append(val)
-                else:
-                    if prefix[-1] != 0:
-                        prefix.append(min(val, prefix[-1]))
-                    else:
-                        prefix.append(val)
-                
-            else:
-                
-                if prefix:
-                    prefix.append(prefix[-1])
-                else:
-                    prefix.append(0)
-        
-        
-        
-        """
-      #  print(ls)
-       # print(prefix)
-        res = float('inf')
-        for x,y in ls:
-            if x + y >= len(arr) or prefix[x + y] == 0:
-                continue
-            res = min(res, y + prefix[x + y])
-        
         return res if res != float('inf') else -1
-            
