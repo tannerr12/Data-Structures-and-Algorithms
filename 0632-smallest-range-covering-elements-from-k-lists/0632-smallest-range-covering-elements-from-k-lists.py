443a5b94c243f0f92985629e64b6eebc982a6f1a
tannerr12 <tanner.01@hotmail.com>
2023-02-06 20:26:22 -0500

    Time: 243 ms (65.22%), Space: 20.7 MB (21.89%) - LeetHub

---
+++ 0632-smallest-range-covering-elements-from-k-lists/0632-smallest-range-covering-elements-from-k-lists.py
@@ -0,0 +1,49 @@
+class Solution:
+    def smallestRange(self, nums: List[List[int]]) -> List[int]:
+        
+        n = len(nums)
+        arr = []
+        tags = {}
+        for i in range(len(nums)):
+            for num in nums[i]:
+                arr.append([num, i])
+                
+        
+        
+        
+        arr.sort()
+        
+        #print(arr)
+        l = 0
+        ans = []
+        res = float('inf')
+        for i in range(len(arr)):
+            val,tag = arr[i]
+            
+            if tag not in tags:
+                tags[tag] = 1
+            else:
+                tags[tag] += 1
+            
+            
+            while len(tags) == n:
+                
+                if len(tags) == n:
+                    if arr[i][0] - arr[l][0] < res:
+                        res = arr[i][0] - arr[l][0]
+                        ans = [arr[l][0],arr[i][0]]
+
+                v,t = arr[l]
+                tags[t] -=1
+                if tags[t] == 0:
+                    del tags[t]
+                l+=1
+            
+            
+            if len(tags) == n:
+                if arr[i][0] - arr[l][0] < res:
+                    res = arr[i][0] - arr[l][0]
+                    ans = [arr[l][0],arr[i][0]]
+        
+        
+        return ans
