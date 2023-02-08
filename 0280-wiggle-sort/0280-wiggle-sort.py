2cc9d11b04b5eb0bcc161b4db9ca4612c69972fb
tannerr12 <tanner.01@hotmail.com>
2023-02-07 21:24:22 -0500

    Time: 96 ms (73.14%), Space: 15.2 MB (31.93%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.py
+++ 0280-wiggle-sort/0280-wiggle-sort.py
@@ -5,10 +5,6 @@ class Solution:
         """
         nums.sort()
         bit = 0
-        for i in range(1, len(nums) -1):
-            if i % 3 == 0:
-                bit ^= 1
-                
-            if i % 2:
-                nums[i], nums[i+1] = nums[i+1],nums[i]
+        for i in range(1, len(nums) -1,2):
+            nums[i], nums[i+1] = nums[i+1],nums[i]
         
