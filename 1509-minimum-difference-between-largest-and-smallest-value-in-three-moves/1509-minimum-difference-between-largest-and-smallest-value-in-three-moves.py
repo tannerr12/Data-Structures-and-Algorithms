fbba77ef555ce5db5e8ff000ea11b1292c976e46
tannerr12 <tanner.01@hotmail.com>
2023-02-07 19:06:22 -0500

    Time: 359 ms (78.01%), Space: 24.7 MB (92.44%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.py
@@ -7,19 +7,10 @@ class Solution:
         nums.sort()
         res = float('inf')
         
-        for i in range(len(nums)):
-        
-            
-            #take 3 from left
-            if i >= 3:
-                res = min(res, abs(nums[3] - nums[-1]))
-            if i < len(nums) - 4:
-                res = min(res, abs(nums[0] - nums[-4]))
-            
-            if i >= 2 and i < len(nums) - 2:
-                res = min(res, abs(nums[2] - nums[-2]))
-            
-            if i >= 1 and i < len(nums) - 3:
-                res = min(res, abs(nums[1] - nums[-3]))
+        #take 3 from left
+        res = min(res, abs(nums[3] - nums[-1]))
+        res = min(res, abs(nums[0] - nums[-4]))
+        res = min(res, abs(nums[2] - nums[-2]))
+        res = min(res, abs(nums[1] - nums[-3]))
 
         return res
