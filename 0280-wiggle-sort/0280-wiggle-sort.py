696fbbef878ba535870208caefeb0a78799d0d46
tannerr12 <tanner.01@hotmail.com>
2023-02-07 21:24:35 -0500

    Time: 98 ms (66.05%), Space: 15.2 MB (8.61%) - LeetHub

--- 0280-wiggle-sort/0280-wiggle-sort.py
+++ 0280-wiggle-sort/0280-wiggle-sort.py
@@ -4,7 +4,6 @@ class Solution:
         Do not return anything, modify nums in-place instead.
         """
         nums.sort()
-        bit = 0
         for i in range(1, len(nums) -1,2):
             nums[i], nums[i+1] = nums[i+1],nums[i]
         
