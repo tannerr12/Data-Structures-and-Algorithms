1f1f4185eae3f8ec82a0c8c43dbcdf18c8f3d370
tannerr12 <tanner.01@hotmail.com>
2023-02-17 20:45:30 -0500

    Time: 1351 ms (41.10%), Space: 70.9 MB (51.57%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.py
@@ -0,0 +1,39 @@
+class Solution:
+    def minReorder(self, n: int, connections: List[List[int]]) -> int:
+        
+        
+        adj = defaultdict(list)
+        
+        for x,y in connections:
+            
+            adj[x].append([y,0])
+            adj[y].append([x,1])
+        
+        
+        leaf = []
+        
+        for key,val in adj.items():
+            if len(val) <= 1:
+                leaf.append(key)
+                
+        
+   
+        res = 0
+        def dfs(i,par):
+            nonlocal res
+            
+            if i not in adj:
+                return 
+            
+            for key,c in adj[i]:
+                if key == par:
+                    continue
+                res += c
+                dfs(key,i)
+            
+            
+           
+        
+        dfs(0,-1)
+        
+        return n - res -1
