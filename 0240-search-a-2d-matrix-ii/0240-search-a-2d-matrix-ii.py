0dead2e9db917383d2368ec6feaed0508dbfc7ea
tannerr12 <tanner.01@hotmail.com>
2023-02-25 20:00:51 -0500

    Time: 172 ms (77.55%), Space: 21.2 MB (5.42%) - LeetHub

--- 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.py
@@ -1,49 +1,33 @@
 class Solution:
     def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
         
+        #divide and conquer method to search grid
         
-        def binarySearchLeft(x,y):
+        
+        def search(left,up,right,down):
             
-            l,r = y,len(matrix[0]) -1
             
-            while l <= r:
+            if left > right or up > down:
+                return False
+            
+            
+            #we should check the top left and bottom right corner to make sure target is within range
+            elif matrix[up][left] > target or matrix[down][right] < target:
+                return False
+            
+            mid = (left + right) // 2
+            
+            row = up
+            
+            while row <= down and matrix[row][mid] <= target:
                 
-                curr = (l+r) // 2
-                
-                
-                if matrix[x][curr] == target:
+                if matrix[row][mid] == target:
                     return True
-                
-                if matrix[x][curr] > target:
-                    r = curr -1
-                else:
-                    l = curr +1
-            return False
-        
-        def binarySearchUp(x,y):
+                row +=1
+            #search bottom left or search top right
+            res = search(left,row, mid - 1,down) or search(mid+1, up, right, row-1)
+            return res
             
-            l,r = y,len(matrix) -1
-            
-            while l <= r:
-                
-                curr = (l+r) // 2
-                
-                
-                if matrix[curr][y] == target:
-                    return True
-                
-                if matrix[curr][y] > target:
-                    r = curr -1
-                else:
-                    l = curr +1
-            return False
         
-        for i in range(min(len(matrix), len(matrix[0]))):
-            
-            if binarySearchLeft(i,i):
-                return True
-            if binarySearchUp(i,i):
-                return True
-        
-        return False
-                
+        return search(0,0,len(matrix[0])-1, len(matrix)-1)
+    
