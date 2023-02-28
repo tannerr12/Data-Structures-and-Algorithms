b933d28c13dbaeb78e682130f8683f928af805c5
tannerr12 <tanner.01@hotmail.com>
2023-02-27 22:06:16 -0500

    Time: 5099 ms (5.03%), Space: 566.1 MB (5.14%) - LeetHub

--- 0869-reordered-power-of-2/0869-reordered-power-of-2.py
+++ 0869-reordered-power-of-2/0869-reordered-power-of-2.py
@@ -1,20 +1,12 @@
 class Solution:
     def reorderedPowerOf2(self, n: int) -> bool:
-        
-        ls=[]
-        
-        for char in str(n):
-            ls.append(char)
-            
-        N = len(str(n))
+
         mask = 0
+        
+        n = str(n)
+        N = len(n)
         for i in range(N):
-            
             mask |= (1 << i)
-        
-        
-        
-        val = str(n)
         @cache
         def backtrack(bitmask,num):
             
@@ -27,10 +19,10 @@ class Solution:
             res = False
             for i in range(N):
                 
-                if (num == 0 and val[i] == '0') or bitmask & (1 << i) != 0:
+                if (num == 0 and n[i] == '0') or bitmask & (1 << i) != 0:
                     continue
                 
-                res = res or backtrack(bitmask | (1<< i), (num * 10) + int(val[i]))
+                res = res or backtrack(bitmask | (1<< i), (num * 10) + int(n[i]))
             
             
             return res
