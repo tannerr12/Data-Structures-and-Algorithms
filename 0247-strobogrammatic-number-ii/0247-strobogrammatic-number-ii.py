c34427952d2b8afead91c6a94868fe703fe81e94
tannerr12 <tanner.01@hotmail.com>
2023-03-03 17:40:42 -0500

    Time: 224 ms (40.62%), Space: 29.9 MB (22.16%) - LeetHub

--- 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
@@ -4,7 +4,7 @@ class Solution:
         #if the start number is a 0 and its not length 1 skip it
         res = []
         
-        ls = [0,1,6,8,9]
+        ls = ['0','1','6','8','9']
         def dfs(num):
             
             if len(num) == n:
@@ -15,16 +15,16 @@ class Solution:
             
             for i in ls:
                 
-                if n - len(num) == 1 and (i == 0 or i == 1 or i == 8):
-                    dfs(num[:len(num)//2] + str(i) + num[len(num)//2:])
+                if n - len(num) == 1 and (i == '0' or i == '1' or i == '8'):
+                    dfs(num[:len(num)//2] + i + num[len(num)//2:])
                 
                 if n - len(num) != 1:
-                    if i == 6:
-                        dfs(str(i) + num + '9')
-                    elif i == 9:
-                        dfs(str(i) + num + '6')
-                    elif i == 1 or i == 8 or (i == 0 and n - len(num) != 2 and n - len(num) != 3):
-                        dfs(str(i) + num + str(i))
+                    if i == '6':
+                        dfs(i + num + '9')
+                    elif i == '9':
+                        dfs(i + num + '6')
+                    elif i == '1' or i == '8' or (i == '0' and n - len(num) != 2 and n - len(num) != 3):
+                        dfs(i + num + i)
             
         
         dfs('')
