8b343b601cfdbbbcdf6df8e4a2a481b9e4632c63
tannerr12 <tanner.01@hotmail.com>
2023-03-03 17:34:04 -0500

    Time: 312 ms (21.59%), Space: 30.1 MB (10.23%) - LeetHub

--- 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
+++ 0247-strobogrammatic-number-ii/0247-strobogrammatic-number-ii.py
@@ -1,6 +1,7 @@
 class Solution:
     def findStrobogrammatic(self, n: int) -> List[str]:
-        #go left to right on all numbers and add the same number to the front and back as we 
+        #go left to right on all numbers and add the same number to the front and back for 1 8 or 0 as they rotate the same for 6 and 9 assign them opposite and for middle we can only use 0,1,8
+        #if the start number is a 0 and its not length 1 skip it
         res = []
         
     
