f3b077239731abb3420ae484add34c37bf4b3a37
tannerr12 <tanner.01@hotmail.com>
2023-03-02 23:35:20 -0500

    Time: 30 ms (78.91%), Space: 13.9 MB (54.52%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,17 +1,44 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
         
-        needle = deque(needle)
-        s  = deque()
-    
-        for i in range(len(haystack)):
-            s.append(haystack[i])
+        lps = [0] * len(needle)
+        lis = 0
+        i = 1
+        
+        while i < len(needle):
             
-            if len(s) > len(needle):
-                s.popleft()
+            if needle[lis] == needle[i]:
+                
+                lps[i] = lis + 1
+                lis +=1 
+                i+=1
+            elif lis == 0:
+                lps[i] = 0
+                i+=1
+            else:
+                
+                lis = lps[lis-1]
+
+        #haystack
+        i = 0
+        #needle
+        j = 0
+        
+        while i < len(haystack):
+            
+            if needle[j] == haystack[i]:
+                i+=1
+                j+=1
+            
+            elif j == 0:
+                i+=1
+            
+            else:
+                j = lps[j-1]
             
             
-            if len(s) == len(needle) and s == needle:
-                return i - len(s) +1
-            
+            if j >= len(needle):
+                return i - len(needle)
+        
         return -1
+                
