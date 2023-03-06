ff7c6bc1f13a5bce02bb5a7b5b8cf451abeb35ec
tannerr12 <tanner.01@hotmail.com>
2023-03-06 10:38:06 -0500

    Time: 46 ms (64.52%), Space: 13.9 MB (66.52%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.py
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.py
@@ -1,29 +1,34 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        """
+        
         i = 2
-        res = 1
+        res = 0
         
         while n != 0 and i < 1000:
-            if n % i == 0:
-                res +=1
+        
             
             while n % i == 0:
                 n //= i
-                res +=1
+                res +=i
             
             
             i += 1 + i % 2
         
         
         return res
+    
+        
+        
+        
+        
+        
         """
         if n == 1:
             return 0
         if n == 2:
             return 2
         
-        
+        @cache
         def dfs(count, cur):
             
             if count == n:
@@ -41,5 +46,5 @@ class Solution:
         
         
         return dfs(2, 1) + 2
-            
+        """  
             
