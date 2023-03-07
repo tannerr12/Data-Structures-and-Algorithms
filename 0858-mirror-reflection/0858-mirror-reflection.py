61f3aa0a12c47627a1eab1b5016a29595a2265bf
tannerr12 <tanner.01@hotmail.com>
2023-03-07 17:26:24 -0500

    Time: 32 ms (64.41%), Space: 13.9 MB (12.16%) - LeetHub

---
+++ 0858-mirror-reflection/0858-mirror-reflection.py
@@ -0,0 +1,34 @@
+class Solution:
+    def mirrorReflection(self, p: int, q: int) -> int:
+        
+        
+        #top left = divisible by p and bounces are even cycle odd
+        #top right = divisible by p and bounces are odd cycle odd
+        #bottom right = divisible by p and bounces odd and cycle even
+        
+        if q == p:
+            return 1
+        if q == 0:
+            return 0
+        val = q
+        cycle = 1
+        bounce = 1
+        while val != 0:
+            
+            if val + q > p:
+                cycle +=1
+            val += q
+            val %= p
+            bounce +=1
+            
+        
+        
+        print(cycle)
+        print(bounce)
+        
+        if cycle % 2 and bounce % 2 == 0:
+            return 2
+        if cycle % 2 and bounce % 2:
+            return 1
+        else:
+            return 0
