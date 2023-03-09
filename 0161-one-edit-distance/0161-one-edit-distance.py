ac540877722c6e893a3ac019e6595b6d957c9465
tannerr12 <tanner.01@hotmail.com>
2023-03-08 21:01:25 -0500

    Time: 38 ms (44.86%), Space: 14 MB (34.19%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.py
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -13,43 +13,26 @@ class Solution:
             
             return diff == 1
         
-        elif len(s) < len(t):
-            
-            i,j = 0,0
-            diff = 0
-            while i < len(s) and j < len(t):
-                
-                if s[i] != t[j]:
-                    j+=1
-                    diff +=1
-                
-                else:
-                    i+=1
-                    j+=1
-            
-            if j < len(t):
-                diff += len(t) - j
-            
-            return diff == 1
-        
-        else:
-            
-            i,j = 0,0
-            diff = 0
-            while i < len(t) and j < len(s):
-                
-                if t[i] != s[j]:
-                    j+=1
-                    diff +=1
-                
-                else:
-                    i+=1
-                    j+=1
-            
-            if j < len(s):
-                diff += len(s) - j
-            
-            return diff == 1
-         
+        if len(t) < len(s):
+            s,t = t,s
+        i,j = 0,0
+        diff = 0
+        while i < len(s) and j < len(t):
+
+            if s[i] != t[j]:
+                j+=1
+                diff +=1
+
+            else:
+                i+=1
+                j+=1
+
+        if j < len(t):
+            diff += len(t) - j
+
+        return diff == 1
+
+
+
             
         
