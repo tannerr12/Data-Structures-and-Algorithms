3dcfeaddcc7fd1973b24f85380c68ff517c20edf
tannerr12 <tanner.01@hotmail.com>
2023-03-09 20:38:21 -0500

    Time: 29 ms (94.18%), Space: 14 MB (76.40%) - LeetHub

---
+++ 0241-different-ways-to-add-parentheses/0241-different-ways-to-add-parentheses.py
@@ -0,0 +1,53 @@
+class Solution:
+    def diffWaysToCompute(self, expression: str) -> List[int]:
+        arr = []
+        num = 0
+        c = 0
+        for i in range(len(expression)):
+            
+            if expression[i] == '-' or expression[i] == '*' or expression[i] == '+':
+                
+                arr.append(num)
+                num = 0
+                arr.append(expression[i])
+  
+            else:
+                num = num * 10 + int(expression[i])
+        
+        else:
+            arr.append(num)
+        
+        print(arr)
+        
+        def backtrack(left,right):
+            
+            if left == right:
+                return [arr[left]]
+            ans = []
+            for i in range(left+1,right,2):
+                r = backtrack(i+1,right)
+                l = backtrack(left, i-1)
+            
+                if arr[i] == "*":
+                    for k in l:
+                        for j in r:
+                            ans.append(k * j)
+                               
+                if arr[i] == '-':
+                    for k in l:
+                        for j in r:
+                            ans.append(k - j)
+                if arr[i] == "+":
+                    for k in l:
+                        for j in r:
+                            ans.append(k + j)
+            
+            return ans
+            
+        return backtrack(0,len(arr)-1)
+        
+            
+            
+            
+            
+            
