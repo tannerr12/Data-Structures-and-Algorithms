616e21bba5abb9d0bfbdd09063cb2ac82d841d10
tannerr12 <tanner.01@hotmail.com>
2023-03-11 16:42:06 -0500

    Time: 632 ms (83.88%), Space: 28.6 MB (57.85%) - LeetHub

--- 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
+++ 1574-shortest-subarray-to-be-removed-to-make-array-sorted/1574-shortest-subarray-to-be-removed-to-make-array-sorted.py
@@ -1,7 +1,8 @@
 class Solution:
     def findLengthOfShortestSubarray(self, arr: List[int]) -> int:
         
-        
+        #check both the left and right sides of the array first and keep l as a starting point
+    
         l,r = 0,len(arr)-1
         
         while l < len(arr):
@@ -24,8 +25,14 @@ class Solution:
             else:
                 break
         
+        
         res = min(len(arr) - (l + 1), tr)
         prev = arr[r]
+        
+        #now process the array r starting at the end and l starting at its max point 
+        #if the l value is greater than r shift l -=1
+        #if the r value is greater = to left keep shifting and update res
+        #if right ever starts to increase break we cant go any further
         while l >= 0 and arr[r] <= prev:
             
             while l >= 0 and arr[r] < arr[l]:
