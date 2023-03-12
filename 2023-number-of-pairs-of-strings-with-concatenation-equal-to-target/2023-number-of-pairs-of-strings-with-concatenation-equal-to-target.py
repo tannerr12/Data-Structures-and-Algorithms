0446d3703e31f3556f99232f2acb448814b6cb4d
tannerr12 <tanner.01@hotmail.com>
2023-03-11 19:34:45 -0500

    Time: 36 ms (95.20%), Space: 14 MB (58.67%) - LeetHub

--- 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
+++ 2023-number-of-pairs-of-strings-with-concatenation-equal-to-target/2023-number-of-pairs-of-strings-with-concatenation-equal-to-target.py
@@ -4,13 +4,25 @@ class Solution:
         
         res = 0
         
+        hmap = defaultdict(int)
         for i in range(len(nums)):
+            hmap[nums[i]] += 1
+        
             
-            for j in range(i + 1, len(nums)):
-                
-                if nums[i] + nums[j] == target:
-                    res +=1
-                if nums[j] + nums[i] == target:
-                    res +=1
-                    
+        for key,val in hmap.items():
+            
+            if len(key) >= len(target) or target[:len(key)] != key:
+                continue
+            
+            tar = target[len(key):]
+            if tar not in hmap:
+                continue
+            v1 = hmap[tar]
+            
+            if tar == key:
+                res += val * (val-1)
+                continue
+            
+            res += val * v1
+        
         return res
