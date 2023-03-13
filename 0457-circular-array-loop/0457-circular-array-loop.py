60ae9b130b6a1e5af68f26695005cd046cb08874
tannerr12 <tanner.01@hotmail.com>
2023-03-13 18:44:40 -0400

    Time: 42 ms (62.44%), Space: 13.9 MB (48.90%) - LeetHub

--- 0457-circular-array-loop/0457-circular-array-loop.py
+++ 0457-circular-array-loop/0457-circular-array-loop.py
@@ -6,8 +6,10 @@ class Solution:
         #a loop must have atleast 1 indegree
         
         parent = [i for i in range(len(nums))]
+        rank = [0] * len(nums)
+        
+        
         def find(val):
-            
             if val == parent[val]:
                 return val
             
@@ -20,45 +22,24 @@ class Solution:
             v1,v2 = find(x), find(y)
             
             if v1 != v2:
-                parent[v1] = parent[v2]
+                if rank[v1] > rank[v2]:
+                    parent[v2] = v1
+                elif rank[v2] > rank[v1]:
+                    parent[v1] = v2
+                else:
+                    parent[v2] = v1
+                    rank[v1] +=1
+                    
                 return False
             return True
         
-        adj = defaultdict(list)
-        badLoop = set()
-        indegree = set()
+        
         for i in range(len(nums)):
-
             left = i
             right = (left + nums[i]) % len(nums)
             
-            if nums[left] > 0 and nums[right] < 0 or nums[left] < 0 and nums[right] > 0 or right == left:
-                badLoop.add(i)
-            else:
-                pos = 0
-                if nums[left] > 0:
-                    pos = 1
-               # if right in adj and ((nums[right] > 0 and pos) or (nums[right] < 0 and not pos)):
-               #     return True
-                adj[i].append((right,pos))
+            if ((nums[left] > 0 and nums[right] > 0) or (nums[left] < 0 and nums[right] < 0)) and right != left:
                 if union(left,right):
                     return True
-                indegree.add(right)
-
-        # 0 - > 2 -> 4 - > 6 - > 7
-        # 1 - > 3 - > 5 - > 2
-        #[2,2,2,2,2,4,7]
-        #return False
-        #def dfs(i):
-            
-            
-        #print(adj)
-        #print(badLoop)
-        #res = False
         
-        #for key in adj:
-        #    if key not in indegree:
-        #        continue
-            
-        #    dfs(key)
-            
+        return False
