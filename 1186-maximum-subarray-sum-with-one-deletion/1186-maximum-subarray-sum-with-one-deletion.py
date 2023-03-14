976b0517c65708e04cf718cbfd4b6814d07bccce
tannerr12 <tanner.01@hotmail.com>
2023-03-14 14:14:07 -0400

    Time: 277 ms (65.22%), Space: 24.7 MB (87.58%) - LeetHub

--- 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.py
@@ -2,80 +2,22 @@ class Solution:
     def maximumSum(self, arr: List[int]) -> int:
         #anytime our running total can stay >= 0 we should keep it
         #we should also keep track of our biggest negative to remove it from sum when we see a negative
-        r = 0
-        r2 = 0
-        res = float('-inf')
-        mxNeg = float('-inf')
-        curmxNeg = 0
-        curmxNeg2 = 0
-        left = float('inf')
-        right = -1
+        
+        
+        res = 0
+        
+        noskip = 0
+        skip = 0
+        allneg = True
+        mxneg = float('-inf')
         for i in range(len(arr)):
-            
-            if arr[i] > 0:
-                left = min(i, left)
-                right = max(i, right)
+            if arr[i] >=0:
+                allneg = False
             else:
-                mxNeg = max(arr[i],mxNeg)
-        
-        if left == float('inf'):
-            return mxNeg
-    
-        
-        for i in range(left, len(arr)):
+                mxneg = max(arr[i],mxneg)
+            skip = max(noskip, skip + arr[i])
+            noskip = max(noskip + arr[i],arr[i])
+            res = max(res, skip,noskip)
             
-            if arr[i] >= 0:
-                r += arr[i]
-                r2 += arr[i]
-                res = max(res,r,r2)
-
-            else:
-                res = max(res,r,r2)
-                if arr[i] < curmxNeg:
-                    r += curmxNeg
-                    r2 += curmxNeg2
-                else:
-                    r += arr[i]
-                    r2 += arr[i]
-                
-                
-                curmxNeg = min(curmxNeg, arr[i])
-                curmxNeg2 = min(curmxNeg2, arr[i])
-                if r + curmxNeg <= 0:
-                    r = 0
-                    curmxNeg = 0
-                if r2 <= 0:
-                    r2 = 0
-                    curmxNeg2 = 0
         
-        r = 0
-        r2 = 0
-        curmxNeg = 0
-        curmxNeg2 = 0
-        for i in range(right,-1,-1):
-            
-            if arr[i] >= 0:
-                r += arr[i]
-                r2 += arr[i]
-                res = max(res,r,r2)
-            else:
-                res = max(res,r,r2)
-                if arr[i] < curmxNeg:
-                    r += curmxNeg
-                    r2 += curmxNeg2
-                else:
-                    r += arr[i]
-                    r2 += arr[i]
-
-                curmxNeg = min(curmxNeg, arr[i])
-                curmxNeg2 = min(curmxNeg2, arr[i])
-                if r + curmxNeg <= 0:
-                    r = 0
-                    curmxNeg = 0
-                if r2 <= 0:
-                    r2 = 0
-                    curmxNeg2 = 0
-                
-        
-        
-        return res 
+        return res if allneg == False else mxneg
