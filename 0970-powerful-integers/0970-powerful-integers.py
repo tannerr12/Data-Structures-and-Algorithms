c7e38475a9376a58a38d12ae9dd2bd354f88767e
tannerr12 <tanner.01@hotmail.com>
2023-03-14 20:46:47 -0400

    Time: 33 ms (71.43%), Space: 13.9 MB (78.88%) - LeetHub

--- 0970-powerful-integers/0970-powerful-integers.py
+++ 0970-powerful-integers/0970-powerful-integers.py
@@ -1,36 +1,21 @@
 class Solution:
     def powerfulIntegers(self, x: int, y: int, bound: int) -> List[int]:
-        
-        i =0
-        ls1 = []
-        ls2 = []
-        
-        if x == 1:
-            ls1 = [1]
-        else:
-            while x ** i < bound:
-                ls1.append(x ** i)
-                i+=1
-        
-        i = 0
-        
-        if y == 1:
-            ls2 = [1]
-        else:
-            while y ** i < bound:
-                ls2.append(y**i)
-                i+=1
 
-                
         res = set()
-        
-        for i in range(len(ls1)):
-            for j in range(len(ls2)):
-                
-                v1,v2 = ls1[i], ls2[j]
-                if v1 + v2 > bound:
-                    break
+        i = 0
+        while x ** i <= bound:
+            v1 = x ** i
+            j = 0
+            while (y ** j) + v1 <= bound:
+                v2 = y ** j
                 res.add(v1 + v2)
+                if y == 1:
+                    break
+                j+=1
+                
+            if x == 1:
+                break
+            i+=1
         
         
         return list(res)
