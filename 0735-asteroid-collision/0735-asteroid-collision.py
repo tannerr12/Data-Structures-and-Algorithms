f524b9e3c706a1ac3c90b217fd57de3878395baa
tannerr12 <tanner.01@hotmail.com>
2023-03-14 22:32:52 -0400

    Time: 93 ms (89.32%), Space: 15.3 MB (19.08%) - LeetHub

--- 0735-asteroid-collision/0735-asteroid-collision.py
+++ 0735-asteroid-collision/0735-asteroid-collision.py
@@ -6,18 +6,23 @@ class Solution:
         negsur = []
         for i in range(len(a)):
             
+            #anything that is positive just add to stack
             if a[i] >= 0:
                 stack.append(a[i])
             
             else:
-                
+                #if our negative astroid is larger keep crushing our stack
                 while stack and abs(a[i]) > stack[-1]:
                     stack.pop()
                 
+                #it crushed the whole stack, it survives
                 if not stack:
                     negsur.append(a[i])
+                
+                #it hit an astroid of the same size they both die
                 elif stack and stack[-1] == abs(a[i]):
                     stack.pop()
         
         
+        #negatives always have to be at the beginning & positives at the end
         return negsur + stack
