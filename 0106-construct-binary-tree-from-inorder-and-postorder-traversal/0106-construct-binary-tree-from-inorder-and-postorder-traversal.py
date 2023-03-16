0414d75ee05096207ac0a923868794c924be2b44
tannerr12 <tanner.01@hotmail.com>
2023-03-15 21:27:32 -0400

    Time: 51 ms (97.18%), Space: 19 MB (74.09%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.py
@@ -12,30 +12,34 @@ class Solution:
         
         
         keyidx = {}
-        
+        #map out all of the values for inorder first to save time searching
         for i in range(len(inorder)):
             
             keyidx[inorder[i]] = i
-            
-        
+ 
+    
+        #we will use post order for our middle nodes and each time we will decrement to get a new middle node.. this will help us keep track
         index = len(postorder)
+        
         def dfs(l,r):
             nonlocal index
+            
+            #left is bigger than right?
             if l > r:
                 return
-            
+            #decrement our middle node
             index -=1
             val = postorder[index]
+            #create a new node with middle value
             root = TreeNode(val)
             
+            #find the middle in our inorder so that left of this value is our left subtree and right of this value is our right subtree
             mid = keyidx[val]
             
-            
+            #build our right than our left since the postorder backwards goes right first
             root.right = dfs(mid + 1, r)
             root.left = dfs(l, mid -1)
-            
-            
-            
+    
             return root
         
         
