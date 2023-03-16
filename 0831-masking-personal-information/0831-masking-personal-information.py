8b66fff409fe4fe281457dd54844cc3ffef2c54f
tannerr12 <tanner.01@hotmail.com>
2023-03-16 19:07:22 -0400

    Time: 36 ms (30.10%), Space: 14 MB (15.53%) - LeetHub

---
+++ 0831-masking-personal-information/0831-masking-personal-information.py
@@ -0,0 +1,56 @@
+class Solution:
+    def maskPII(self, s: str) -> str:
+        
+        #first figure out if it is an email or a phone #
+        email = False
+        if '@' in s:
+            email = True
+            
+        newst = ''
+        if email:
+            
+            idx = s.find('@')
+            
+            name = s[:idx]
+            
+            domain = s[idx + 1:]
+            
+            newst += s[0].lower()
+            newst += '*' * 5
+            newst += s[idx-1].lower()
+            
+            newst += '@'
+            
+            for st in domain:
+                
+                newst += st.lower()
+            
+            return newst
+            
+        else:
+            
+            #op = s.find('(')
+            
+            #cl = s.find(')')
+            
+            #bracket = s.find('-')
+            ns = ''
+            for char in s:
+                if char in '()-+ ':
+                    continue
+                
+                ns += char
+            
+            newst = '-' + ns[len(ns) -4:]
+            count = 0
+            
+            for i in range(len(ns)-5,-1,-1):
+                
+                if count and count % 3 == 0:
+                    newst = '-' + newst
+                newst = '*' + newst
+                count +=1
+            
+            if count > 6:
+                newst = '+' + newst
+            return newst
