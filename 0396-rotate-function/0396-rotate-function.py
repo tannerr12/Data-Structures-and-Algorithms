17e42fbfaa3504ddc792292e759bf29709a73ad4
tannerr12 <tanner.01@hotmail.com>
2023-03-16 10:35:29 -0400

    Time: 1401 ms (31.11%), Space: 23 MB (79.72%) - LeetHub

--- 0396-rotate-function/0396-rotate-function.py
+++ 0396-rotate-function/0396-rotate-function.py
@@ -2,19 +2,6 @@ class Solution:
     def maxRotateFunction(self, nums: List[int]) -> int:
         #0s dont matter
         
-        
-        
-        p = 0
-        
-        for i in range(1,len(nums)):
-        
-                p = (nums[i] * i) + p
-                
-        
-        #print(p)
-        
-        
-        total = sum(nums)
         """
         #brute force how can we compress this into linear
         res = 0
@@ -35,21 +22,27 @@ class Solution:
         return res
         """
         
-        #problem is number * distance from position
-        # y + z = a /2 /1
-        # 
+        #we take the current prefix sum with mult as a start and assign that to p
+        #we also take a total of all the values in the array and assign that to total
+        #since each shift will subtract each number 1 time ex: 5 * 2 becomes 5 * 1 which is just subtracting its value in nums once
+        #all we have to do to check the next value is take our current prefix sum - total which is our total without adding the new value
+        #than + our new value * the length of the array since it will be at the end of the array
         
-        # left pointer
+        #example 1: our starting prefix is 25 and our total is 15 we want to shift our starting position to nums[1] = 3 we do 25-15 = 10 than 10 + (4 * 4) = 26
+        #Note: we had to wrap around our right pointer to the first position
+        total = nums[0]
+        p = 0
+        for i in range(1,len(nums)):
+            p = (nums[i] * i) + p
+            total += nums[i]
         r = len(nums) -1
         res = float('-inf')
         for l in range(len(nums)):
             res = max(res, p)
-            
             r +=1
             r %= len(nums)
+            p = (p - total) + nums[r] * len(nums)
             
-            calc = (p - total) + nums[r] * len(nums)
-            p = calc
         
         return res
             
