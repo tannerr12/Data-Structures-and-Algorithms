de84a4ee52b6140b06168c6584a5539e0927b86d
tannerr12 <tanner.01@hotmail.com>
2023-03-16 22:55:29 -0400

    Time: 2586 ms (72.73%), Space: 15 MB (58.18%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
@@ -10,7 +10,7 @@ class Solution:
         #use bitmasks to keep track of odd sums and ^ prefix ranges to check if valid
         
         
-        
+        """
         prefix = [0]
         
         for i in range(len(word)):
@@ -18,23 +18,26 @@ class Solution:
             
             ch = ord(word[i]) - ord('a')
             prefix.append(prefix[-1] ^ (1 << ch))
-        
+        """
         #print(prefix)
         
         #go right to left and keep a count of times we see the bitmask
         #also we can go through a - j for each bitmask and count the times a 1 bit diff occurs after this point
         
         mp = defaultdict(int)
+        mp[0] +=1
         res = 0
-        for i in range(len(prefix) -1,-1,-1):    
-            
-            res += mp[prefix[i]]
-            bit = prefix[i]
+        bit = 0
+        for i in range(len(word) -1,-1,-1):    
+            ch = ord(word[i]) - ord('a')
+            bit ^= (1 << ch)
+            res += mp[bit]
+    
             for j in range(10):
                 flip = bit ^ (1 << j)
                 res += mp[flip]
                 
-            mp[prefix[i]] +=1
+            mp[bit] +=1
         
         
         return res
