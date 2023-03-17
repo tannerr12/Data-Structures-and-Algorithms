67924ed1e7402d2fdf9bace1cd9bb1fd7c8ef33c
tannerr12 <tanner.01@hotmail.com>
2023-03-16 22:59:58 -0400

    Time: 2615 ms (65.45%), Space: 15.1 MB (20.00%) - LeetHub

--- 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
+++ 1915-number-of-wonderful-substrings/1915-number-of-wonderful-substrings.py
@@ -1,42 +1,30 @@
 class Solution:
     def wonderfulSubstrings(self, word: str) -> int:
-        
-            # 00
-        #01 # 01
-        #10 # 11
-        #01 # 10
-        #01 # 11
-        
-        #use bitmasks to keep track of odd sums and ^ prefix ranges to check if valid
-        
-        
-        """
-        prefix = [0]
-        
-        for i in range(len(word)):
-            
-            
-            ch = ord(word[i]) - ord('a')
-            prefix.append(prefix[-1] ^ (1 << ch))
-        """
-        #print(prefix)
+
+        #use bitmasks to keep track of odd sums and ^ suffix ranges to check if valid
         
         #go right to left and keep a count of times we see the bitmask
         #also we can go through a - j for each bitmask and count the times a 1 bit diff occurs after this point
         
         mp = defaultdict(int)
+        #the start of our list will be 0 so we increase it by 1
         mp[0] +=1
         res = 0
         bit = 0
         for i in range(len(word) -1,-1,-1):    
+            #extract and apply the bit value from character into our suffix
             ch = ord(word[i]) - ord('a')
             bit ^= (1 << ch)
+            #if we found an exact match add the number of times this occurance has shown up
             res += mp[bit]
-    
+            #A-J so we cycle 10 bits
             for j in range(10):
+                #flip each bit in our current suffix and see how many matches we find
                 flip = bit ^ (1 << j)
+                #add the matches
                 res += mp[flip]
-                
+            
+            #increment this bitmask
             mp[bit] +=1
         
         
