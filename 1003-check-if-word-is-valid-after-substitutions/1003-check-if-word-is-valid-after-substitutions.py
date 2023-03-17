dd70945752c58fd46e0e20aeec089ce1e7ac5f07
tannerr12 <tanner.01@hotmail.com>
2023-03-17 17:33:54 -0400

    Time: 61 ms (52.94%), Space: 14.1 MB (49.13%) - LeetHub

--- 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
+++ 1003-check-if-word-is-valid-after-substitutions/1003-check-if-word-is-valid-after-substitutions.py
@@ -2,16 +2,16 @@ class Solution:
     def isValid(self, s: str) -> bool:
         
         
-        #
-        #c = Counter(s)
         
-        #if s[0] != 'a' or c['a'] != c['b'] or c['a'] != c['c'] or 'abc' not in s:
-        #    return False
+        c = Counter(s)
         
+        if s[0] != 'a' or c['a'] != c['b'] or c['a'] != c['c']:
+            return False
         
+        #the solution is to just go through with a stack and anytime we run into a 'c' we pop twice which should be 'b' and 'a' in that order
+        #if we run into an 'a' or 'b' we simply add it to the top of the stack
         stack = []
-        
-        
+    
         for i in range(len(s)):
             
             char = s[i]
