24ca1187efd7158d696b1786a9b27bc5bf0b73bb
tannerr12 <tanner.01@hotmail.com>
2023-03-19 21:59:29 -0400

    Time: 43 ms (31.68%), Space: 13.9 MB (41.93%) - LeetHub

--- 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
+++ 1008-construct-binary-search-tree-from-preorder-traversal/1008-construct-binary-search-tree-from-preorder-traversal.py
@@ -7,28 +7,34 @@
 class Solution:
     def bstFromPreorder(self, preorder: List[int]) -> Optional[TreeNode]:
         
-        
+        #Working with an array instead of pointers made this problem far easier to solve
         def dfs(A,parent):
-            
+            #return element since we went down to 1
             if len(A) == 1:
                 return TreeNode(A[0])
+            
+            #nothing
             if len(A) ==0:
                 return None
             
             mid = 0
             
+            #find last lower element
             for i in range(1,len(A)):
             
                 if A[i] > A[0]:
                     break
                 mid = i           
-            
+            #assign node to first element
             t = TreeNode(A[0])
-        
+            
+            #lower elements
             t.left = dfs(A[1:mid+1],A[0])
           
+            #higher elements
             t.right = dfs(A[mid+1:],A[0])
             
+            #return Tree
             return t
         
         return dfs(preorder, preorder[0])
