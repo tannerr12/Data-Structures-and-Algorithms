c94f559b8531d1bd4a7f6ce4b850b03c65be09b1
tannerr12 <tanner.01@hotmail.com>
2023-03-20 18:47:38 -0400

    Time: 37 ms (48.62%), Space: 13.9 MB (22.13%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,22 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        
+        @cache
+        def dfs(num):
+            
+            if num == 1:
+                return 0
+            
+            res = float('inf')
+            #if even always // 2
+            if num % 2 == 0:
+                res = min(res, dfs(num // 2) + 1)
+            #else if odd we take 2 paths
+            else:
+                res = min(res,dfs(num + 1) + 1)
+                res = min(res,dfs(num - 1) + 1)
+            
+            
+            return res
+        
+        return dfs(n)
