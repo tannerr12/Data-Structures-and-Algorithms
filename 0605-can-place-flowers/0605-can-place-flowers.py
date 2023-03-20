31f41dc86702ca06b156a308fd862c1c612a159c
tannerr12 <tanner.01@hotmail.com>
2023-03-19 20:38:38 -0400

    Time: 168 ms (62.38%), Space: 14.4 MB (20.94%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -3,20 +3,21 @@ class Solution:
         
       
         new = 0
-        for i in range(len(f)):
-      
+        i = 0
+        while i < len(f):
+            if f[i] == 1:
+                i += 2
+                continue
             if i == 0:
-                
                 if f[i] == 0 and i + 1 < len(f) and f[i+1] != 1 or f[i] == 0 and i + 1 == len(f):
                     new +=1
-                    f[i] = 1 
-
+                    f[i] = 1
+                    i+=1
             else:
-                
                 if f[i] == 0 and i + 1 < len(f) and f[i+1] != 1 and f[i-1] != 1 or f[i] == 0 and i + 1 == len(f) and f[i-1] != 1: 
                     new +=1
-                    f[i] = 1 
-    
-        
-    
+                    f[i] = 1
+                    i+=1
+            i+=1
+
         return new >= n
