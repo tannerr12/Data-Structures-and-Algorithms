7e8f220fcf4c98dd7dc57250fedc37347991efa2
tannerr12 <tanner.01@hotmail.com>
2023-03-19 20:37:31 -0400

    Time: 179 ms (30.39%), Space: 14.5 MB (20.94%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.py
+++ 0605-can-place-flowers/0605-can-place-flowers.py
@@ -1,27 +1,22 @@
 class Solution:
     def canPlaceFlowers(self, f: List[int], n: int) -> bool:
         
-        orig = Counter(f)
-        
+      
+        new = 0
         for i in range(len(f)):
-            
+      
             if i == 0:
                 
-                if i + 1 < len(f) and f[i+1] != 1:
+                if f[i] == 0 and i + 1 < len(f) and f[i+1] != 1 or f[i] == 0 and i + 1 == len(f):
+                    new +=1
                     f[i] = 1 
-                
-                if i + 1 == len(f):
-                    f[i] = 1
-            
+
             else:
                 
-                if i + 1 < len(f) and f[i+1] != 1 and f[i-1] != 1: 
+                if f[i] == 0 and i + 1 < len(f) and f[i+1] != 1 and f[i-1] != 1 or f[i] == 0 and i + 1 == len(f) and f[i-1] != 1: 
+                    new +=1
                     f[i] = 1 
-                
-                if i + 1 == len(f) and f[i-1] != 1:
-                    f[i] = 1
+    
         
-        
-        c = Counter(f)
-        
-        return c[1] - orig[1] >= n
+    
+        return new >= n
