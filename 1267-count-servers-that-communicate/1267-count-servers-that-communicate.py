fa3a1bb67633345e53d7a1ae70c6c8ec06068199
tannerr12 <tanner.01@hotmail.com>
2023-03-21 19:40:54 -0400

    Time: 497 ms (47.06%), Space: 15.6 MB (90.76%) - LeetHub

--- 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
+++ 1267-count-servers-that-communicate/1267-count-servers-that-communicate.py
@@ -1,18 +1,31 @@
 class Solution:
     def countServers(self, grid: List[List[int]]) -> int:
         
-        rows = defaultdict(set)
-        cols = defaultdict(set)
+        rows = defaultdict(int)
+        cols = defaultdict(int)
         
         for i in range(len(grid)):
             
             for j in range(len(grid[0])):
                 
                 if grid[i][j] == 1:
-                    rows[i].add((i,j))
-                    cols[j].add((i,j))
+                    rows[i] +=1
+                    cols[j] +=1
         
         
+        res = 0
+        
+        for i in range(len(grid)):
+            
+            for j in range(len(grid[0])):
+                
+                if grid[i][j] == 1:
+                    if rows[i] > 1 or cols[j] > 1:
+                        res +=1
+        
+        return res
+        
+        """
         res = set()
         
         for key,val in rows.items():
@@ -24,6 +37,4 @@ class Solution:
         
         #print(res)
             
-            
-        return len(res)
-    
+        """
