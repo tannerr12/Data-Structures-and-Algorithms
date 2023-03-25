edc5e3f9f40885e59e022158d7bd46864fc245ac
tannerr12 <tanner.01@hotmail.com>
2023-03-25 19:43:37 -0400

    Time: 12763 ms (5.15%), Space: 182.8 MB (5.15%) - LeetHub

---
+++ 1981-minimize-the-difference-between-target-and-chosen-elements/1981-minimize-the-difference-between-target-and-chosen-elements.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minimizeTheDifference(self, mat: List[List[int]], target: int) -> int:
+        res = float('inf')
+        @cache
+        def dfs(r,total):
+            nonlocal res
+            if total - target >= res:
+                return
+            if r == len(mat):
+                res = min(res,abs(target - total))
+                return 
+
+            for j in range(len(mat[r])):
+                dfs(r + 1,total + mat[r][j])
+            
+            return res
+        
+        
+        return dfs(0,0)
