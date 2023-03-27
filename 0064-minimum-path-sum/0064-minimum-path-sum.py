29e282540b223a5610455d546141ca83888a87b8
tannerr12 <tanner.01@hotmail.com>
2023-03-26 20:26:42 -0400

    Time: 168 ms (5.04%), Space: 19.3 MB (13.62%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.py
+++ 0064-minimum-path-sum/0064-minimum-path-sum.py
@@ -1,6 +1,6 @@
 class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
-        
+        """
         
         dp = [[0 for j in range(len(grid[0]) + 1)] for i in range(len(grid) + 1)]
          
@@ -23,5 +23,27 @@ class Solution:
                 dp[i][j] = min(dp[i+1][j], dp[i][j+1]) + grid[i][j]
                 
         
-        print(dp)
+        
         return dp[0][0]
+        """
+        
+        heap = [[grid[0][0],0,0]]
+        seen = set()
+        while heap:
+            
+            val, r,c = heappop(heap)
+            
+            if (r,c) in seen:
+                continue
+            seen.add((r,c))
+            if r == len(grid)-1 and c == len(grid[0])-1:
+                return val
+            #down 
+            if r + 1 < len(grid):
+                heappush(heap, [val + grid[r+1][c], r+1,c])
+            #right
+            if c + 1 < len(grid[0]):
+                heappush(heap,[val + grid[r][c+1], r, c+1])
+        
+        
+        
