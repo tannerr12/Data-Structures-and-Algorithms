c88b6e75052838eb8a8880bc2aac0f270628932d
tannerr12 <tanner.01@hotmail.com>
2023-03-28 23:55:51 -0400

    Time: 860 ms (98.36%), Space: 28.3 MB (57.38%) - LeetHub

--- 2031-count-subarrays-with-more-ones-than-zeros/2031-count-subarrays-with-more-ones-than-zeros.py
+++ 2031-count-subarrays-with-more-ones-than-zeros/2031-count-subarrays-with-more-ones-than-zeros.py
@@ -49,6 +49,8 @@ class Solution:
         """
         
         mp = defaultdict(int)
+        #start off the prefix
+        #start of the array = 0
         mp[0] = 1
         
         run, res,total = 0,0,0
@@ -56,14 +58,19 @@ class Solution:
         for i in nums:
             
             if i > 0:
+                #if we see a 1 we update our prefix sum
                 run += 1
                 #
+                #this is the key it is the number of numbers before this point that are less than our running total
                 total += mp[run-1]
             else:
                 run -=1
+                #this is also key here we are removing what we had previously added earlier 0 -> 1 -> 0 first we add 0 than we remove 0 when we go below it
                 total -= mp[run]
             
+            #add number of numbers less than our running total before this point
             res += total
+            #add the running total we just saw
             mp[run] += 1
         
         return res % MOD
