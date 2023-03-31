8bddf0d09b038981a48effafe592dd0a1a96dfae
tannerr12 <tanner.01@hotmail.com>
2023-03-31 16:06:16 -0400

    Time: 1841 ms (65.00%), Space: 55.6 MB (37.50%) - LeetHub

--- 2113-elements-in-array-after-removing-and-replacing-elements/2113-elements-in-array-after-removing-and-replacing-elements.py
+++ 2113-elements-in-array-after-removing-and-replacing-elements/2113-elements-in-array-after-removing-and-replacing-elements.py
@@ -1,33 +1,27 @@
 class Solution:
     def elementInNums(self, nums: List[int], queries: List[List[int]]) -> List[int]:
         
-        
-        for i in range(len(queries)):
-            
-            queries[i].append(i)
-            
-            
-        queries.sort()
-        
+
         idx = 0
-        time = 0
-        turn = False
-        
-        
+            
         res = [-1] * len(queries)
-        for t,j,i in queries:
+        i = 0
+        for t,j in queries:
             
             idx = t % len(nums)
             cycle = t // len(nums)
             if cycle % 2 == 1 and idx == 0:
+                i+=1
                 continue
             elif cycle % 2 == 0 and j + idx < len(nums):
                 ans = nums[j + idx]
-                res[i] = ans
+                res[i] =ans
             elif cycle % 2 == 1 and j < idx:
                 ans = nums[j]
-                res[i] = ans
-
+                res[i] =ans
+            
+            
+            i+=1
         return res
                 
         
