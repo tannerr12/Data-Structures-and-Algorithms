77da71307b6d1a937d59e6d5052d04278bf7074e
tannerr12 <tanner.01@hotmail.com>
2023-04-01 15:07:56 -0400

    Time: 945 ms (100.00%), Space: 29.4 MB (100.00%) - LeetHub

--- 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.py
+++ 2607-make-k-subarray-sums-equal/2607-make-k-subarray-sums-equal.py
@@ -2,7 +2,11 @@ class Solution:
     def makeSubKSumEqual(self, arr: List[int], k: int) -> int:
         
         mp = defaultdict(list)
-        
+        #gcd is important here to account for k overlapps 2,5,5,7 k = 3 
+        #if we try to use k here we will have 3 groups 2 and 7 as well as 5,5 in seperate groups  
+        #if we use GCD it will count the overlaps as well since after 5 the next 3 is actually idx = 1 than idx =0 so they should all be in the same group
+        #gcd len(arr) and k is 1 here which puts all numbers in the same group
+                                                
         g = gcd(len(arr), k)
         
         for i in range(len(arr)):
