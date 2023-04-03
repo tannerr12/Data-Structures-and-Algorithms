4c555242949880526682fd19d57d046d2d293757
tannerr12 <tanner.01@hotmail.com>
2023-04-02 21:07:37 -0400

    Time: 66 ms (50.19%), Space: 14.8 MB (5.79%) - LeetHub

---
+++ 0649-dota2-senate/0649-dota2-senate.py
@@ -0,0 +1,37 @@
+class Solution:
+    def predictPartyVictory(self, s: str) -> str:
+        
+        rad = s.count('R')
+        dire = s.count('D')
+        r1,d1 = rad,dire
+        banned = set()
+        rban = 0
+        dban = 0
+        while True:
+
+            for i in range(len(s)):
+                if i in banned:
+                    continue
+                if s[i] == 'R' and dban:
+                    dban -=1
+                    banned.add(i)
+                    r1 -=1
+                    continue
+                elif s[i] == 'D' and rban:
+                    rban -=1
+                    banned.add(i)
+                    d1 -=1
+                    continue
+                    
+                if s[i] == 'R' and d1 > 0:
+                    rban += 1
+                elif s[i] == 'D' and r1 > 0:
+                    dban += 1
+                    
+                
+                
+            
+            if r1 and not d1:
+                return "Radiant"
+            elif d1 and not r1:
+                return "Dire"
