004f0b8872061382a20c6311f79cbd8933d5dfe9
tannerr12 <tanner.01@hotmail.com>
2023-04-05 20:11:31 -0400

    Time: 2034 ms (88.03%), Space: 89.8 MB (76.06%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.py
@@ -1,5 +1,6 @@
 class Solution:
     def containsCycle(self, grid: List[List[str]]) -> bool:
+        """
         n,m = len(grid), len(grid[0])
         
         seen = set()
@@ -33,6 +34,62 @@ class Solution:
         
         
         return False
+        """ 
+        n,m = len(grid), len(grid[0])  
+
+        parent= {}
+        rank = defaultdict(int)
+        
+        def find(val):
+            
+            if parent[val] == val:
+                return val
+            
+            parent[val] = find(parent[val])
+            return parent[val]
+            
+        def union(x,y):
+            
+            v1,v2 = find(x), find(y)
             
             
+            if v1 != v2:
+                
+                if rank[v1] > rank[v2]:
+                    parent[v2] = v1
+                elif rank[v2] > rank[v1]:
+                    parent[v1] = v2
+                    
+                else:
+                    parent[v2] = v1
+                    rank[v1] += 1
+                
+                return False
             
+            return True
+        
+        def isConnected(x,y):
+            return find(x) == find(y)
+        
+        
+        for i in range(n):
+            for j in range(m):
+                
+                if i + 1 < n and grid[i+1][j] == grid[i][j]:
+                    if (i,j) not in parent:
+                        parent[(i,j)] = (i,j)
+                    if (i+1, j) not in parent:
+                        parent[(i+1,j)] = (i+1,j)
+                    if union((i,j), (i+1,j)):
+                        return True
+                
+                if j + 1 < m and grid[i][j+1] == grid[i][j]:
+                    if (i,j) not in parent:
+                        parent[(i,j)] = (i,j)
+                    if (i, j + 1) not in parent:
+                        parent[(i,j+1)] = (i,j+1)
+                    if union((i,j), (i,j+1)):
+                        return True
+        
+        return False
+                
