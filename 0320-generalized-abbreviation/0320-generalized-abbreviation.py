ec99799071e1a6d2cdbec3cec80852d85ac25133
tannerr12 <tanner.01@hotmail.com>
2023-04-07 15:44:10 -0400

    Time: 1331 ms (5.21%), Space: 23.8 MB (10.90%) - LeetHub

--- 0320-generalized-abbreviation/0320-generalized-abbreviation.py
+++ 0320-generalized-abbreviation/0320-generalized-abbreviation.py
@@ -3,8 +3,6 @@ class Solution:
         
         
         res = set()
-        #mp = {}
-        #taken = set()
         def dfs(i, w):
             nonlocal res
             if i >= len(word):
@@ -17,26 +15,14 @@ class Solution:
                 #make sure range is not an abriviation
                 wrd = word[i:j+1]
                 num = j - i +1
-               
-                dfs(j+1, w + word[i:j+1])
+                
+                dfs(j+1, w + wrd)
                 
                 #convert to number 
                 #can only do this if prev is a letter
                 if not w or w[-1].isalpha():
-                    """
-                    if num in mp:
-                        if wrd == mp[num]:
-                            dfs(j+1, w + str(num))
-                        else:
-                            continue
-                    """    
-                    #mp[num] = wrd
-                    #taken.add(num)
                     dfs(j+1, w + str(num))
-                    #del mp[num]
-                    #taken.remove(num)
-        
-        
+
         dfs(0,'')
         
         return res
