683aaac063bf2aa556d8b3a0683b7f7d009a02f7
tannerr12 <tanner.01@hotmail.com>
2023-04-18 19:27:29 -0400

    Time: 1640 ms (42.86%), Space: 15.9 MB (87.91%) - LeetHub

--- 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
+++ 1072-flip-columns-for-maximum-number-of-equal-rows/1072-flip-columns-for-maximum-number-of-equal-rows.py
@@ -4,6 +4,27 @@ class Solution:
         #create a map of compliments
         mp = defaultdict(int)
         
+        #take this example
+        '''
+        000
+        001
+        110
+        
+        The idea is we will try to make all of the rows 000
+        if it starts with a 0 dont flip anything but if it 
+        starts with a 1 we will want flip everything
+        
+        000 -> 000
+        001 -> 001
+        110 -> 001
+        
+        as we can see here 110 and 001 are exact opposties 
+        in terms of bits which means they fit together when flipped
+        since we turned them both to their 0 compliments they both become 
+        001 so the idea is we want to return the compliment that occurs the most often
+        when since that will give us our best result
+        
+        '''
         
         for r in matrix:
             
