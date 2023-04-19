c1ed1e1a536d54774890aed2fbc68a72dd43a714
tannerr12 <tanner.01@hotmail.com>
2023-04-18 20:20:49 -0400

    Time: 846 ms (89.56%), Space: 28.5 MB (12.62%) - LeetHub

--- 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.py
@@ -1,24 +1,17 @@
 class Solution:
     def corpFlightBookings(self, bookings: List[List[int]], n: int) -> List[int]:
-        
-        arr = []
+        res = [0] * n
+        arr = [0] * (n+1)
         for x,y,z in bookings:
             
-            arr.append([x, z])
-            arr.append([y+1, -z])
+            arr[x-1] += z
+            arr[y] -= z
         
-        
-        arr.sort()
-        
-        res = [0] * n
-        idx = 0
+
         running = 0
         for i in range(1, n + 1):
             
-            while idx < len(arr) and arr[idx][0] <= i:
-                running += arr[idx][1]
-                idx +=1
-            
+            running += arr[i-1]
             res[i-1] = running
         
         return res
