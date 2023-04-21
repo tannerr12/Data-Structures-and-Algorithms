80312dec5168e80efb558fafa3fe9ec32c54c9d0
tannerr12 <tanner.01@hotmail.com>
2023-04-21 17:24:00 -0400

    Time: 764 ms (59.35%), Space: 57 MB (66.67%) - LeetHub

---
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -0,0 +1,26 @@
+class FirstUnique:
+
+    def __init__(self, nums: List[int]):
+        self.count = defaultdict(int)
+        self.q = deque()
+        for val in nums:
+            self.q.append(val)
+            self.count[val] +=1
+        
+
+    def showFirstUnique(self) -> int:
+        while self.q and self.count[self.q[0]] > 1:
+            self.q.popleft()
+        
+        return self.q[0] if self.q else -1
+
+    def add(self, value: int) -> None:
+        
+        self.q.append(value)
+        self.count[value] +=1
+
+
+# Your FirstUnique object will be instantiated and called as such:
+# obj = FirstUnique(nums)
+# param_1 = obj.showFirstUnique()
+# obj.add(value)
