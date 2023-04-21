1db348d77a23cbc504cd492ac53443ce825aebed
tannerr12 <tanner.01@hotmail.com>
2023-04-21 17:35:23 -0400

    Time: 699 ms (100.00%), Space: 56 MB (89.43%) - LeetHub

--- 1429-first-unique-number/1429-first-unique-number.py
+++ 1429-first-unique-number/1429-first-unique-number.py
@@ -1,26 +1,24 @@
 class FirstUnique:
 
     def __init__(self, nums: List[int]):
-        self.count = defaultdict(int)
-        self.q = deque()
+        self.mp = {}
+        self.deleted = set()
         for val in nums:
-            self.q.append(val)
-            self.count[val] +=1
-        
+            self.add(val)
 
     def showFirstUnique(self) -> int:
-        while self.q and self.count[self.q[0]] > 1:
-            self.q.popleft()
         
-        return self.q[0] if self.q else -1
-
+        return next(iter(self.mp)) if self.mp else -1
+    
     def add(self, value: int) -> None:
-        if self.count[value] == 0:
-            self.q.append(value)
-            
-        self.count[value] +=1
-        while self.q and self.count[self.q[0]] > 1:
-            self.q.popleft()
+        
+        if value in self.deleted:
+            return
+        if value in self.mp:
+            del self.mp[value]
+            self.deleted.add(value)
+        else:
+            self.mp[value] = 1
 
 
 # Your FirstUnique object will be instantiated and called as such:
