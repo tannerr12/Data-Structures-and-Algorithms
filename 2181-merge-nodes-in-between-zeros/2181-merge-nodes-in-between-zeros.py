edb283ae9449ded3535fc86274fada91594770cc
tannerr12 <tanner.01@hotmail.com>
2023-04-23 17:30:35 -0400

    Time: 1991 ms (95.41%), Space: 69.7 MB (89.42%) - LeetHub

--- 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
+++ 2181-merge-nodes-in-between-zeros/2181-merge-nodes-in-between-zeros.py
@@ -5,15 +5,16 @@
 #         self.next = next
 class Solution:
     def mergeNodes(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        
-        node = ListNode()
-        dnode = node
+        node = head
+        dnode = head
+        prev = None
         head = head.next   
         running = 0
         while head:
         
             if head.val == 0:
-                node.next = ListNode(running)
+                prev = node
+                node.val = running
                 node = node.next
                 running = 0
             
@@ -22,6 +23,7 @@ class Solution:
             
             head = head.next
         
-        
-        return dnode.next
+        if prev:
+            prev.next = None
+        return dnode
             
