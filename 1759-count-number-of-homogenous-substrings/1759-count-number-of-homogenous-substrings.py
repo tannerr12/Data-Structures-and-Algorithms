f99296daf2732d81ec7b643d1af0daceea228064
tannerr12 <tanner.01@hotmail.com>
2023-04-26 23:45:59 -0400

    Time: 142 ms (65.64%), Space: 17.4 MB (6.13%) - LeetHub

--- 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
+++ 1759-count-number-of-homogenous-substrings/1759-count-number-of-homogenous-substrings.py
@@ -7,29 +7,21 @@ class Solution:
         
         curChar = ''
         size = 0
-        
-        mp = defaultdict(int)
+    
         for i, e in enumerate(s):
             
             if e == curChar:
                 size +=1
             else:
                 if size > 0:
-                    mp[curChar * size] += 1
+                    res += (size * (size + 1)) // 2
+                    res %= MOD
                 curChar = e
                 size = 1
         
-        mp[curChar * size] += 1
         
-        res = 0
+        return (res + (size * (size + 1)) // 2) % MOD
         
-        for key,val in mp.items():
-            res += ((len(key) * (len(key) + 1)) // 2) * val
-            res %= MOD
-        
-        
-        return res % MOD
-        #print(mp)
             
             
         
