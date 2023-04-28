79bd8573e19072802fa7dcf4b4f81dad4b34fc44
tannerr12 <tanner.01@hotmail.com>
2023-04-28 16:55:12 -0400

    Time: 2570 ms (5.06%), Space: 215.1 MB (5.25%) - LeetHub

---
+++ 0524-longest-word-in-dictionary-through-deleting/0524-longest-word-in-dictionary-through-deleting.py
@@ -0,0 +1,65 @@
+class trie:
+    
+    def __init__(self, char):
+        
+        self.char = char
+        self.adj = {}
+
+class Solution:
+    def findLongestWord(self, s: str, dictionary: List[str]) -> str:
+        
+        mp = trie('')
+        group = defaultdict(list)
+        for word in dictionary:
+            group[word[0]].append(word)
+            mpp = mp
+            for w in word:
+                
+                if w in mpp.adj:
+                    mpp = mpp.adj[w]
+                else:
+                    mpp.adj[w] = trie(w)
+                    mpp = mpp.adj[w]
+        
+        
+        pot = defaultdict(list)
+        def dfs(i,w,j,t):
+            nonlocal pot
+            if j >= len(w):
+                pot[len(w)].append(w)
+                return w
+            if i >= len(s):
+                return ''
+            
+            res = ''
+            while i < len(s) and s[i] != w[j]:
+                i+=1
+            
+            if i < len(s):
+                res = max(res,dfs(i+1, w, j+1, t.adj[s[i]]))
+            
+            return res 
+        
+        print(group)
+        
+        res = ''
+        seen = set()
+        for i in range(len(s)):
+            w = s[i]
+            if w in seen:
+                continue
+            seen.add(w)
+            for word in group[w[0]]:
+                res = max(res,dfs(i + 1,word,1,mp.adj[w[0]]))
+        
+        res = '{'
+        #print(max(pot))
+        if pot:
+            for val in pot[max(pot)]:
+                res = min(res, val)
+        
+        return res if res != '{' else ''
+                
+            
+        
+        
