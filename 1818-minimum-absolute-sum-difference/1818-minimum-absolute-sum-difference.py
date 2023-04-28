ea18bd34b31cbd470bb3b367767a43d7d4dd5b9b
tannerr12 <tanner.01@hotmail.com>
2023-04-28 16:17:50 -0400

    Time: 938 ms (85.58%), Space: 32.6 MB (9.77%) - LeetHub

--- 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.py
+++ 1818-minimum-absolute-sum-difference/1818-minimum-absolute-sum-difference.py
@@ -3,27 +3,26 @@ class Solution:
         
         numbers = list(set(nums1))
         numbers.sort()
+        
         MOD= 10 ** 9 + 7
         mx = 0
         total = 0
         for i in range(len(nums1)):
             curr = abs(nums1[i] - nums2[i])
             total += curr
+            total %= MOD
+            
+            val = 0
             if nums1[i] > nums2[i]:
                 val = bisect_left(numbers, nums1[i] - curr)
-                if val < len(numbers):
-                    mx = max(mx,curr - abs(numbers[val] - nums2[i]))
-                if val > 0:
-                    mx = max(mx,curr - abs(numbers[val-1] - nums2[i]))
-            elif nums1[i] != nums2[i]:
+            else:
                 val = bisect_left(numbers, nums1[i] + curr)
-                if val < len(numbers):
-                    mx = max(mx,curr - abs(numbers[val] - nums2[i]))
-                if val > 0:
-                    mx = max(mx,curr - abs(numbers[val-1] - nums2[i]))
+                
+                
+            if val < len(numbers):
+                mx = max(mx,curr - abs(numbers[val] - nums2[i]))
+            if val > 0:
+                mx = max(mx,curr - abs(numbers[val-1] - nums2[i]))
+                
             
-            
-        
-        #print(mx)
-        
         return (total - mx) % MOD 
