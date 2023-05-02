76d747e1e0b2cd25b99e211e0462fef24a5e57bb
tannerr12 <tanner.01@hotmail.com>
2023-05-02 18:34:54 -0400

    Time: 63 ms (6.45%), Space: 16.4 MB (6.45%) - LeetHub

---
+++ 1222-queens-that-can-attack-the-king/1222-queens-that-can-attack-the-king.py
@@ -0,0 +1,28 @@
+class Solution:
+    def queensAttacktheKing(self, queens: List[List[int]], king: List[int]) -> List[List[int]]:
+        
+        
+        #try all 8 directions from the king and exit once we hit a queen
+        
+        result = set()
+        q = set()
+        for i in range(len(queens)):
+            x,y = queens[i]
+            q.add((x,y))
+            
+        directions = [[1,0], [-1,0], [0,1], [0,-1], [-1,-1], [-1,1], [1, -1], [1, 1]]
+        i,j = king[0],king[1]
+        for x,y in directions:
+            
+            nx,ny = i + x, j + y
+            while nx >= 0 and nx < 8 and ny >= 0 and ny < 8:
+                if (nx,ny) in q:
+                    result.add((nx,ny))
+                    break
+                
+                nx += x
+                ny += y
+        
+        
+        return result
+        
