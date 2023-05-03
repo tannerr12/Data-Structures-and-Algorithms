47691275399c62fd3fcaf853ee6155784b30fc40
tannerr12 <tanner.01@hotmail.com>
2023-05-03 17:14:07 -0400

    Time: 57 ms (13.79%), Space: 17.1 MB (11.72%) - LeetHub

---
+++ 0808-soup-servings/0808-soup-servings.py
@@ -0,0 +1,27 @@
+class Solution:
+    def soupServings(self, n: int) -> float:
+        if n >= 5000:
+            return 1
+        
+        @cache
+        def dfs(soup,soup2):
+            
+            if soup ==0 or soup2 ==0:
+                
+                if soup == 0 and soup2:
+                    return 1
+                elif soup == 0 and soup2 == 0:
+                    return 0.5
+                else:
+                    return 0 
+                
+                
+            
+            res = 0.25 * (dfs(soup - min(soup,100),soup2) + dfs(soup - min(soup, 75),soup2 - min(soup2, 25)) + \
+                          dfs(soup - min(soup, 50),soup2 - min(soup2, 50)) + dfs(soup - min(soup,25),soup2 - min(soup2, 75)))
+            
+            
+            return res
+        
+        
+        return dfs(n,n)
