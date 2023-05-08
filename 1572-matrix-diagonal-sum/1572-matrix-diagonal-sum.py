20eb17d7581e493f088669234f29d3ad11c43256
tannerr12 <tanner.01@hotmail.com>
2023-05-07 21:11:25 -0400

    Time: 122 ms (10.41%), Space: 16.5 MB (13.51%) - LeetHub

---
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.py
@@ -0,0 +1,16 @@
+class Solution:
+    def diagonalSum(self, mat: List[List[int]]) -> int:
+        
+        
+        res = 0
+        
+        
+        for i in range(len(mat)):
+            
+            res += mat[i][i]
+            
+            if i != len(mat)-1-i:
+                res += mat[i][len(mat)-1 -i]
+                
+        
+        return res
