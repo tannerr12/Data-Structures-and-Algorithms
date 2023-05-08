4190a059ca6f3512678f5b1fedda816fff26beea
tannerr12 <tanner.01@hotmail.com>
2023-05-07 22:32:52 -0400

    Time: 76 ms (41.42%), Space: 16.6 MB (15.70%) - LeetHub

--- 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
+++ 0311-sparse-matrix-multiplication/0311-sparse-matrix-multiplication.py
@@ -1,21 +1,25 @@
 class Solution:
     def multiply(self, mat1: List[List[int]], mat2: List[List[int]]) -> List[List[int]]:
         
-        
-        #mat1 is the source
         res = [[0 for j in range(len(mat2[0]))] for i in range(len(mat1))]
-        
-        #print(res)
-        
+
         for i in range(len(mat1)):
-            for j in range(len(mat2[0])):
-                
-                total = 0
-                for k in range(len(mat1[0])):
-                    total += mat1[i][k] * mat2[k][j]
-                
-                res[i][j] = total
-                
-        
-        
+            for k in range(len(mat1[0])):
+                # Skip calculation if the mat1 element is zero
+                if mat1[i][k] == 0:
+                    continue
+
+                for j in range(len(mat2[0])):
+                    # Skip calculation if the mat2 element is zero
+                    if mat2[k][j] == 0:
+                        continue
+
+                    res[i][j] += mat1[i][k] * mat2[k][j]
+
         return res
+
+
+
+
+
+
