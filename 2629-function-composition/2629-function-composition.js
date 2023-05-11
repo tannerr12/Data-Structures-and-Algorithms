ce82aebe67c81bfced6510123c3ae51239643bf7
tannerr12 <tanner.01@hotmail.com>
2023-05-11 14:01:21 -0400

    Time: 85 ms (5.80%), Space: 42.9 MB (91.66%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -4,14 +4,11 @@
  */
 var compose = function(functions) {
 	return function(x) {
-        total = x
         for (i = functions.length-1; i >= 0; i --){
-            fn = functions[i]
-            //console.log(fn)
-            total = fn(total)
+            x = functions[i](x)
         }
         
-        return total
+        return x
     }
 };
 
