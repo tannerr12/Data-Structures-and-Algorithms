053e19126d813bc1369cdc0622c09ee89716a281
tannerr12 <tanner.01@hotmail.com>
2023-05-14 01:05:47 -0400

    Time: 2025 ms (74.68%), Space: 25.6 MB (21.52%) - LeetHub

--- 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
+++ 1799-maximize-score-after-n-operations/1799-maximize-score-after-n-operations.py
@@ -13,16 +13,13 @@ class Solution:
             for k in range(len(nums)):
                 if (1 << k) & bitmask > 0:
                     continue
-                for j in range(len(nums)):
-                    if j == k:
-                        continue
+                for j in range(k + 1, len(nums)):
+                    
                     if (1 << j) & bitmask > 0:
                         continue
                     
                     res = max(res, dfs(i+1,bitmask | (1 << k) | (1 << j)) + i * gcd(nums[k], nums[j]))
          
-            
-            
             return res
         
         
