866f0871a5c3c589b4b130e4fb3a6a655af80739
tannerr12 <tanner.01@hotmail.com>
2023-05-14 23:12:13 -0400

    Time: 2164 ms (5.70%), Space: 47.4 MB (5.95%) - LeetHub

---
+++ 2621-sleep/2621-sleep.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} millis
+ */
+async function sleep(millis) {  
+    
+    let t = Date.now()
+    
+    while (Date.now() != t + millis){
+        
+    }
+    
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
