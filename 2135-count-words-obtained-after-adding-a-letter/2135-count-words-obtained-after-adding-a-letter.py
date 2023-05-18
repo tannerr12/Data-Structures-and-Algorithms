abaf7a8a76e81ce12a12ef9a0555ae9b0ededaa6
tannerr12 <tanner.01@hotmail.com>
2023-05-17 20:49:07 -0400

    Time: 1536 ms (28.13%), Space: 36.6 MB (31.25%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -1,9 +1,8 @@
 class Solution:
     def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
         
-         
         mp = set()
-        
+        found = set()
         for i,e in enumerate(startWords):
             mask = 0
             for val in e:
@@ -18,15 +17,19 @@ class Solution:
             for val in e:
                 idx = ord(val) - ord('a')
                 mask |= (1 << idx)
-        
+            
+            if mask in found:
+                res +=1
+                continue
+                
             for i in range(32):
                 if mask & (1 << i) > 0:
-                    m = mask
                     mask ^= (1 << i)
                     if mask in mp:
+                        found.add(mask ^ (1 << i))
                         res += 1
                         break
-                    mask = m
+                    mask ^= (1 << i)
         
         return res
                 
