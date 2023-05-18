e9ea70ecef4fc9d4132fb5506648d89122f17570
tannerr12 <tanner.01@hotmail.com>
2023-05-18 17:27:46 -0400

    Time: 4975 ms (5.04%), Space: 792.7 MB (5.32%) - LeetHub

---
+++ 0313-super-ugly-number/0313-super-ugly-number.py
@@ -0,0 +1,53 @@
+class Solution:
+    def nthSuperUglyNumber(self, n: int, primes: List[int]) -> int:
+        if n ==1:
+            return 1
+        p = set(primes)
+        vals = set()
+        heap = primes.copy()
+        
+        n-=1
+        
+        while n:
+            val = heappop(heap)
+            vals.add(val)
+            n-=1
+            if n ==0:
+                return val
+            
+            for v in primes:
+                if v * val in vals:
+                    continue
+                heappush(heap, val * v)
+                vals.add(val * v)
+                
+            
+            
+        '''
+        for i in range(len(primes)):
+            for j in range(i, len(primes)):
+                vals.add(lcm(primes[i], primes[j]))
+                
+        print(vals)
+        
+        found = 0
+        for i in range(1,(10**6) + 1):
+            num = i
+            for j in range(len(primes)):
+                if num in p:
+                    num = 1
+                    break
+                while num % primes[j] == 0:
+                    num //= primes[j]
+            
+            
+            if num == 1:
+                found +=1
+                if found == n:
+                    return i
+        
+    
+                
+        '''     
+        #print(found)
+                
