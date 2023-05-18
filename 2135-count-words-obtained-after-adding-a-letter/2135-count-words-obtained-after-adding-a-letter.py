4e3e707ad06dccd71efe86f56926627dedd510ff
tannerr12 <tanner.01@hotmail.com>
2023-05-17 20:51:32 -0400

    Time: 1557 ms (27.50%), Space: 36.5 MB (31.25%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -1,8 +1,7 @@
 class Solution:
     def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
-        
+        #create a unique grouping of masks from starting words
         mp = set()
-        found = set()
         for i,e in enumerate(startWords):
             mask = 0
             for val in e:
@@ -10,23 +9,19 @@ class Solution:
                 mask |= (1 << idx)
             mp.add(mask) 
 
-        
+        #create the masks from targetWords but we need to find a match by removing 1 character from targetwords
         res = 0
         for i,e in enumerate(targetWords):
             mask = 0
             for val in e:
                 idx = ord(val) - ord('a')
                 mask |= (1 << idx)
+         
             
-            if mask in found:
-                res +=1
-                continue
-                
-            for i in range(32):
+            for i in range(26):
                 if mask & (1 << i) > 0:
                     mask ^= (1 << i)
                     if mask in mp:
-                        found.add(mask ^ (1 << i))
                         res += 1
                         break
                     mask ^= (1 << i)
