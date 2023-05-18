2147fb4ae5ba15aa7c5263e96269fc2a75e12dee
tannerr12 <tanner.01@hotmail.com>
2023-05-17 20:46:55 -0400

    Time: 1583 ms (26.88%), Space: 36.6 MB (28.75%) - LeetHub

--- 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
+++ 2135-count-words-obtained-after-adding-a-letter/2135-count-words-obtained-after-adding-a-letter.py
@@ -1,24 +1,16 @@
 class Solution:
     def wordCount(self, startWords: List[str], targetWords: List[str]) -> int:
-        #startwords does not contain a letter not in target
-        #startwords is less than = len(target) -1 len
-        #you only get one letter
         
-        #can we group these words by length
-        #bitmasks
-        
-        mp = defaultdict(int)
+         
+        mp = set()
         
         for i,e in enumerate(startWords):
             mask = 0
             for val in e:
                 idx = ord(val) - ord('a')
                 mask |= (1 << idx)
-        
-            mp[mask] += 1
-            
-    
-        #print(mp)
+            mp.add(mask) 
+
         
         res = 0
         for i,e in enumerate(targetWords):
@@ -28,7 +20,6 @@ class Solution:
                 mask |= (1 << idx)
         
             for i in range(32):
-                
                 if mask & (1 << i) > 0:
                     mask ^= (1 << i)
                     if mask in mp:
