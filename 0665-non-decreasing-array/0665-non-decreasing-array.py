61ec3d44fc869106a4e9f2e31351ff7a5011da2c
tannerr12 <tanner.01@hotmail.com>
2023-05-19 21:48:28 -0400

    Time: 186 ms (69.75%), Space: 17.9 MB (12.96%) - LeetHub

--- 0665-non-decreasing-array/0665-non-decreasing-array.py
+++ 0665-non-decreasing-array/0665-non-decreasing-array.py
@@ -1,20 +1,15 @@
 class Solution:
     def checkPossibility(self, nums: List[int]) -> bool:
-        
-        
-        #start = -((10 ** 5) + 1)
-        last = float('-inf')
+ 
         used = False
-        nums = [last] + nums
-        #print(nums)
+        nums = [float('-inf')] + nums
         for i in range(2, len(nums)):
             
             if nums[i] < nums[i-1] and not used:
                 if nums[i-2] <= nums[i]:
                     nums[i-1] = nums[i-2]
                 else:
-                    nums[i] = nums[i-1]
-                
+                    nums[i] = nums[i-1]     
                 used = True
             elif nums[i] < nums[i-1] and used:
                 return False
