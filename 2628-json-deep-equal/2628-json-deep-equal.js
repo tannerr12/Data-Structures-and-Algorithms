c0435a110c62a38a5b90c19622de0be02d56d8c1
tannerr12 <tanner.01@hotmail.com>
2023-05-21 01:25:33 -0400

    Time: 74 ms (70.68%), Space: 47.6 MB (38.20%) - LeetHub

--- 2628-json-deep-equal/2628-json-deep-equal.js
+++ 2628-json-deep-equal/2628-json-deep-equal.js
@@ -4,9 +4,7 @@
  * @return {boolean}
  */
 var areDeeplyEqual = function(o1, o2) {
-    let map1 = new Map();
-    //console.log(typeof o1)
-    //console.log(typeof o2)
+
     if (o1 === o2){
         return true
     }else if (o1 === null || o1 === undefined){
@@ -26,31 +24,30 @@ var areDeeplyEqual = function(o1, o2) {
                     return true
                 }
                 for(i=0;i<o1.length;i++){
-                    console.log(o1)
                     if (!areDeeplyEqual(o1[i], o2[i])){
                         return false
                     }
                 }
                 return true
+            }else{
+                
+                for (const [key, value] of Object.entries(o1)) {
+                    if (key in o2){
+
+                        if (!areDeeplyEqual(value, o2[key])){
+                            return false
+                        }
+                    }else{
+                        return false
+                    }
+                }
+                return true
             }
-            
         }
         
     }else{
         return false
     }
-    //console.log(o1)
-    //console.log(o2)
 
-    for (const [key, value] of Object.entries(o1)) {
-        if (key in o2){
-            
-            if (!areDeeplyEqual(value, o2[key])){
-                return false
-            }
-        }else{
-            return false
-        }
-    }
-    return true
+
 };
