b866acc635c158bf2db079d5fc6749c1d9b1420b
tannerr12 <tanner.01@hotmail.com>
2023-05-20 21:28:11 -0400

    Time: 404 ms (57.58%), Space: 17.4 MB (71.13%) - LeetHub

--- 0934-shortest-bridge/0934-shortest-bridge.py
+++ 0934-shortest-bridge/0934-shortest-bridge.py
@@ -15,14 +15,14 @@ class Solution:
         
         while q:
             c,i,j = heappop(q)
-            if c > 0 and grid[i][j] == 1:
-                return c -1
             for x,y in directions:
                 if i+x < 0 or i+x >= len(grid) or j+y < 0 or j+y >= len(grid[0]) or (i+x,j+y) in seen:
                     continue
                 seen.add((i+x,j+y))
                 if c == 0 and grid[i+x][j+y] == 1:
                     heappush(q, (0,i+x,j+y))
+                elif c > 0 and grid[i+x][j+y] == 1:
+                    return c
                 else:
                     heappush(q, (c + 1,i+x,j+y))
         
