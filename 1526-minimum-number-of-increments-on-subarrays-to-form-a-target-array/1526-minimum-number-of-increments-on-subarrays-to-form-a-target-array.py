6416d1ef5f1cd6137286ce80c076d1f01a0beefd
tannerr12 <tanner.01@hotmail.com>
2023-05-25 23:03:04 -0400

    Time: 889 ms (11.63%), Space: 28.5 MB (5.23%) - LeetHub

--- 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
+++ 1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array/1526-minimum-number-of-increments-on-subarrays-to-form-a-target-array.py
@@ -2,26 +2,20 @@ class Solution:
     def minNumberOperations(self, target: List[int]) -> int:
         
         res = 0
-        mn,mx = float('inf'),target[0]
+        mx = target[0]
         sub = 0
         
-        for i in range(len(target)):
-            v = target[i] - sub
-            if v < 0:
+        for i in range(1,len(target)):
+        
+            if target[i] - sub < 0:
                 #below 0 pay the cost by incrementing it by its abs(val)
-                res += (v * -1) 
+                res += ((target[i] - sub) * -1) 
                 #we can no longer use the extra negative values in our across the board subtraction so we need to remove them
-                sub -= (v * -1)
-                
-            
-            if v > mn:
-                sub += mn
-                res += mx - mn
-                mx,mn = target[i]-sub, float('inf')
-            
-            #update the min and max values for our decreasing subarray (the first value will always )
-            mn = min(mn, target[i]-sub)
-        
-        
+                sub -= ((target[i] - sub) * -1)
+
+            if target[i] - sub > target[i-1]-sub:
+                res += mx - (target[i-1]-sub)
+                sub += target[i-1]-sub
+                mx = target[i]-sub
 
         return res + sub + mx
