5af5705f0d73411b535546e665b25b89c734ce0d
tannerr12 <tanner.01@hotmail.com>
2023-05-26 15:32:38 -0400

    Time: 1494 ms (13.43%), Space: 27.2 MB (22.82%) - LeetHub

--- 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
+++ 1793-maximum-score-of-a-good-subarray/1793-maximum-score-of-a-good-subarray.py
@@ -2,19 +2,8 @@ class Solution:
     def maximumScore(self, nums: List[int], k: int) -> int:
         
         stack = []
-        left = [0] * len(nums)
         right = [len(nums)-1] * len(nums)
-        for i in range(len(nums)):
-        
-            while stack and nums[stack[-1]] >= nums[i]:
-                stack.pop()
-                
-            if stack:
-                left[i] = stack[-1] + 1 
-                
-            stack.append(i)
-        
-        
+
         stack = []
         for i in range(len(nums)-1,-1,-1):
             
@@ -28,9 +17,18 @@ class Solution:
         
         
         res = 0
+        stack = []
         for i in range(len(nums)):
             
-            if left[i] <= k and right[i] >= k:
-                res = max(res, nums[i] * (right[i] - left[i] +1))
+            while stack and nums[stack[-1]] >= nums[i]:
+                stack.pop()
+                
+            val = 0   
+            if stack:
+                val = stack[-1] + 1 
+                
+            stack.append(i)
+            if val <= k and right[i] >= k:
+                res = max(res, nums[i] * (right[i] - val +1))
         
         return res
