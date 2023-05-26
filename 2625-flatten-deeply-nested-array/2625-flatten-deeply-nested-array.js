46565ea9606527bc1596baac125c34e4c7552741
tannerr12 <tanner.01@hotmail.com>
2023-05-25 21:04:33 -0400

    Time: 107 ms (91.19%), Space: 61.1 MB (84.82%) - LeetHub

--- 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
+++ 2625-flatten-deeply-nested-array/2625-flatten-deeply-nested-array.js
@@ -7,10 +7,7 @@ var flat = function (arr, n) {
     let res = []
     let mx = n
     function dfs(arr, n){
-        if (n > mx){
-            return
-        }
-        
+
         for (let j = 0; j < arr.length; j++){
             if (typeof arr[j] == 'object' && n + 1 <= mx){
                 dfs(arr[j], n + 1)
