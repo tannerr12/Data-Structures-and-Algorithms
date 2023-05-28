eb722dc41ecf1d32d4eff041a4765cc4438c8476
tannerr12 <tanner.01@hotmail.com>
2023-05-28 12:18:55 -0400

    Time: 61 ms (61.19%), Space: 16.5 MB (11.42%) - LeetHub

--- 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
+++ 1255-maximum-score-words-formed-by-letters/1255-maximum-score-words-formed-by-letters.py
@@ -2,44 +2,38 @@ class Solution:
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
         
         count = Counter(letters)
-        wordls = defaultdict(dict)
+        wordls = {}
         
         for word in words:
             wordls[word] = Counter(word)
         
-       
-        def dfs(i):
-            nonlocal count
+  
+        def dfs(i,count):
+   
             if i >= len(words):
                 return 0
             
             res = 0
             
             #dont do word
-            res = max(res, dfs(i+1))
-            
-            c = count
+            res = max(res, dfs(i+1,count))
+
             process = True
             sco = 0
             
             for key,val in wordls[words[i]].items():
+                sco += val * score[ord(key) - ord('a')]
                 if count[key] < val:
                     process = False
                     break
             
             if process:
-                for key,val in wordls[words[i]].items():
-                    count[key] -= val
-                    sco += score[ord(key) - ord('a')] * val
-
-                res = max(res, dfs(i+1) + sco)
+     
+                res = max(res, dfs(i+1,count - wordls[words[i]]) + sco)
                 
-                for key,val in wordls[words[i]].items():
-                    count[key] += val  
-
-
+              
             
             return res
         
         
-        return dfs(0)
+        return dfs(0,count)
