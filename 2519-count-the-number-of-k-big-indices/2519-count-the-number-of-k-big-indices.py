d953a7de308d2c1e9b019075f80924b5632c38e1
tannerr12 <tanner.01@hotmail.com>
2023-05-29 15:31:57 -0400

    Time: 590 ms (84.00%), Space: 25.9 MB (34.00%) - LeetHub

--- 2519-count-the-number-of-k-big-indices/2519-count-the-number-of-k-big-indices.py
+++ 2519-count-the-number-of-k-big-indices/2519-count-the-number-of-k-big-indices.py
@@ -1,6 +1,7 @@
 from sortedcontainers import SortedList
 class Solution:
     def kBigIndices(self, nums: List[int], k: int) -> int:
+        '''
         res = 0
         
         for i,v in enumerate(nums):
@@ -29,3 +30,36 @@ class Solution:
             last = v
             
         return res
+    
+        '''
+        
+        heap = []
+        left = [0] * len(nums)
+        res = 0
+        
+        for i,v in enumerate(nums):
+            
+            if len(heap) == k and -heap[0] < v:
+                left[i] = True
+            
+            heappush(heap, -v)
+            
+            if len(heap) > k:
+                heappop(heap)
+                
+        
+        heap = []
+        
+        for i in range(len(nums)-1,-1,-1):
+            
+            if left[i] and len(heap) == k and -heap[0] < nums[i]:
+                res +=1
+            
+            heappush(heap, -nums[i])
+            
+            if len(heap) > k:
+                heappop(heap)
+        
+        
+        return res
+        
