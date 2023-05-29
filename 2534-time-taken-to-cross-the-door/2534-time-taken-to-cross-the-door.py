aa740106150b97c43f47b1f5ec85ee2339170028
tannerr12 <tanner.01@hotmail.com>
2023-05-29 12:57:59 -0400

    Time: 887 ms (37.81%), Space: 39.8 MB (31.71%) - LeetHub

--- 2534-time-taken-to-cross-the-door/2534-time-taken-to-cross-the-door.py
+++ 2534-time-taken-to-cross-the-door/2534-time-taken-to-cross-the-door.py
@@ -1,7 +1,7 @@
 class Solution:
     def timeTaken(self, arrival: List[int], state: List[int]) -> List[int]:
-        enter = []
-        exit = []
+        enter = deque()
+        exit = deque()
         
         time = arrival[0]
         prevt, prevType = -2,-2
@@ -15,37 +15,37 @@ class Solution:
             prevt = time
             while i < len(arrival) and arrival[i] == time:
                 if state[i] == 1:
-                    heappush(exit, (arrival[i],i))
+                    exit.append((arrival[i],i))
                 else:
-                    heappush(enter, (arrival[i],i))
+                    enter.append((arrival[i],i))
                 
                 i +=1
             
             
             if useType:
                 if prevType == 1 and exit:
-                        val,j = heappop(exit)
+                        val,j = exit.popleft()
                         res[j] = time
                         prevType = 1
                 elif prevType == 0 and enter:
-                        val,j = heappop(enter)
+                        val,j = enter.popleft()
                         res[j] = time
                         prevType = 0
                 elif exit:
-                        val,j = heappop(exit)
+                        val,j = exit.popleft()
                         res[j] = time
                         prevType = 1
                 elif enter:
-                        val,j = heappop(enter)
+                        val,j = enter.popleft()
                         res[j] = time
                         prevType = 0
             else:
                 if exit:
-                        val,j = heappop(exit)
+                        val,j = exit.popleft()
                         res[j] = time
                         prevType = 1
                 elif enter:
-                        val,j = heappop(enter)
+                        val,j = enter.popleft()
                         res[j] = time
                         prevType = 0
                 
