b6b15e47343e421c88285ef8aa1e162f7b2cee34
tannerr12 <tanner.01@hotmail.com>
2023-05-30 15:55:51 -0400

    Time: 320 ms (8.63%), Space: 18 MB (16.31%) - LeetHub

---
+++ 1231-divide-chocolate/1231-divide-chocolate.py
@@ -0,0 +1,37 @@
+class Solution:
+    def maximizeSweetness(self, sweetness: List[int], k: int) -> int:
+        
+        def isGood(val):
+            groups = k+1
+            total = 0
+            for i in range(len(sweetness)):
+                total += sweetness[i]
+                if total >= val:
+                    total = 0
+                    groups -=1
+                
+                
+            if groups <= 0:
+                return True
+            return False
+        
+        
+        l,r = min(sweetness), sum(sweetness)
+        res = 0
+        while l <= r:
+            
+            mid = (l+r) //2
+            
+            if isGood(mid):
+                res = mid
+                l = mid + 1
+                
+            else:
+                r = mid -1
+        
+        
+        return res
+                
+            
+        
+        
