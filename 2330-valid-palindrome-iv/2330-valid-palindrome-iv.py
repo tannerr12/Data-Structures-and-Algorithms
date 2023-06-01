d679189485438ec04c449d7bddafc58d41cf7f00
tannerr12 <tanner.01@hotmail.com>
2023-05-31 23:13:36 -0400

    Time: 81 ms (5.06%), Space: 17 MB (81.01%) - LeetHub

---
+++ 2330-valid-palindrome-iv/2330-valid-palindrome-iv.py
@@ -0,0 +1,10 @@
+class Solution:
+    def makePalindrome(self, s: str) -> bool:
+        
+        count = 0
+        for i in range(len(s) // 2):
+            if s[i] != s[-(i + 1)]:
+                count +=1
+        
+        
+        return count <= 2
