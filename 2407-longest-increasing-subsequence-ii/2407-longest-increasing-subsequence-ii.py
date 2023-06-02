48619513cc10a43cf8cd3a29de21259fa52277ac
tannerr12 <tanner.01@hotmail.com>
2023-06-01 21:42:29 -0400

    Time: 3006 ms (48.54%), Space: 26.4 MB (77.67%) - LeetHub

--- 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.py
+++ 2407-longest-increasing-subsequence-ii/2407-longest-increasing-subsequence-ii.py
@@ -1,7 +1,7 @@
 class SegmentTree:
     def __init__(self,array):
         self.n = len(array)
-        self.max_tree = [0] * (2 * self.n)
+        self.max_tree = [0] * (self.n << 1)
         self.build_tree(array)
     
     def build_tree(self,array):
@@ -12,14 +12,14 @@ class SegmentTree:
         #set the first half of the tree which is our calculations/internal
         #children are i * 2 and i*2 + 1 and parent is i // 2
         for i in range(self.n-1,0,-1):
-            self.max_tree[i] = max(self.max_tree[i*2], self.max_tree[i*2+1])
+            self.max_tree[i] = max(self.max_tree[i << 1], self.max_tree[(i << 1)+1])
     
     def update(self,p,value):
         self.max_tree[p+self.n] = value
         p += self.n
         #go up each parent until we hit the root
         while p > 1:
-            p = p // 2
+            p = p >> 1
             #go back up the tree and recalculate max
             self.max_tree[p] = max(self.max_tree[p*2], self.max_tree[p*2+1])
     def query_max(self,l,r):
@@ -28,16 +28,16 @@ class SegmentTree:
         
         max_val = float('-inf')
         while l < r:
-            if l % 2:
+            if l & 1:
                 max_val = max(max_val, self.max_tree[l])
                 l += 1
-            if r % 2:
+            if r & 1:
                 r -= 1
                 max_val = max(max_val, self.max_tree[r])
             
             #go to parent
-            l = l//2
-            r = r//2
+            l = l >> 1
+            r = r >> 1
         
         return max_val
             
