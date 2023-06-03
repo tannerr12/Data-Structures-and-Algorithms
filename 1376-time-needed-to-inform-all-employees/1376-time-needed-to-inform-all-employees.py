b2d8ae6bf13848a0994351c5512069ffc2579a5d
tannerr12 <tanner.01@hotmail.com>
2023-06-02 20:58:00 -0400

    Time: 1437 ms (27.74%), Space: 54.3 MB (22.56%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,33 +1,17 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
         adj = defaultdict(list)
-        
         for i in range(len(manager)):
-            
             if manager[i] == -1:
                 continue
-            
             adj[manager[i]].append([i,informTime[manager[i]]])   
 
-            
-        #print(adj)
-        q = deque()
-        q.append([headID,0])
-        seen = set()
-        seen.add(headID)
+        q = deque([[headID,0]])
         res = 0
         while q:
-            
             for i in range(len(q)):
-                
                 idx, cost = q.popleft()
                 res = max(res, cost)
-                seen.add(idx)
-                
                 for x,c in adj[idx]:
-                    
-                    if x in seen:
-                        continue
-                    
                     q.append([x, c + cost])
         return res
