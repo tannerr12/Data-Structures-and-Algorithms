dcb4522f9e773d99ef99f7e7eb346b5e3d788a7f
tannerr12 <tanner.01@hotmail.com>
2023-06-02 21:07:56 -0400

    Time: 1705 ms (6.51%), Space: 55.1 MB (21.71%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.py
@@ -1,5 +1,6 @@
 class Solution:
     def numOfMinutes(self, n: int, headID: int, manager: List[int], informTime: List[int]) -> int:
+        '''
         adj = defaultdict(list)
         for i in range(len(manager)):
             if manager[i] == -1:
@@ -15,3 +16,23 @@ class Solution:
                 for x,c in adj[idx]:
                     q.append([x, c + cost])
         return res
+        '''
+        adj = defaultdict(list)
+        for i in range(len(manager)):
+            if manager[i] == -1:
+                continue
+            adj[manager[i]].append([i,informTime[i]])   
+
+        heap = [[-informTime[headID], headID]]
+        res = float('inf')
+        while heap:
+            cost,node = heappop(heap)
+            res= min(res, cost)
+            for x,c in adj[node]:
+                heappush(heap, [cost - c, x])
+        
+        return -res
+            
+            
+            
+            
