82f6f50eaa9dd624228ab816870c1fc9265a0084
tannerr12 <tanner.01@hotmail.com>
2023-06-13 10:21:59 -0400

    Time: 5950 ms (5.06%), Space: 23.5 MB (20.24%) - LeetHub

---
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
@@ -0,0 +1,50 @@
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        m = 2 ** len(graph)
+        m -= 1
+        print(m)
+        dp = {}
+        '''
+        def dfs(i,mask):
+            nonlocal m
+            
+            if mask == m:
+                return 0
+            
+            if (i,mask) in dp:
+                return dp[(i,mask)]
+            if i == None:
+                return float('inf')
+            
+            res = float('inf')
+            for x in graph[i]:
+                res = min(res, dfs(x, mask | (1 << x)) + 1)
+            
+            dp[(i,mask)] = res            
+            return res
+        
+        res = float('inf')
+        for i in range(len(graph)):
+            res = min(res, dfs(i, 0 | (1 << i)))
+        '''
+        res = float('inf')
+        
+        for node in range(len(graph)):
+            heap = [[0,node,0 | (1 << node)]]
+            dp = {}
+            while heap:
+
+                cost, n, mask = heappop(heap)
+                if mask == m:
+                    res = min(res,cost)
+                    break
+                if (n,mask) in dp:
+                    continue
+                
+                dp[(n,mask)] = True
+                
+                for val in graph[n]:
+                    heappush(heap, [cost + 1, val, mask | (1 << val)])
+        
+        
+        return res
