b66c4029be9636155d609b0545d9b09fca94b5a4
tannerr12 <tanner.01@hotmail.com>
2023-06-13 12:11:46 -0400

    Time: 1733 ms (9.53%), Space: 22.8 MB (22.02%) - LeetHub

--- 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
+++ 0847-shortest-path-visiting-all-nodes/0847-shortest-path-visiting-all-nodes.py
@@ -2,40 +2,35 @@ class Solution:
     def shortestPathLength(self, graph: List[List[int]]) -> int:
         m = 2 ** len(graph)
         m -= 1
-        #print(m)
-        seen = set()
-        N = len(graph)
-        dist = [[float('inf')] * N for _ in range(N)]
-        
-        for i, edges in enumerate(graph):
-            dist[i][i] = 0
-            for edge in edges:
-                dist[i][edge] = 1
-        
-        for k in range(N):
-            for i in range(N):
-                for j in range(N):
-                    dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j])
-                    
-        @cache
-        def visit(node,mask):
-            nonlocal m
-            if mask == m:
-                return 0
-            
-            res = float('inf')
-            for i in range(N):
-                
-                if mask & (1 << i) == 0:
-                    res = min(res, visit(i, (1 << i) | mask) + dist[node][i])
-            
-            return res
-        
-        
         res = float('inf')
+        for node in range(len(graph)):
+            
+            q = deque([[node,(1 << node)]])
+            dp = {}
+            level = 0
+            while q:
+                
+                for i in range(len(q)):
+                    
+                    n, mask = q.popleft()
+                    
+                    if (n,mask) in dp:
+                        continue
+                    
+                    if mask == m:
+                        dp[(n,mask)] = level
+                        res = min(res, level)
+                        continue
+                    
+                    dp[(n,mask)] = level
+                    
+                    for val in graph[n]:
+                        
+                        q.append((val, mask | (1 << val)))
+                
+                level +=1
         
-        for i in range(N):
-            res = min(res, visit(i,(1 << i)))
-        
-                      
         return res
+            
+            
+            
