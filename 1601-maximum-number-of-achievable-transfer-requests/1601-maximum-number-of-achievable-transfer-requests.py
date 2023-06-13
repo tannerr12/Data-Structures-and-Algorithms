9e5d6f5330ea413f72176c0635caa9b8fdc4a2ee
tannerr12 <tanner.01@hotmail.com>
2023-06-13 17:50:47 -0400

    Time: 2515 ms (24.72%), Space: 16.5 MB (43.01%) - LeetHub

--- 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
+++ 1601-maximum-number-of-achievable-transfer-requests/1601-maximum-number-of-achievable-transfer-requests.py
@@ -1,51 +1,36 @@
 class Solution:
     def maximumRequests(self, n: int, requests: List[List[int]]) -> int:
-        
+
         people = defaultdict(int)
-        #requests.sort()
         def dfs(i):
-            
+
             if i >= len(requests):
-                
-                if len(people) == 0:
+                if all(v == 0 for v in people.values()):
                     return 0
                 return float('-inf')
-            
-            res = float('-inf')
-            #ignore request
-            res = max(res, dfs(i+1))
-            
-            #take request
 
+            res = float('-inf')
+            # ignore request
+            res = max(res, dfs(i+1))
+
+            # take request
             x,y = requests[i]
             prevx = people[x]
             prevy = people[y]
-            
-            people[x] -=1
-            people[y] +=1
-            
-            if x in people and people[x] == 0:
+
+            people[x] -= 1
+            people[y] += 1
+
+            if people[x] == 0:
                 del people[x]
-            if y in people and people[y] == 0:
+            if people[y] == 0:
                 del people[y]
+                
             res = max(res, dfs(i+1) + 1)
-            
+
             people[x] = prevx
             people[y] = prevy
-            
-            if x in people and people[x] == 0:
-                del people[x]
-            if y in people and people[y] == 0:
-                del people[y]
-            
+
             return res
-            
-        
-        res = dfs(0)
-        
-        if res == float('-inf'):
-            return 0
-        else:
-            return res
-            
-            
+
+        return dfs(0)
