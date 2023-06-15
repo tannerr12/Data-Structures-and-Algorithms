2014695a198bdc7687a0fe129bf8a537e5a523ba
tannerr12 <tanner.01@hotmail.com>
2023-06-15 15:02:54 -0400

    Time: 592 ms (48.89%), Space: 118.3 MB (10.37%) - LeetHub

--- 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
+++ 1125-smallest-sufficient-team/1125-smallest-sufficient-team.py
@@ -1,40 +1,58 @@
 class Solution:
     def smallestSufficientTeam(self, req_skills: List[str], people: List[List[str]]) -> List[int]:
         
-        n = len(people)
-        m = len(req_skills)
-        skill_id = dict()
-        for i, skill in enumerate(req_skills):
-            skill_id[skill] = i
-        skills_mask_of_person = [0] * n
-        for i in range(n):
-            for skill in people[i]:
-                skills_mask_of_person[i] |= 1 << skill_id[skill]
-        dp = [-1] * (1 << m)
-        dp[0] = 0
-
-        
-        
-        def dfs(m):
-            if dp[m] != -1:
-                return dp[m]
+        arr = []
+        skillmap = {}
+        for i in range(len(req_skills)):
+            skillmap[req_skills[i]] = i
             
-            for i in range(len(people)):
-                nm = m & ~skills_mask_of_person[i]
-                if nm != m:
-                    pm = dfs(nm) |  (1 << i)
-                    if (dp[m] == -1 or
-                        pm.bit_count()
-                       < dp[m].bit_count()):
-                        dp[m] = pm
-                    
-            return dp[m]
-        res = dfs((1 << m) -1)
-        ans = []
+        
         for i in range(len(people)):
+            mask = 0
+            for skill in people[i]:
+                mask |= (1 << skillmap[skill])
+            
+            arr.append(mask)
+        
+        
+        bitmask = 2 ** len(req_skills)
+        bitmask -= 1
+        bitPeople = 2 ** len(people) 
+        bitPeople -=1
+
+        memo = {}
+        @cache
+        def dfs(i,m):
+            nonlocal bitmask, bitPeople
+            if i >= len(arr) or m == bitmask:
+                if m == bitmask:
+                    return 0
+                
+                return bitPeople
+            
+
+            res = bitPeople
+            
+            #dont take
+            val = dfs(i+1, m)
+            if val.bit_count() < res.bit_count():
+                res = val
+            
+            if arr[i] > 0 and arr[i] & m != arr[i]:
+                val = dfs(i+1, m | arr[i]) | (1 << i)
+                if val.bit_count() < res.bit_count():
+                    res = val
+                    
+           
+            return res
+        
+        res = dfs(0,0)
+        ans = []
+ 
+        for i in range(60):
             if res & (1 << i) > 0:
                 ans.append(i)
-                
+        
         return ans
             
             
