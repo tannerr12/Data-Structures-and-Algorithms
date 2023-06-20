bfb199ce6dc1ed032737ec43002a0e1b15321a65
tannerr12 <tanner.01@hotmail.com>
2023-06-20 13:13:02 -0400

    Time: 101 ms (68.24%), Space: 16.6 MB (50.68%) - LeetHub

--- 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
+++ 1754-largest-merge-of-two-strings/1754-largest-merge-of-two-strings.py
@@ -7,38 +7,16 @@ class Solution:
       
         while word1 or word2:
 
-            if not word2 or (word1 and word1[0] > word2[0]):
+            if word1 >= word2:
         
                 merge.append(word1[0])
                 word1 = word1[1:]
 
-            elif not word1 or (word2 and word2[0] > word1[0]):
+            elif word2 > word1:
             
                 merge.append(word2[0])
                 word2 = word2[1:]
-            
-            else:
-                idx = 0
-                while idx < len(word1) and idx < len(word2) and word1[idx] == word2[idx]:
-                    idx+=1
-                
-                if idx >= len(word1) and idx >= len(word2):
-                    merge.append(word1[0])
-                    word1 = word1[1:]
-          
-                elif idx >= len(word1):
-                    merge.append(word2[0])
-                    word2 = word2[1:]
-                    
-                elif idx >= len(word2):
-                    merge.append(word1[0])
-                    word1 = word1[1:]
-                elif word1[idx] > word2[idx]:
-                    merge.append(word1[0])
-                    word1 = word1[1:]
-                else:
-                    merge.append(word2[0])
-                    word2 = word2[1:]
+
         
 
                 
