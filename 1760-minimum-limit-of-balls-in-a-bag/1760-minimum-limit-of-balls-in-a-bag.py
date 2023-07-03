68e56fdc0b69d82356c801a67a42669ed1966929
tannerr12 <tanner.01@hotmail.com>
2023-07-03 14:20:12 -0400

    Time: 3063 ms (5.15%), Space: 29.6 MB (10.86%) - LeetHub

---
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.py
@@ -0,0 +1,32 @@
+class Solution:
+    def minimumSize(self, nums: List[int], maxOperations: int) -> int:
+        nums.sort(reverse=True)
+        
+        def isGood(mid):
+            op = maxOperations
+            for i in range(len(nums)):
+                if nums[i] > mid:
+                    opneeded = math.ceil(nums[i] / mid) - 1
+                    op -= opneeded
+                    if op < 0:
+                        return False
+            
+            return True
+            
+        
+        l,r = 1,max(nums)
+        res = float('inf')
+        while l <= r:
+            
+            mid = (l+r) // 2
+            
+            if isGood(mid):
+                res = mid
+                r = mid -1
+            else:
+                l = mid + 1
+                
+        
+        return res
+                
+            
