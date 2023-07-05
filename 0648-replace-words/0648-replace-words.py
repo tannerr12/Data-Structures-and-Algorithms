45c8aa9e87b0b677d920ddef5dd79237bec14d4c
tannerr12 <tanner.01@hotmail.com>
2023-07-05 15:53:47 -0400

    Time: 162 ms (70.94%), Space: 38.8 MB (14.94%) - LeetHub

---
+++ 0648-replace-words/0648-replace-words.py
@@ -0,0 +1,53 @@
+class Trie:
+    
+    def __init__(self,char):
+        
+        self.char = char
+        self.adj = {}
+        self.EOW = False
+    
+class Solution:
+    def replaceWords(self, dictionary: List[str], sentence: str) -> str:
+        
+        tri = Trie('')
+        
+        for word in dictionary:
+            t = tri      
+            for char in word:
+                if char in t.adj:
+                    t = t.adj[char]
+                else:
+                    t.adj[char] = Trie(char)
+                    t = t.adj[char]
+            
+            t.EOW = True
+        
+        
+        res = []
+        
+        words = sentence.split(' ')
+        
+        
+        print(words)
+    
+        res = []
+        for word in words:
+            t = tri        
+            found = False
+            for i in range(len(word)):
+                if t.EOW == True:
+                    res.append(word[:i])
+                    found = True
+                    break
+                if word[i] in t.adj:
+                    t = t.adj[word[i]]
+                else:
+                    break
+            
+            if not found:
+                res.append(word)
+        
+        return ' '.join(res)
+                
+        
+                
