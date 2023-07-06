05de85970e368486aef72d172b0f08f7f4ccd2c8
tannerr12 <tanner.01@hotmail.com>
2023-07-06 17:02:44 -0400

    Time: 303 ms (35.70%), Space: 23.7 MB (63.26%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.py
@@ -0,0 +1,50 @@
+class Trie:
+    
+    def __init__(self,val):
+        
+        self.val = val
+        self.adj = {}
+        self.EOP = False
+        self.res = -1
+
+class FileSystem:
+
+    def __init__(self):
+        self.tri = Trie('')
+
+    def createPath(self, path: str, value: int) -> bool:
+        t = self.tri
+        p = path.split('/')[1:]
+        res = False
+        for i in range(len(p)):
+            if p[i] in t.adj:
+                t = t.adj[p[i]]
+            else:
+                if i == len(p)-1:   
+                    t.adj[p[i]] = Trie(p[i])
+                    t = t.adj[p[i]]
+                    res = True
+                else:
+                    return False
+        
+        if res:
+            t.res = value
+            t.EOP = True
+        return res
+        
+
+    def get(self, path: str) -> int:
+        t = self.tri
+        p = path.split('/')[1:]
+        for i in range(len(p)):
+            if p[i] in t.adj:
+                t = t.adj[p[i]]
+            else:
+                return -1
+        
+        return t.res
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
