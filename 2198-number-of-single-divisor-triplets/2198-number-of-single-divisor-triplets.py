22ad8dbae3d346add2494d70ff5f5df312fc4a37
tannerr12 <tanner.01@hotmail.com>
2023-07-24 15:06:30 -0400

    Time: 1853 ms (37.04%), Space: 20.1 MB (14.81%) - LeetHub

--- 2198-number-of-single-divisor-triplets/2198-number-of-single-divisor-triplets.py
+++ 2198-number-of-single-divisor-triplets/2198-number-of-single-divisor-triplets.py
@@ -5,18 +5,7 @@ class Solution:
         c = Counter(nums)
         ls = list(c)
         res = 0
-        
-        def calc_permutations(count_i, count_j, count_k):
-            # all numbers are distinct
-            if count_i == count_j == count_k == 1:
-                return 6  # 3! permutations
-            # two numbers are the same
-            elif count_i == 2 or count_j == 2 or count_k == 2:
-                return 3  # 3 permutations
-            # all numbers are the same
-            elif count_i == 3:
-                return 1  # 1 permutation
-            
+
             
         for i in range(len(ls)):
             for j in range(i, len(ls)):
@@ -26,33 +15,14 @@ class Solution:
                         continue
 
                     val = ls[i] + ls[j] + ls[k]
-                    if val % ls[i] == 0 and val % ls[j] != 0 and val % ls[k] != 0:
+                    if (val % ls[i] == 0 and val % ls[j] != 0 and val % ls[k] != 0) or (val % ls[i] != 0 and val % ls[j] == 0 and val % ls[k] != 0) or (val % ls[i] != 0 and val % ls[j] != 0 and val % ls[k] == 0):
                         if ls[j] == ls[k]:
+                            res += 3 * (c[ls[j]]) * (c[ls[j]]-1) * c[ls[i]]
+                        elif ls[i] == ls[j]:
+                            res += 3 * (c[ls[i]]) * (c[ls[i]]-1) * c[ls[k]]
+                        else:
+                            res += 6 * (c[ls[i]] * c[ls[j]] * c[ls[k]]) 
 
-                            res += 3 * (c[ls[j]]) * (c[ls[j]]-1) * c[ls[i]]
-                        elif ls[i] == ls[j]:
-   
-                            res += 3 * (c[ls[i]]) * (c[ls[i]]-1) * c[ls[k]]
-                        else:
-                            res += 6 * (c[ls[i]] * c[ls[j]] * c[ls[k]]) 
-                    if val % ls[i] != 0 and val % ls[j] == 0 and val % ls[k] != 0:
-                        if ls[j] == ls[k]:
-        
-                            res += 3 * (c[ls[j]]) * (c[ls[j]]-1) * c[ls[i]]
-                        elif ls[i] == ls[j]:
-
-                            res += 3 * (c[ls[i]]) * (c[ls[i]]-1) * c[ls[k]]
-                        else:
-                            res += 6 * (c[ls[i]] * c[ls[j]] * c[ls[k]]) 
-                    if val % ls[i] != 0 and val % ls[j] != 0 and val % ls[k] == 0:
-                        if ls[j] == ls[k]:
-     
-                            res += 3 * (c[ls[j]]) * (c[ls[j]]-1) * c[ls[i]]
-                        elif ls[i] == ls[j]:
-   
-                            res += 3 * (c[ls[i]]) * (c[ls[i]]-1) * c[ls[k]]
-                        else:
-                            res += 6 * (c[ls[i]] * c[ls[j]] * c[ls[k]]) 
         return res
                     
                     
