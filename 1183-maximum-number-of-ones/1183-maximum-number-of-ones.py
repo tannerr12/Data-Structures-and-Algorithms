485c5d3e79edd91c7be3874113be2d1ee5a3e1a8
tannerr12 <tanner.01@hotmail.com>
2023-07-25 19:36:37 -0400

    Time: 175 ms (6.52%), Space: 17.3 MB (10.87%) - LeetHub

---
+++ 1183-maximum-number-of-ones/1183-maximum-number-of-ones.py
@@ -0,0 +1,19 @@
+class Solution:
+    def maximumNumberOfOnes(self, width: int, height: int, sideLength: int, maxOnes: int) -> int:
+        
+        mp = defaultdict(int)
+        for i in range(height):
+            for j in range(width):
+                
+                mp[str(i % sideLength) + ',' + str(j % sideLength)] += 1
+                
+                
+        
+        #print(mp)
+        
+        ls = sorted(mp.values(), reverse=True)
+        res = 0
+        for i in range(maxOnes):
+            res += ls[i]
+            
+        return res
