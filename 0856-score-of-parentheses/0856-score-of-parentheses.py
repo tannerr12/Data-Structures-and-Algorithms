42b838abca74f3ef703a8d8d8ce06b2c084436e3
tannerr12 <tanner.01@hotmail.com>
2023-07-28 11:18:42 -0400

    Time: 36 ms (96.45%), Space: 16.2 MB (72.66%) - LeetHub

--- 0856-score-of-parentheses/0856-score-of-parentheses.py
+++ 0856-score-of-parentheses/0856-score-of-parentheses.py
@@ -1,25 +1,15 @@
 class Solution:
     def scoreOfParentheses(self, s: str) -> int:
         
-        def dfs(k,j):
+        stack = [0]
+        
+        for x in s:
             
-            stack = []
-            res = 0
-            for i in range(k,j):
+            if x == '(':
+                stack.append(0)
+            else:
+                val = stack.pop()
+                stack[-1] += max(2 * val, 1)
+        
+        return stack.pop()
                 
-                if s[i] == '(':
-                    stack.append(i)
-                
-                else:
-                    idx = stack.pop()
-                    if len(stack) == 0:
-                        if i-idx-1 > 0:
-                            res += 2 * dfs(idx+1,i)      
-                        else:
-                            res += 1
-                        
-                
-            return res    
-            
-            
-        return dfs(0,len(s))
