ec4ad39e109be95d7f2e848d878be3ef7854a621
tannerr12 <tanner.01@hotmail.com>
2023-07-28 16:03:04 -0400

    Time: 1241 ms (22.88%), Space: 42.9 MB (18.64%) - LeetHub

--- 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
+++ 1411-number-of-ways-to-paint-n-3-grid/1411-number-of-ways-to-paint-n-3-grid.py
@@ -2,27 +2,34 @@ class Solution:
     def numOfWays(self, n: int) -> int:
         #print(len(list(itertools.permutations(['r','r','r', 'y','y','y','g','g','g'], 3))))
         MOD = 10 ** 9 + 7
+        colors = []
+        
+        def buildColors(curr):
+            
+            if len(curr) == 3:
+                colors.append(curr)
+                return
+            for val in 'rgy':
+                if len(curr) == 0 or curr[-1] != val:
+                    buildColors(curr + val)
+            
         
         @cache
-        def dfs(i,prev,last):
+        def dfs(i,prev):
             
             if i > n:
                 return 1
             
             res = 0
             #add layer
-            if len(last) == 3:
-                res += dfs(i+1,last,'')
-            else:
-                for val in 'rgy':
-                    if last and val == last[-1]:
-                        continue
-                    if val == prev[len(last)]:
-                        continue
-                    res += dfs(i,prev,last + val)
-                    
+            #create map of colors
+            for val in colors:
+                if prev[0] != val[0] and prev[1] != val[1] and prev[2] != val[2]:    
+                    res += dfs(i+1,val)
             res %= MOD
             return res
         
-        return dfs(1,'000','')
+        
+        buildColors('')
+        return dfs(1,'000')
             
