ba3901a98bb2508cf039b4e6760d907ddaf27823
tannerr12 <tanner.01@hotmail.com>
2023-07-29 16:47:53 -0400

    Time: 407 ms (33.69%), Space: 21.8 MB (29.89%) - LeetHub

--- 1263-minimum-moves-to-move-a-box-to-their-target-location/1263-minimum-moves-to-move-a-box-to-their-target-location.py
+++ 1263-minimum-moves-to-move-a-box-to-their-target-location/1263-minimum-moves-to-move-a-box-to-their-target-location.py
@@ -5,52 +5,56 @@ class Solution:
                 return True
             return False
             
+        def manhat(i,j):
+            #manhatten distance
+            return abs(i - target[0]) + abs((j - target[1]))
+   
         start = 0,0
         box = 0,0
+        target = 0,0
         for i in range(len(grid)):
             for j in range(len(grid[0])):
                 if grid[i][j] == 'S':
                     start = i,j
                 elif grid[i][j] == 'B':
                     box = i,j
+                elif grid[i][j] == 'T':
+                    target = i,j
         
         grid[start[0]][start[1]] = '.'
         grid[box[0]][box[1]] = '.'
         directions = [[-1,0], [1,0], [0,1],[0,-1]]
-        q = deque()
-        q.append((0, start[0], start[1], box[0], box[1]))
+        q = []
+        q.append((manhat(box[0],box[1]),0,start[0], start[1], box[0], box[1]))
         seen = {}
-        res = float('inf')
-        while q:
-            
-            for i in range(len(q)):
-                
-                moves, s1,s2,b1,b2 = q.popleft()
-                
-                if (s1,s2,b1,b2) in seen and seen[(s1,s2,b1,b2)] <= moves:
-                    continue
-                
-                seen[(s1,s2,b1,b2)] = moves
-                
-                if grid[b1][b2] == 'T':
-                    res = min(res, moves)
-                    continue
-                
-                for x,y in directions:
-                    
-                    newx, newy = s1 + x, s2 + y
-                    
-                    if [newx,newy] == [b1,b2]:
-                        
-                        #cant push box
-                        if oob(b1+x,b2 + y) or (grid[b1 + x][b2 + y] != '.' and grid[b1 + x][b2 + y] != 'T'):
-                            continue
-                        
-                        q.append((moves + 1, newx, newy, b1 + x, b2 + y))
-                    elif not oob(newx,newy) and grid[newx][newy] != '#':
-                        q.append((moves, newx,newy, b1,b2))
         
-        return res if res != float('inf') else -1
+        while q:
+
+            _, moves, s1,s2,b1,b2 = heappop(q)
+
+            if b1 == target[0] and b2 == target[1]:
+                return moves
+
+            if (s1,s2,b1,b2) in seen:
+                continue
+
+            seen[(s1,s2,b1,b2)] = moves
+
+            for x,y in directions:
+
+                newx, newy = s1 + x, s2 + y
+
+                if [newx,newy] == [b1,b2]:
+
+                    #cant push box
+                    if oob(b1+x,b2 +y) or (grid[b1 + x][b2 + y] != '.' and grid[b1 + x][b2 + y] != 'T'):
+                        continue
+
+                    heappush(q,(manhat(b1 + x,b2 + y) + moves + 1,moves + 1, newx, newy, b1 + x, b2 + y))
+                elif not oob(newx,newy) and grid[newx][newy] != '#':
+                    heappush(q,(manhat(b1, b2) + moves, moves, newx,newy, b1,b2))
+
+        return -1
                         
                     
                 
