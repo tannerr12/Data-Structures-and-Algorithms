43fe97b495fd4f9c5ca288bb38582c4f24e92b35
tannerr12 <tanner.01@hotmail.com>
2023-08-11 19:12:37 -0400

    Time: 48 ms (51.81%), Space: 16.4 MB (8.43%) - LeetHub

---
+++ 0761-special-binary-string/0761-special-binary-string.py
@@ -0,0 +1,42 @@
+class Solution:
+    def makeLargestSpecial(self, s: str) -> str:
+        
+        @cache
+        def reorder(s):
+            count1 = 0
+            count2 = 0
+            
+            for i in range(len(s)):
+                count1 = 0
+                for j in range(i, len(s)):
+                    if s[j] == '1':
+                        count1 += 1
+                    else:
+                        count1 -= 1
+                    if count1 == -1:
+                        break
+                    if count1 == 0:
+                        count2 = 0
+                        for k in range(j + 1, len(s)):
+
+                            if s[k] == '1':
+                                count2 += 1
+                            else:
+                                count2 -= 1
+                            
+                            if count2 == -1:
+                                break
+                            if count2 == 0:
+                                new = s[:i] + s[j+1:k+1] + s[i:j+1] + s[k+1:]
+                                if new > s:
+                                    return new
+        
+            return s
+        
+        while True:
+            ns = reorder(s)
+            if s == ns:
+                return ns
+            s = ns
+        
+        
