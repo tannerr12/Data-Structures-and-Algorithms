c1e91f6a72485a388c4669bd8f12aec85a41ea2d
tannerr12 <tanner.01@hotmail.com>
2023-08-12 14:44:27 -0400

    Time: 433 ms (37.67%), Space: 32.3 MB (24.83%) - LeetHub

--- 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
+++ 1092-shortest-common-supersequence/1092-shortest-common-supersequence.py
@@ -1,49 +1,36 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
-        
-        ans = []
-        
-        @cache
-        def dfs(i,j):
-            
-            if i >= len(str1) and j >= len(str2):
-                return 0
-            
-            res = float('inf')
-
-            if i < len(str1) and j < len(str2) and str1[i] == str2[j]:
-                #advance both (they are the same)
-                res = min(res, dfs(i+1,j+1) + 1)
-            else:    
-                if i < len(str1):
-                    #advance i
-                    res = min(res, dfs(i+1, j) + 1)
-                if j < len(str2):
-                    #advance j
-                    res = min(res, dfs(i, j+1) + 1)
-            
-            
-            return res
-        
-        
-        q = deque([[0,0,'']])
-        seen = set()
-        while q:
-            
-            for i in range(len(q)):
                 
-                i,j,path = q.popleft()
-                if (i,j) in seen:
-                    continue
-                seen.add((i,j))
-                if i >= len(str1) and j >= len(str2):
-                    return path
-                
-                if i < len(str1) and j < len(str2) and str1[i] == str2[j]:
-                    q.append([i+1,j+1,path + str1[i]])
-                else:
-                    if i < len(str1):
-                        q.append([i+1, j, path + str1[i]])
-                    if j < len(str2):
-                        q.append([i, j + 1, path + str2[j]])
-                    
+        
+        def lcs(A, B):
+            n, m = len(A), len(B)
+            dp = [["" for _ in range(m + 1)] for _ in range(n + 1)]
+            for i in range(n):
+                for j in range(m):
+                    if A[i] == B[j]:
+                        dp[i + 1][j + 1] = dp[i][j] + A[i]
+                    else:
+                        dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1], key=len)
+            return dp[-1][-1]
+        
+        word = lcs(str1,str2)
+        idx1,idx2 = 0,0
+        ans = ''
+        for c in word:
+            
+            while idx1 < len(str1) and str1[idx1] != c:
+                ans += str1[idx1]
+                idx1 += 1
+            while idx2 < len(str2) and str2[idx2] != c:
+                ans += str2[idx2]
+                idx2 += 1
+            
+            ans += c
+            idx1 += 1
+            idx2 += 1
+        
+        
+        ans += str1[idx1:]
+        ans += str2[idx2:]
+        
+        return ans
