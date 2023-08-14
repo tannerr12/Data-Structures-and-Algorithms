13c3b71e3e59d25c5667881ccdc8f9ea52c3d9a8
tannerr12 <tanner.01@hotmail.com>
2023-08-14 17:00:01 -0400

    Time: 1924 ms (5.68%), Space: 72.3 MB (5.67%) - LeetHub

---
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.py
@@ -0,0 +1,41 @@
+class Solution:
+    def palindromePartition(self, s: str, k: int) -> int:
+        
+        
+        def minCostPal(i, st):
+            
+            if i == len(st) // 2:
+                return 0
+            res = float('inf')
+            #they are the same
+            if st[i] == st[-i-1]:
+                res = min(res, minCostPal(i+1, st))
+            
+            else:
+                #change left or right
+                res = min(res, minCostPal(i+1, st) + 1)
+            
+            
+            return res
+        
+        @cache
+        def dfs(i, last, count):
+
+            if i >= len(s):
+                return float('inf')
+            if count == k-1:
+                return minCostPal(0, s[last+1:])
+
+            
+            res = float('inf')
+     
+            #split
+            res = min(res, dfs(i+1, i, count +1) + minCostPal(0, s[last+1:i+1]))
+            
+            
+            #dont split
+            res = min(res, dfs(i+1, last, count))
+            
+            return res
+        
+        return dfs(0,-1, 0)
