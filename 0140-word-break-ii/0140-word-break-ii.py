c5b256324eda7637a5d23d8eb1b1a19fe7318bbe
tannerr12 <tanner.01@hotmail.com>
2023-08-19 11:55:55 -0400

    Time: 38 ms (75.54%), Space: 16.4 MB (20.92%) - LeetHub

---
+++ 0140-word-break-ii/0140-word-break-ii.py
@@ -0,0 +1,24 @@
+class Solution:
+    def wordBreak(self, s: str, wordDict: List[str]) -> List[str]:
+        ans = []
+        
+        @cache
+        def dfs(i,st,last):
+            
+            if i >= len(s):
+                if last == i-1:
+                    ans.append(st[:-1])
+                return 
+            
+            
+            #take character
+            dfs(i+1, st + s[i], last)
+            #take word
+            if s[last+1:i+1] in wordDict:
+                dfs(i+1, st + s[i] + ' ', i)
+        
+        
+        dfs(0,'',-1)
+        
+        print(ans)
+        return ans
