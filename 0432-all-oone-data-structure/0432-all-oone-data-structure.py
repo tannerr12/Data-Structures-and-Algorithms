09974c3d6e3a59a42f2d333573ed5836fb53e75e
tannerr12 <tanner.01@hotmail.com>
2023-08-26 12:34:35 -0400

    Time: 169 ms (89.31%), Space: 33.3 MB (33.33%) - LeetHub

--- 0432-all-oone-data-structure/0432-all-oone-data-structure.py
+++ 0432-all-oone-data-structure/0432-all-oone-data-structure.py
@@ -1,43 +1,63 @@
+class DoubleLinked:
+    def __init__(self, key):
+        self.key = key
+        self.count = 1
+        self.prev = None
+        self.next = None
+
 class AllOne:
-
     def __init__(self):
-        self.mp = defaultdict(int)
+        self.mp = {}
+        self.head = DoubleLinked('')
+        self.tail = DoubleLinked('')
+        self.head.next = self.tail
+        self.tail.prev = self.head
+        self.head.count = float('-inf')
+        self.tail.count = float('inf')
         
-
     def inc(self, key: str) -> None:
-        self.mp[key] += 1
+        if key not in self.mp:
+            new = DoubleLinked(key)
+            new.next = self.head.next
+            self.head.next.prev = new
+            self.head.next = new
+            new.prev = self.head
+            self.mp[key] = new
+        else:
+            self.mp[key].count += 1
+            current = self.mp[key]
+            while current.next.count < current.count:
+                next_node = current.next
+                current.prev.next = next_node
+                next_node.prev = current.prev
+                current.next = next_node.next
+                current.next.prev = current
+                next_node.next = current
+                current.prev = next_node
 
     def dec(self, key: str) -> None:
-        self.mp[key] -= 1
-        if self.mp[key] == 0:
+        if key not in self.mp: 
+            return
+        self.mp[key].count -= 1
+        if self.mp[key].count == 0:
+            prv = self.mp[key].prev
+            nxt = self.mp[key].next
+            prv.next = nxt
+            nxt.prev = prv
             del self.mp[key]
+        else:
+            current = self.mp[key]
+            while current.prev.count > current.count:
+                prev_node = current.prev
+                prev_node.next = current.next
+                current.next.prev = prev_node
+                current.prev = prev_node.prev
+                prev_node.prev.next = current
+                prev_node.prev = current
+                current.next = prev_node
 
     def getMaxKey(self) -> str:
-        res,total = '',0
-        for key,val in self.mp.items():
-            
-            if val > total:
-                total = val
-                res = key
-        
-        return res
-            
+        return self.tail.prev.key
 
     def getMinKey(self) -> str:
-        res,total = '',float('inf')
-        for key,val in self.mp.items():
-            
-            if val < total:
-                total = val
-                res = key
-        
-        return res
-            
-
-
-# Your AllOne object will be instantiated and called as such:
-# obj = AllOne()
-# obj.inc(key)
-# obj.dec(key)
-# param_3 = obj.getMaxKey()
-# param_4 = obj.getMinKey()
+        return self.head.next.key
