0acb0d634ad8e6bf312ecf44db7863f2ce2a2df7
tannerr12 <tanner.01@hotmail.com>
2023-08-27 13:25:13 -0400

    Time: 2228 ms (25.92%), Space: 16.5 MB (85.19%) - LeetHub

---
+++ 0548-split-array-with-equal-sum/0548-split-array-with-equal-sum.py
@@ -0,0 +1,32 @@
+class Solution:
+    def splitArray(self, nums: List[int]) -> bool:
+        
+        
+        #1,2,1,2,1,2,1
+        #1,3,4,6,7,9,10
+        
+        
+        #0 -> 1 2 - 3
+        prefix = []
+        prefix.append(0)
+        for i in range(len(nums)):
+            prefix.append(prefix[-1] + nums[i])
+            
+        for i in range(1,len(nums)):
+            left = prefix[i]
+            seen = set()
+            
+            for j in range(i + 2, len(nums)-1):
+                total = (prefix[j] - prefix[i+1])
+                targetAmt = left * 2
+                diff = total - targetAmt
+                if (diff, left) in seen and prefix[-1] - prefix[j+1] == left:
+                    return True
+                
+                if j > i + 2:
+                    seen.add((nums[j-1], prefix[j-1] - prefix[i+1]))
+
+                
+        
+        return False
+                
