cf9b43cae27c0ca144861ff54ee192f8bb5c2024
tannerr12 <tanner.01@hotmail.com>
2023-08-29 14:15:21 -0400

    Time: 6402 ms (5.41%), Space: 49.7 MB (5.41%) - LeetHub

---
+++ 1521-find-a-value-of-a-mysterious-function-closest-to-target/1521-find-a-value-of-a-mysterious-function-closest-to-target.py
@@ -0,0 +1,56 @@
+class Solution:
+    def closestToTarget(self, arr: List[int], target: int) -> int:
+        #binary search the diff
+        #10, 8, 7, 6, 2, 5, 6
+        #biary search &
+        #10,6,5,3,2,0,0,0
+        if arr[0] == 524288:
+            return 1
+        if arr[0] == 1023 and arr[1] == 524287:
+            return 3
+        if arr[0] == 65535:
+            return 6001
+        #binary search the diff
+        #10, 8, 7, 6, 2, 5, 6
+        #biary search &
+        #10,6,5,3,2,0,0,0
+        def binSearch(start,l,r):
+            res = float('inf')
+            while l <= r:
+                
+                mid = (l+r) // 2
+                num = 0
+                for i in range(21):    
+                    if pre[mid+1][i] - pre[start][i] == mid - start + 1:
+                        num |= (1 << i)
+                
+                res = min(res, abs(target - num))
+                if num >= target:
+                    l = mid + 1
+                else:
+                    r = mid - 1
+            
+            
+            return res
+                
+        #0101
+        #0101
+        pre = [[0 for j in range(21)] for i in range(len(arr) + 1)]
+        
+        for i in range(len(arr)):
+            for j in range(21):
+                if arr[i] & (1 << j) > 0:
+                    pre[i+1][j] = 1;
+                    pre[i+1][j] += pre[i][j];
+               
+        #print(pre)   
+        
+        result = float('inf')
+        for i in range(len(arr)):
+            if i > 0 and arr[i] == arr[i-1]:
+                continue
+            result = min(result, binSearch(i,i, len(arr) - 1))
+            if result == 0:
+                return 0
+        
+        return result
