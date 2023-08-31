4c32f8291951885c72cf10e7f3b04bd7cf69e9d7
tannerr12 <tanner.01@hotmail.com>
2023-08-31 10:47:26 -0400

    Time: 171 ms (48.57%), Space: 19.8 MB (5.48%) - LeetHub

---
+++ 1326-minimum-number-of-taps-to-open-to-water-a-garden/1326-minimum-number-of-taps-to-open-to-water-a-garden.py
@@ -0,0 +1,38 @@
+class Solution:
+    def minTaps(self, n: int, ranges: List[int]) -> int:
+        
+        ls= []
+        
+        for i in range(len(ranges)):
+            
+            ls.append((i - ranges[i],i + ranges[i], 1))
+            ls.append((i + ranges[i],float('-inf'), -1))
+            
+        ls.sort()
+        
+        count = 0
+        idx = 0
+        close = 0
+        mx = 0
+        cost = 0
+        for i in range(n):
+            
+            while idx < len(ls) and ls[idx][2] == -1 and ls[idx][0] <= i:
+                count += ls[idx][2]
+                idx +=1
+            
+                
+            while idx < len(ls) and ls[idx][0] <= i:
+                count += ls[idx][2]
+                mx = max(mx, ls[idx][1])
+                idx +=1
+            
+            
+            if i == close:
+                if mx <= i:
+                    return -1
+                close = mx
+                cost += 1
+                mx = 0
+            
+        return cost
