d93d1f90df81b9ef58ce261db2a9071b92abd9dc
tannerr12 <tanner.01@hotmail.com>
2023-09-17 15:11:42 -0400

    Time: 794 ms (38.84%), Space: 31.9 MB (79.34%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.py
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.py
@@ -1,68 +1,18 @@
 class Solution:
     def getXORSum(self, arr1: List[int], arr2: List[int]) -> int:
         
-        bitmask = 0
         andVal = 0
         
-        arr = []
+        
         for val in arr2:
             andVal ^= val
         
-        for val in arr1:
-            arr.append(val & andVal)
-            
-        
         res = 0
-        
-        for val in arr:
-            res ^= val
+        for val in arr1:
+            res ^= val & andVal
+
         
         return res
 
-        '''
-        001
-        110
-        101
-        
-    
-        000
-        001
-        010
-        000
-        010
-        011
-        010
-        
-        
-        001
-        010
-        011
-        
-        000
-        
-        
-        
-        110
-        101
-        
-        
-        010
-        011
-        
-        011
-        011
-        111
-        
-        001
-        010
-        011
-        000
-        
-        
-        001
-        
-        011
-        
-        '''
         
         
