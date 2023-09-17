e6b5cac64af03bf298a0d7fc992a8908498b3146
tannerr12 <tanner.01@hotmail.com>
2023-09-17 15:12:21 -0400

    Time: 753 ms (73.55%), Space: 31.9 MB (55.37%) - LeetHub

--- 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.py
+++ 1835-find-xor-sum-of-all-pairs-bitwise-and/1835-find-xor-sum-of-all-pairs-bitwise-and.py
@@ -1,6 +1,8 @@
 class Solution:
     def getXORSum(self, arr1: List[int], arr2: List[int]) -> int:
         
+        if len(arr2) > len(arr1):
+            arr1,arr2 = arr2,arr1
         andVal = 0
         
         
