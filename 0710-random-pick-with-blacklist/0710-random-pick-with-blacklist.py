7c7b6575e7a0c0cf98992bfad94b7dc292b6199d
tannerr12 <tanner.01@hotmail.com>
2023-09-18 14:57:39 -0400

    Time: 221 ms (92.23%), Space: 30.1 MB (17.96%) - LeetHub

---
+++ 0710-random-pick-with-blacklist/0710-random-pick-with-blacklist.py
@@ -0,0 +1,37 @@
+class Solution:
+
+    def __init__(self, n: int, blacklist: List[int]):
+        self.vals =set()
+        self.ranges = []
+        blacklist.sort()
+        prev = 0
+        for v in blacklist:
+            if v < n:
+                self.vals.add(v)
+            if v -1 >= prev:
+                self.ranges.append((prev, v - 1))
+                
+            prev = v + 1
+        if n -1 >= prev:
+            self.ranges.append((prev, n -1))
+        self.n = n
+        self.ptr = self.ranges[0][0]
+        self.idx = 0
+        print(self.ranges)
+        
+    def pick(self) -> int:
+        
+        if self.ptr < self.ranges[self.idx][1]:
+            self.ptr += 1
+        
+        else:
+            self.idx += 1
+            self.idx %= len(self.ranges)
+            self.ptr = self.ranges[self.idx][0]
+        
+        return self.ptr
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(n, blacklist)
+# param_1 = obj.pick()
