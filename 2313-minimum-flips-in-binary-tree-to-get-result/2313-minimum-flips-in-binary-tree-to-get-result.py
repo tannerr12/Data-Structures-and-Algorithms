ce8ecad5476f5241071209c1c7edc192b110a723
tannerr12 <tanner.01@hotmail.com>
2023-09-19 18:19:36 -0400

    Time: 905 ms (46.96%), Space: 150.5 MB (40.00%) - LeetHub

--- 2313-minimum-flips-in-binary-tree-to-get-result/2313-minimum-flips-in-binary-tree-to-get-result.py
+++ 2313-minimum-flips-in-binary-tree-to-get-result/2313-minimum-flips-in-binary-tree-to-get-result.py
@@ -1,9 +1,3 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 class Solution:
     def minimumFlips(self, root: Optional[TreeNode], result: bool) -> int:
        
@@ -11,73 +5,76 @@ class Solution:
         def dfs(root):
             
             if root is None:
-                return [True,0,False,0]
+                return [0,0]
             
             Tmin = float('inf')
             Fmin = float('inf')
             left = dfs(root.left)
             right = dfs(root.right)
             
+            #False
             if root.val == 0:
-                return [True, 1, False, 0]
+                return [1,0]
+            #True
             elif root.val == 1:
-                return [True, 0, False, 1]
+                return [0,1]
             
+            #OR
             elif root.val == 2:
 
                 #True and True
-                Tmin = min(Tmin, left[1] + right[1])
+                Tmin = min(Tmin, left[0] + right[0])
                 
                 #False and True
-                Tmin = min(Tmin, left[3] + right[1])
+                Tmin = min(Tmin, left[1] + right[0])
                 
                 #True and False
-                Tmin = min(Tmin, left[1] + right[3])
+                Tmin = min(Tmin, left[0] + right[1])
                     
                 #False + False
-                Fmin = min(Fmin, left[3] + right[3])
+                Fmin = min(Fmin, left[1] + right[1])
                 
                      
-                    
+            #AND    
             elif root.val == 3:
                 #True + True
-                Tmin = min(Tmin, left[1] + right[1])
+                Tmin = min(Tmin, left[0] + right[0])
 
                 #True + False
-                Fmin = min(Fmin, left[1] + right[3])
+                Fmin = min(Fmin, left[0] + right[1])
                 
                 #False + True
-                Fmin = min(Fmin, left[3] + right[1])
+                Fmin = min(Fmin, left[1] + right[0])
                 
                 #False + False
-                Fmin = min(Fmin, left[3] + right[3])
+                Fmin = min(Fmin, left[1] + right[1])
 
-           
+            #XOR
             elif root.val == 4:
                 
                 #False + True
-                Tmin = min(Tmin, left[3] + right[1])
-
+                Tmin = min(Tmin, left[1] + right[0])
+                
                 #True + False
-                Tmin = min(Tmin, left[1] + right[3])
-                
+                Tmin = min(Tmin, left[0] + right[1])
+    
                 #False + False
-                Fmin = min(Fmin, left[3] + right[3])
-                
-                Fmin = min(Fmin, left[1] + right[1]) 
-
-            else:
-                
-                Tmin = min(Tmin, left[3] + right[3])
                 Fmin = min(Fmin, left[1] + right[1])
+                
+                #True + True
+                Fmin = min(Fmin, left[0] + right[0]) 
             
-            return [True,Tmin,False,Fmin]
+            #NOT
+            else:
+                Tmin = min(Tmin, left[1] + right[1])
+                Fmin = min(Fmin, left[0] + right[0])
+            
+            return [Tmin,Fmin]
         
         
         res = dfs(root)
         
         if result:
-            return res[1]
+            return res[0]
         else:
-            return res[3]
-                
+            return res[1]
