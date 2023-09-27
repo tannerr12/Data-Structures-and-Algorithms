4b2f4c72a98c867501e944e17a6bd3c2320d99db
tannerr12 <tanner.01@hotmail.com>
2023-09-27 18:55:37 -0400

    Time: 749 ms (28.26%), Space: 31.2 MB (35.14%) - LeetHub

--- 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.py
+++ 2772-apply-operations-to-make-all-array-elements-equal-to-zero/2772-apply-operations-to-make-all-array-elements-equal-to-zero.py
@@ -1,18 +1,19 @@
 class Solution:
     def checkArray(self, nums: List[int], k: int) -> bool:
         
-        ls = []
-        ls.append([k,nums[0]])
+        ls = deque([[k,nums[0]]])
         run = -nums[0]
+        
         for i in range(len(nums)):
-            while ls and ls[0][0] == i:
-                run += heappop(ls)[1]
+            if ls and ls[0][0] == i:
+                run += ls.popleft()[1]
+                
             newnum = nums[i] + run
             if newnum < 0 or (newnum > 0 and i + k > len(nums)):
                 return False
             else:
                 if newnum > 0:
-                    heappush(ls, [i + k, newnum])
+                    ls.append([i + k, newnum])
                     run -= newnum
         
         return True
