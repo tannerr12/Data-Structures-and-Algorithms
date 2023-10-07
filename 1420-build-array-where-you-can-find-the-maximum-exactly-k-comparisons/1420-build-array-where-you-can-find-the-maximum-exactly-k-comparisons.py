4edc9c147abb6c8583bf062e372b9e645bcf629a
tannerr12 <tanner.01@hotmail.com>
2023-10-07 11:41:59 -0400

    Time: 861 ms (56.64%), Space: 39.3 MB (27.43%) - LeetHub

--- 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
+++ 1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons/1420-build-array-where-you-can-find-the-maximum-exactly-k-comparisons.py
@@ -12,14 +12,14 @@ class Solution:
             if size == 0:
                 return rem == 0
             
+            res = 0
+            for i in range(cur + 1, m+1):        
+                res += dfs(i, rem - 1, size - 1)
+                res %= MOD
             
-            res = (dfs(cur, rem, size - 1) * cur) % MOD
-            
-            for i in range(cur+1, m+1):
-                res = (res + dfs(i, rem - 1, size - 1)) % MOD
-                
-            
-            return res 
+            res += dfs(cur, rem, size-1) * cur
+    
+            return res % MOD 
         
         
         return dfs(0, k, n)
