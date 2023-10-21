d0867164d8e2774df61b16109b6dcfa7ac5793b0
tannerr12 <tanner.01@hotmail.com>
2023-10-21 19:25:39 -0400

    Time: 1696 ms (29.36%), Space: 38.2 MB (11.06%) - LeetHub

--- 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
+++ 1425-constrained-subsequence-sum/1425-constrained-subsequence-sum.py
@@ -1,16 +1,29 @@
-import heapq
-
 class Solution:
     def constrainedSubsetSum(self, nums: List[int], k: int) -> int:
-        heap = [(-nums[0], 0)]
-        ans = nums[0]
         
-        for i in range(1, len(nums)):
-            while i - heap[0][1] > k:
-                heapq.heappop(heap)
-
-            curr = max(0, -heap[0][0]) + nums[i]
-            ans = max(ans, curr)
-            heapq.heappush(heap, (-curr, i))
-
-        return ans
+        #at any point there will be a best value and if that value is > 0 we should use it since the cost is free
+        #we may have a current sum and a best sum
+        
+        #cur could be 10 + -2 = 8 
+        
+        #we need to be careful about going over the distance limit
+        
+        heap = [[0,0]]
+        res = float('-inf')
+        for i in range(len(nums)):
+            
+            while heap[0][1] < i - k:
+                heappop(heap)
+                
+            val, idx = heap[0]
+            val = -val
+            
+            if val >= 0:
+                res = max(res, val + nums[i])
+                heappush(heap, [-(val + nums[i]), i])
+            else:
+                res = max(res, nums[i])
+                heappush(heap, [-nums[i], i])
+        
+        return res
+            
