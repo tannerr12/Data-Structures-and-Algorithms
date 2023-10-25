41a63fd2feacda20fd3c2a93a84a9cb3ccfb06c8
tannerr12 <tanner.01@hotmail.com>
2023-10-25 11:57:21 -0400

    Time: 5122 ms (9.87%), Space: 23.6 MB (59.52%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
@@ -1,19 +1,38 @@
 class Solution:
     def longestDupSubstring(self, s: str) -> str:
+        MOD = 10 ** 9 + 7
+        
+        def check(num, word,st):
+            
+            for pos in st[num]:
+                if s[pos - mid: pos + 1] == word:
+                    return True
+            
+            return False
         
         def isGood(mid):
-           
-            st = set()
+            st = defaultdict(list)
+            val = 0
+            
             for j in range(len(s)):
+                if j < mid:
+                    val = (val + ((ord(s[j]) - ord('a') + 1) * pow(26, (mid - j -1),MOD))) % MOD
+                    if j+1 < mid: 
+                        continue
+                else:
+                    val = (val - ((ord(s[j-mid]) - ord('a') + 1) * pow(26,(mid - 1),MOD))) % MOD
+                    val = (val * 26) % MOD
+                    val = (val + ((ord(s[j]) - ord('a') + 1) * pow(26, 0, MOD))) % MOD
+                
+                if val in st:
+                    if check(val, s[j-mid:j+1],st):
+                        return s[j-mid:j+1]
 
-                if s[j:j+mid] in st:
-                    return s[j:j+mid]
-
-                st.add(s[j:j+mid])
+                st[val].append(j)
                 
             return ''
     
-        l,r = 1, len(s)
+        l,r = 0, len(s)
         res = ''
         while l <= r:
             
