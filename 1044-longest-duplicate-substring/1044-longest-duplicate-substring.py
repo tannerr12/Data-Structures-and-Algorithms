064f6bd2e5683d17196f2e0da4322d46aaf46426
tannerr12 <tanner.01@hotmail.com>
2023-10-25 10:13:59 -0400

    Time: 6832 ms (5.11%), Space: 461.3 MB (5.10%) - LeetHub

---
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.py
@@ -0,0 +1,34 @@
+class Solution:
+    def longestDupSubstring(self, s: str) -> str:
+        
+        def isGood(mid):
+           
+            st = set()
+            for j in range(len(s)):
+
+                if s[j:j+mid] in st:
+                    return s[j:j+mid]
+
+                st.add(s[j:j+mid])
+                
+            return ''
+        
+        l,r = 0, len(s)
+        res = ''
+        while l <= r:
+            
+            mid = (l + r) // 2    
+            val = isGood(mid)
+            
+            if val != '':
+                res = val
+                l = mid + 1
+            else:
+                r = mid -1
+        
+        
+        
+        
+        return res
+    
+        
