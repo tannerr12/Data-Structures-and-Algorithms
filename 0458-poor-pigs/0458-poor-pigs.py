051a736499fdeafc55f6e6fa329f36054c22a939
tannerr12 <tanner.01@hotmail.com>
2023-10-29 12:11:02 -0400

    Time: 34 ms (75.20%), Space: 16.3 MB (36.00%) - LeetHub

---
+++ 0458-poor-pigs/0458-poor-pigs.py
@@ -0,0 +1,4 @@
+class Solution:
+    def poorPigs(self, buckets: int, minutesToDie: int, minutesToTest: int) -> int:
+        states = minutesToTest // minutesToDie + 1
+        return math.ceil(math.log2(buckets) / math.log2(states))
