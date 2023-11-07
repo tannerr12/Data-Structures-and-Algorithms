790a9f85101ee003afb56fa99cb88d16cb9a6853
tannerr12 <tanner.01@hotmail.com>
2023-11-07 13:38:29 -0500

    Time: 80 ms (81.18%), Space: 16.3 MB (71.76%) - LeetHub

--- 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
+++ 0923-3sum-with-multiplicity/0923-3sum-with-multiplicity.py
@@ -4,42 +4,43 @@ class Solution:
         c = Counter(arr)
         MOD = 10 ** 9 + 7
         ls = list(c)
+        ls.sort()
+        
         #print(c)
         #print(ls)
         res = 0
         for i in range(len(ls)):
             for j in range(i, len(ls)):
-                for k in range(j, len(ls)):
-                    need = target - ls[i] - ls[j]
-                    if ls[k] != need:
-                        continue
+         
+                need = target - ls[i] - ls[j]
+                if need < ls[j]:
+                    continue
+                totalNeed = c[need]
 
-                    totalNeed = c[need]
+                if ls[i] == need:
+                    totalNeed -=1
+                if ls[j] == need:
+                    totalNeed -=1
 
-                    if ls[i] == need:
-                        totalNeed -=1
-                    if ls[j] == need:
-                        totalNeed -=1
+                totalI = c[ls[i]]
+                totalJ = c[ls[j]]
 
-                    totalI = c[ls[i]]
-                    totalJ = c[ls[j]]
+                if ls[i] == ls[j]:
+                    totalJ -= 1
 
-                    if ls[i] == ls[j]:
-                        totalJ -= 1
+                if totalI > 0 and totalJ > 0 and totalNeed > 0: 
+                    if ls[i] == ls[j] == need:
+                        res += factorial(totalI) // (factorial(3) * factorial(totalI - 3))
 
-                    if totalI > 0 and totalJ > 0 and totalNeed > 0: 
-                        if ls[i] == ls[j] == ls[k]:
-                            res += factorial(totalI) // (factorial(3) * factorial(totalI - 3))
-                        
-                        elif ls[j] == ls[k]:
-                            res += ((totalNeed * (totalNeed + 1)) // 2) * totalI
-                        elif ls[i] == ls[j]:
-                            res += totalNeed * ((totalJ * (totalJ + 1)) // 2)
-                        else:
-                            res += max(0, totalI * totalJ * totalNeed)
-                        
-                        res %= MOD
-                        
+                    elif ls[j] == need:
+                        res += ((totalNeed * (totalNeed + 1)) // 2) * totalI
+                    elif ls[i] == ls[j]:
+                        res += totalNeed * ((totalJ * (totalJ + 1)) // 2)
+                    else:
+                        res += max(0, totalI * totalJ * totalNeed)
+
+                    res %= MOD
+
         return res
                 
                 
