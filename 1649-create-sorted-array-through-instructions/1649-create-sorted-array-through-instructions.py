e910d1157f4398a9c2eb98908fa2eb6387a59fe6
tannerr12 <tanner.01@hotmail.com>
2023-11-08 12:28:43 -0500

    Time: 3082 ms (94.24%), Space: 31.2 MB (55.40%) - LeetHub

--- 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
+++ 1649-create-sorted-array-through-instructions/1649-create-sorted-array-through-instructions.py
@@ -1,18 +1,28 @@
+
 class Solution:
-    def createSortedArray(self, instructions: List[int]) -> int:
+    def createSortedArray(self, A: List[int]) -> int:
         
-        
-        nums = []
-        cost = 0
-        MOD = 10 ** 9 + 7
-        for i in range(len(instructions)):
-            
-            insort(nums, instructions[i])
-            
-            left = bisect_left(nums, instructions[i])
-            right = bisect_right(nums, instructions[i])
-            
-            cost = (cost + min(left, (len(nums) - right))) % MOD
-        
-        
-        return cost
+        m = max(A)
+        c = [0] * (m + 1)
+
+        def update(x):
+            while (x <= m):
+                c[x] += 1
+                #add lsb
+                x += x & -x
+
+        def get(x):
+            res = 0
+            #x is the bit
+            while (x > 0):
+                #add parnet
+                res += c[x]
+                #remove lsb
+                x -= x & -x
+            return res
+
+        res = 0
+        for i, a in enumerate(A):
+            res += min(get(a - 1), i - get(a))
+            update(a)
+        return res % (10**9 + 7)
