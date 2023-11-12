192212e115e83231b7f6bd2fbade87eb7b73c44a
tannerr12 <tanner.01@hotmail.com>
2023-11-11 20:54:56 -0500

    Time: 510 ms (95.44%), Space: 0B (100.00%) - LeetHub

--- 2687-bikes-last-time-used/2687-bikes-last-time-used.sql
+++ 2687-bikes-last-time-used/2687-bikes-last-time-used.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select b.bike_number, end_time from bikes b where end_time = (select max(end_time) from bikes where bike_number = b.bike_number) group by bike_number
+select b.bike_number, max(end_time) as end_time from bikes b group by bike_number
