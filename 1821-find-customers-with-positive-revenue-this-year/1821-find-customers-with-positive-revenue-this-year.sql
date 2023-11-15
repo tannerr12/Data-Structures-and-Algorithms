0bf5bfb849969b4f653b7f40896f3ae95c43246d
tannerr12 <tanner.01@hotmail.com>
2023-11-15 18:21:51 -0500

    Time: 1293 ms (74.50%), Space: 0B (100.00%) - LeetHub

--- 1821-find-customers-with-positive-revenue-this-year/1821-find-customers-with-positive-revenue-this-year.sql
+++ 1821-find-customers-with-positive-revenue-this-year/1821-find-customers-with-positive-revenue-this-year.sql
@@ -1,2 +1,2 @@
 # Write your MySQL query statement below
-select customer_id from customers where revenue > 0 and year = 2021 group by customer_id
+select customer_id from customers where revenue > 0 and year = 2021
