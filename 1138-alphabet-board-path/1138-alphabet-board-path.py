b3669aeef000fcaab2b5d8c55ead68d8576211c5
tannerr12 <tanner.01@hotmail.com>
2023-11-16 20:34:37 -0500

    Time: 28 ms (98.91%), Space: 16.3 MB (19.13%) - LeetHub

---
+++ 1138-alphabet-board-path/1138-alphabet-board-path.py
@@ -0,0 +1,48 @@
+class Solution:
+    def alphabetBoardPath(self, target: str) -> str:
+        manhat = defaultdict(tuple)
+        board = ["abcde", "fghij", "klmno", "pqrst", "uvwxy", "z...."]
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                cur = board[i][j]
+                manhat[cur] = (i,j)
+        
+        
+        #print(manhat)
+
+        res = ''
+        x,y = 0,0
+   
+        for i in range(len(target)):
+            cur = board[x][y]    
+            find = target[i]
+            distanceVert = x - manhat[find][0]
+            distanceHor = y - manhat[find][1]
+            
+            x -= distanceVert
+            y -= distanceHor
+            
+            if cur != 'z':
+                if distanceHor > 0:
+                    res += 'L' * distanceHor
+                elif distanceHor < 0:
+                    res += 'R' * abs(distanceHor)
+                if distanceVert > 0:
+                    res += 'U' * distanceVert
+                elif distanceVert < 0:
+                    res += 'D' * abs(distanceVert)
+            else:
+                if distanceVert > 0:
+                    res += 'U' * distanceVert
+                elif distanceVert < 0:
+                    res += 'D' * abs(distanceVert)
+                if distanceHor > 0:
+                    res += 'L' * distanceHor
+                elif distanceHor < 0:
+                    res += 'R' * abs(distanceHor)
+            
+            res += '!'
+          
+            
+        return res
+            
