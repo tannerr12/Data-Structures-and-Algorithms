a23dd8398a77ce5cfa9e3c334a1e852ff676c1ed
tannerr12 <tanner.01@hotmail.com>
2023-11-25 19:50:33 -0500

    Time: 1093 ms (61.81%), Space: 42.8 MB (27.08%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.py
@@ -1,14 +1,16 @@
 class Solution:
     def largestSubmatrix(self, matrix: List[List[int]]) -> int:
         
-        
+        #find the longest column sequence length than sort each row by 
+        #the sequence length and multiply its value by its sorted position in the grid
+        #this is possible if we sort in decreasing order since we can assume previous
+        #columns are valid to use
         for j in range(len(matrix[0])):
             for i in range(len(matrix) -2, -1,-1):
                 if matrix[i][j] > 0:
                     matrix[i][j] = matrix[i+1][j] + matrix[i][j]
             
         
-        #print(matrix)
         res = 0
         for i in range(len(matrix)):
             matrix[i].sort(reverse = True)
