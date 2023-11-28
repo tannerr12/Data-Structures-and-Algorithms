a0fe1890abc73acffec5d6c2fa5a8ab4ae4355c4
tannerr12 <tanner.01@hotmail.com>
2023-11-27 21:55:44 -0500

    Time: 473 ms (48.57%), Space: 17.5 MB (52.86%) - LeetHub

--- 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
+++ 2147-number-of-ways-to-divide-a-long-corridor/2147-number-of-ways-to-divide-a-long-corridor.py
@@ -1,20 +1,16 @@
 class Solution:
     def numberOfWays(self, cor: str) -> int:
-        c = Counter(cor)
+    
         res = 1
         MOD = 10 ** 9 + 7
-        #print(c)
-        if c["S"] == 2:
-            return 1
-        elif c["S"] % 2 or c["S"] == 0:
-            return 0
+        
         
         count = 1
         couch = 0
         for i in range(len(cor)):
             if cor[i] == 'S':
                 if couch == 2:
-                    res *= count
+                    res = (res * count) % MOD
                     res %= MOD
                     count = 1
                     couch = 0
@@ -23,6 +19,6 @@ class Solution:
                 if couch == 2:
                     count += 1
             
-                
+        
             
-        return res % MOD
+        return res % MOD if couch == 2 else 0 
