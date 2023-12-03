92b564dfcb6262dff15275c1409a8382f980d681
tannerr12 <tanner.01@hotmail.com>
2023-12-03 15:02:07 -0500

    Time: 35 ms (78.50%), Space: 16.2 MB (82.05%) - LeetHub

--- 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
+++ 1780-check-if-number-is-a-sum-of-powers-of-three/1780-check-if-number-is-a-sum-of-powers-of-three.py
@@ -1,26 +1,11 @@
 class Solution:
     def checkPowersOfThree(self, n: int) -> bool:
         
-        @lru_cache(maxsize=100)
-        def dfs(val, i):
+       
+
+        for i in range(16,-1,-1):
             
-            if val > n:
-                return False
-            elif val == n:
-                return True
-            elif i > 16:
-                return False
-            
-            res = False
-            #take 
-            res = res or dfs(val + 3 ** i, i+1)
-            #dont take
-            res = res or dfs(val, i+1)
-            
-            return res
+            if n >= 3 ** i:
+                n -= 3 ** i
         
-        return dfs(0, 0)
-        
-        
-        
-        
+        return n == 0
