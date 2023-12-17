e5a8a5a4e204cb66f5bf94b3c5625ac343f13598
tannerr12 <tanner.01@hotmail.com>
2023-12-17 12:53:52 -0500

    Time: 1112 ms (73.04%), Space: 61 MB (73.04%) - LeetHub

--- 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
+++ 1665-minimum-initial-energy-to-finish-tasks/1665-minimum-initial-energy-to-finish-tasks.py
@@ -3,37 +3,19 @@ class Solution:
         
         tasks = sorted(tasks, key=lambda x: (-(x[1] - x[0])))
               
-        #[[10, 12], [10, 11], [8, 9], [2, 4], [1, 3]]
-        #31        29    19       9      1
-        #[[1,3],[2,4],[10,11],[10,12],[8,9]]
-        #print(tasks)
-        def isGood(energy):
-            
-            for x,y in tasks:
-                if energy < y or energy < x:
-                    return False
-                energy -= x
-            
-            return True
-        
-        l, r = 0,0
+        energy = 0
+        energyNeeded = 0
         for x,y in tasks:
-            l +=x
-            r += y
-            
-            
-        res = 0
-        while l <= r:
-            
-            mid = (l+r) // 2
-            
-            if isGood(mid):
-                res = mid
-                r = mid-1
+            if y > energy:
+                diff = y - energy
+                energyNeeded += diff
+                energy += diff
                 
-            else:
-                l = mid+1
-                
-            
+            energy -= x
         
-        return res
+        return energyNeeded
+
+        
+
+        
+        
