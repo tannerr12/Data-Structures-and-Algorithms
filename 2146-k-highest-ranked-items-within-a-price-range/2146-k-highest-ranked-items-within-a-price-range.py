3b319ccb40e634694bbe36ff2e8ad63f81125878
tannerr12 <tanner.01@hotmail.com>
2023-12-30 15:29:56 -0500

    Time: 3771 ms (5.66%), Space: 51.2 MB (87.74%) - LeetHub

--- 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.py
+++ 2146-k-highest-ranked-items-within-a-price-range/2146-k-highest-ranked-items-within-a-price-range.py
@@ -4,26 +4,30 @@ class Solution:
         directions = [[-1,0], [1,0], [0,-1], [0, 1]]
         q = deque([start])
         heap = []
-        dist = 0
-        seen = set()
-        seen.add((start[0], start[1]))
+        dist = 1
+        s = grid[start[0]][start[1]]
+        if s >= pricing[0] and s <= pricing[1]:
+            heappush(heap, [0,s,start[0], start[1]])
+        
+        grid[start[0]][start[1]] = 0
+        
         while q:
             
             for i in range(len(q)):
                 
                 x,y = q.popleft()
-                if grid[x][y] >= pricing[0] and grid[x][y] <= pricing[1]:
-                    heappush(heap, [dist, grid[x][y], x, y])
-                    
-                
                 for a,b in directions:
                     newx,newy = x + a, y + b
                     
-                    if newx < 0 or newy < 0 or newx >= m or newy >= n or grid[newx][newy] == 0 or (newx,newy) in seen:
+                    if newx < 0 or newy < 0 or newx >= m or newy >= n or grid[newx][newy] == 0:
                         continue
                         
-                    seen.add((newx,newy))
+                    if grid[newx][newy] >= pricing[0] and grid[newx][newy] <= pricing[1]:
+                        heappush(heap, [dist, grid[newx][newy], newx, newy])                        
+                    
+                    grid[newx][newy] = 0
                     q.append([newx,newy])
+                    
             if len(heap) >= k:
                 break
             dist += 1
