bb9f7b96ee5090779247ead1751f23a42d7969ad
tannerr12 <tanner.01@hotmail.com>
2023-12-30 18:53:39 -0500

    Time: 1061 ms (94.39%), Space: 52.3 MB (18.22%) - LeetHub

--- 2906-construct-product-matrix/2906-construct-product-matrix.py
+++ 2906-construct-product-matrix/2906-construct-product-matrix.py
@@ -8,11 +8,11 @@ class Solution:
         suf.append(1)
         for i in range(len(grid)):
             for j in range(len(grid[0])):
-                pref.append((pref[-1] * (grid[i][j]) % MOD))
+                pref.append((pref[-1] * ((grid[i][j] % MOD)) % MOD))
         
         for i in range(len(grid)-1,-1,-1):
             for j in range(len(grid[0])-1,-1,-1):
-                suf.append((suf[-1] * grid[i][j]) % MOD)
+                suf.append((suf[-1] * (grid[i][j]% MOD)) % MOD)
         
         
         ans = [[0 for j in range(len(grid[0]))] for i in range(len(grid))]
