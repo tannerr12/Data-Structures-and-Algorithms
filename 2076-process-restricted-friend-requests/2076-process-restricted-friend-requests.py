570f129e26746d12831d7b7e6a90823f42eac144
tannerr12 <tanner.01@hotmail.com>
2023-12-31 14:55:56 -0500

    Time: 7670 ms (5.10%), Space: 18.1 MB (8.28%) - LeetHub

---
+++ 2076-process-restricted-friend-requests/2076-process-restricted-friend-requests.py
@@ -0,0 +1,56 @@
+class Solution:
+    def friendRequests(self, n: int, restrictions: List[List[int]], requests: List[List[int]]) -> List[bool]:
+        
+        parent = [i for i in range(n)]
+        rank = [0] * n
+        
+        def find(x):
+            
+            if x == parent[x]:
+                return parent[x]
+            
+            parent[x] = find(parent[x])
+            return parent[x]
+        
+        def union(x,y):
+            
+            px,py = find(x), find(y)
+            
+            if px == py:
+                return True
+            
+            if rank[px] > rank[py]:
+                parent[py] = px
+            elif rank[py] > rank[px]:
+                parent[px] = py
+            else:
+                rank[px] += 1
+                parent[py] = px
+            
+            
+        
+        def isConnected(x,y):
+            return find(x) == find(y)
+        
+        groups = defaultdict(set)
+        for x,y in restrictions:
+            groups[x].add(y)
+            groups[y].add(x)
+            
+        
+        ans = []
+        
+        for x,y in requests:
+            found = False
+            for a,b in restrictions:
+                if (isConnected(a,x) and isConnected(b,y)) or (isConnected(b,x) and isConnected(a,y)):
+                    found = True
+                    break
+
+            if not found:
+                union(x,y)
+                ans.append(True)
+            else:
+                ans.append(False)
+                
+        return ans
