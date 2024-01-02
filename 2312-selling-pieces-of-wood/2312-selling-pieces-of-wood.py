1bdd13dc8929eb0b0492c90ec021516f2e9f8ae0
tannerr12 <tanner.01@hotmail.com>
2024-01-02 16:04:06 -0500

    Time: 6910 ms (7.76%), Space: 45 MB (6.80%) - LeetHub

---
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.py
@@ -0,0 +1,48 @@
+class Solution:
+    def sellingWood(self, m: int, n: int, prices: List[List[int]]) -> int:
+        total = m * n
+        mp = defaultdict(int)
+        
+        for x,y,p in prices:
+            mp[(x,y)] = p
+        
+        @lru_cache(maxsize = 100000)
+        def dfs(h, w):
+            
+            res = mp[(h,w)]
+            
+            #all possible cuts
+            for i in range(1, h):
+                left = dfs(i, w)
+                right = dfs(h-i, w)
+                res = max(res, left + right)
+            for i in range(1, w):
+                left = dfs(h,i)
+                right = dfs(h,w-i)
+                res = max(res, left + right)
+                
+            return res
+            
+        
+        return dfs(m,n)
+            
+            
+            
+            
+        '''   
+        while best:
+            b = best.pop()
+            blocks = 0
+            blocks += m // b[1]
+            blocks *= n // b[2]
+
+            score += blocks * b[3]
+            
+            remh = m % b[1]
+            remw = n % b[2]
+            
+            #print(score)
+
+        #def dfs()
+        return score
+        '''
