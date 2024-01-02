531b09d1b8c0830e67a04dc7b8acaca6b3b01038
tannerr12 <tanner.01@hotmail.com>
2024-01-01 20:36:07 -0500

    Time: 74 ms (18.33%), Space: 17.3 MB (24.43%) - LeetHub

--- 0060-permutation-sequence/0060-permutation-sequence.py
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -6,12 +6,15 @@ class Solution:
         
         #how many permutations start with 1
         #9! would be 1/9 of the options is our answer greater than this?
-        
+        factors = []
+        factors.append(1)
+        for i in range(1, n+1):
+            factors.append(factors[-1] * i)
         ans = []
         has = set([i for i in range(1, n + 1)])
         pos = n
         while len(ans) != n:
-            total = factorial(pos)
+            total = factors[pos]
             i = 1
             for val in sorted(has,reverse=True):
                 if k > (total // pos) * (len(has) -i):
