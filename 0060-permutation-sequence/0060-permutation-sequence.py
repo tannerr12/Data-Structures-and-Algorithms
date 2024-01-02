fd717aa8e60bd465923a26e8290bf94299be9132
tannerr12 <tanner.01@hotmail.com>
2024-01-01 20:32:05 -0500

    Time: 35 ms (83.20%), Space: 17.2 MB (24.43%) - LeetHub

---
+++ 0060-permutation-sequence/0060-permutation-sequence.py
@@ -0,0 +1,29 @@
+class Solution:
+    def getPermutation(self, n: int, k: int) -> str:
+        
+        
+        
+        
+        #how many permutations start with 1
+        #9! would be 1/9 of the options is our answer greater than this?
+        
+        ans = []
+        total = factorial(n)
+        has = set([i for i in range(1, n + 1)])
+        pos = n
+        while len(ans) != n:
+            total = factorial(n - len(ans))
+            i = 1
+            for val in sorted(has,reverse=True):
+                if k > (total // pos) * (len(has) -i):
+                    k -= (total // pos) * (len(has) -i)
+                    ans.append(str(val))
+                    break
+                i += 1
+            has.remove(int(ans[-1]))
+            pos -= 1
+        
+        #print(ans)
+        
+        return ''.join(ans)
+            
