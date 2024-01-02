e21250aad9486e0b6c39ab76b464ab5cf26b6b90
tannerr12 <tanner.01@hotmail.com>
2024-01-02 15:00:28 -0500

    Time: 445 ms (88.33%), Space: 28.4 MB (55.83%) - LeetHub

--- 1537-get-the-maximum-score/1537-get-the-maximum-score.py
+++ 1537-get-the-maximum-score/1537-get-the-maximum-score.py
@@ -1,19 +1,23 @@
 class Solution:
     def maxSum(self, nums1: List[int], nums2: List[int]) -> int:
         MOD = 10 ** 9 + 7
-        mp = defaultdict(int)
         
         idx1 = 0
         idx2 = 0
         p1 = 0
         p2 = 0
+        
+        res = 0
         while idx1 < len(nums1) and idx2 < len(nums2):
             
             if nums1[idx1] == nums2[idx2]:
-                mp[nums1[idx1]] = max(p1,p2)
+                res += (max(p1,p2) + nums1[idx1]) % MOD
+                res %= MOD
                 idx1 += 1
                 idx2 += 1
                 p1,p2 = 0,0
+                
+                
             elif nums1[idx1] < nums2[idx2]:
                 p1 += nums1[idx1]
                 idx1 += 1
@@ -29,15 +33,9 @@ class Solution:
             p2 += nums2[idx2]
             idx2 += 1
         
-        end = [p1, p2]
-        res = 0
-        for key in sorted(mp):
-            v1 = mp[key]
-            res += v1 % MOD
-            res += key % MOD
-            res %= MOD
+
             
-        res += max(end[0], end[1])
+        res += max(p1,p2) % MOD
        
         return res % MOD
         
