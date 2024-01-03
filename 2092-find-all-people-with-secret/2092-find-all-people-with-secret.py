60f609db8167cf86969ebb47cad5f7f65398f9b7
tannerr12 <tanner.01@hotmail.com>
2024-01-03 13:20:25 -0500

    Time: 1602 ms (96.55%), Space: 70.5 MB (28.74%) - LeetHub

---
+++ 2092-find-all-people-with-secret/2092-find-all-people-with-secret.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findAllPeople(self, n: int, meetings: List[List[int]], firstPerson: int) -> List[int]:
+        
+        parent = defaultdict(int)
+        adj = defaultdict(lambda:defaultdict(list))
+        
+        
+        
+        for x,y,z in meetings:
+            adj[z][x].append(y)
+            adj[z][y].append(x)
+    
+        
+        people = set()
+        people.add(0)
+        people.add(firstPerson)
+        
+        for t in sorted(adj):
+            q = deque()
+            for val in adj[t]:
+                if val in people:
+                    q.append(val)
+            
+            while q:
+                
+                for i in range(len(q)):
+                    
+                    node = q.popleft()
+                    
+                    for val in adj[t][node]:
+                        
+                        if val in people:
+                            continue
+                        
+                        people.add(val)
+                        q.append(val)
+        
+        return list(people)
+                    
