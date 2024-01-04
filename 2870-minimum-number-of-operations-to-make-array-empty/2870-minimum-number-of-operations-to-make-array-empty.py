9a26b4de257cf2cdbbc2178c3782cb9ee7c00e6a
tannerr12 <tanner.01@hotmail.com>
2024-01-03 23:48:27 -0500

    Time: 528 ms (99.09%), Space: 31.6 MB (14.85%) - LeetHub

--- 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
+++ 2870-minimum-number-of-operations-to-make-array-empty/2870-minimum-number-of-operations-to-make-array-empty.py
@@ -4,16 +4,15 @@ class Solution:
         
         res = 0
         for key,val in mp.items():
+            
             if val == 1:
                 return -1
             elif val % 3 == 0:
                 res += val // 3
-            elif val % 3 == 2:
-                res += val // 3
-                res += 1
-            elif val % 3 == 1:
+            elif val % 3 == 2 or val % 3 == 1:
                 res += val // 3
                 res += 1
             else:
                 return -1
+            
         return res
