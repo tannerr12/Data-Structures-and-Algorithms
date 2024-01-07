d7575caa374f9a82f11a44c121e8d69b51fd4f68
tannerr12 <tanner.01@hotmail.com>
2024-01-06 19:49:04 -0500

    Time: 659 ms (58.59%), Space: 95.9 MB (5.05%) - LeetHub

---
+++ 0898-bitwise-ors-of-subarrays/0898-bitwise-ors-of-subarrays.py
@@ -0,0 +1,21 @@
+class Solution:
+    def subarrayBitwiseORs(self, A: List[int]) -> int:
+        
+        
+        #1, 4, 1, 3
+        
+        #1,3
+        
+        #2
+        
+        # Tabulation is a list of sets, one for each number in A. 
+        # Each set, at position i, is initialised to containing the element at A[i]
+        tabulation = [set([A[i]]) for i in range(len(A))]
+        
+        # And now we need to go through, updating the sets based on the previous set.
+        for i in range(1, len(A)):
+            for previous_result in tabulation[i - 1]: 
+                tabulation[i].add(A[i] | previous_result)  
+        
+        # Return the number of unique numbers in the tabulation list.
+        return len(set.union(*tabulation)) if len(A) > 0 else 0
