f300905d3143b97d9065d4cbbe634000dccf42df
tannerr12 <tanner.01@hotmail.com>
2024-01-12 11:54:33 -0500

    Time: 128 ms (97.37%), Space: 17.5 MB (15.79%) - LeetHub

--- 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.py
+++ 2151-maximum-good-people-based-on-statements/2151-maximum-good-people-based-on-statements.py
@@ -23,25 +23,26 @@ class Solution:
             #if someone is good all statements should be recorded
             bgood = good 
             bbad = bad
-            
+            #take all statements as fact
             bgood |= maskGood[i]
             bbad |= maskBad[i]
             
+            #check for conflicting statments
             canbegood = (good & maskBad[i]) == 0 and (bad & maskGood[i]) == 0
             
-            #already good
-            if good & (1 << i) > 0 and canbegood == False:
-                return float('-inf')
-            elif canbegood and bad & (1 << i) == 0:
+
+            #can be good and is not already bad
+            if canbegood and bad & (1 << i) == 0:
+                #take statments as fact
                 res = max(res, dfs(i+1, bgood | (1 << i), bbad) + 1)
             
+            #can be bad and is not already good
             if good & (1 << i) == 0:
+                #no statements updated
                 res = max(res, dfs(i+1, good, bad | (1 << i)))
         
                       
             return res
             
-            
-        
                       
         return dfs(0, 0, 0)
