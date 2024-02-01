736a2e15a43404edfec083262c2b0b5f1fd56063
tannerr12 <tanner.01@hotmail.com>
2024-02-01 18:07:55 -0500

    Time: 872 ms (77.72%), Space: 39 MB (32.13%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.py
@@ -0,0 +1,44 @@
+class Solution:
+    def findFarmland(self, land: List[List[int]]) -> List[List[int]]:
+        
+        
+        def check(i,j):
+            
+            if i < 0 or j < 0 or land[i][j] == 0:
+                return False
+            return True
+        
+        def getVal(i,j):
+            
+            if i < 0 or j < 0:
+                return 0
+            
+            return land[i][j]
+        
+        groups = defaultdict(list)
+        gcount = 1
+        for i in range(len(land)):
+            for j in range(len(land[0])):
+                
+                if land[i][j] == 0:
+                    continue
+                up = check(i-1,j)
+                left = check(i,j-1)
+                
+                #start block
+                if not up and not left:
+                    groups[gcount] = [i,j,i,j]
+                    land[i][j] = gcount
+                    gcount += 1
+                    
+                elif up or left:
+                    land[i][j] = max(getVal(i-1,j), getVal(i,j-1))
+                    groups[land[i][j]][2] = max(groups[land[i][j]][2],i)
+                    groups[land[i][j]][3] = max(groups[land[i][j]][3],j)
+        
+        #print(groups)
+        return groups.values()
+                    
+                
+                
+                
