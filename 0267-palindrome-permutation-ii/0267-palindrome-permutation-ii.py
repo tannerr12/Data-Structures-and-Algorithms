c5dea6e33c39d135a83109643dd2df4439dedf21
tannerr12 <tanner.01@hotmail.com>
2024-02-17 12:03:52 -0500

    Time: 125 ms (62.09%), Space: 24.3 MB (90.85%) - LeetHub

--- 0267-palindrome-permutation-ii/0267-palindrome-permutation-ii.py
+++ 0267-palindrome-permutation-ii/0267-palindrome-permutation-ii.py
@@ -2,7 +2,17 @@ class Solution:
     def generatePalindromes(self, s: str) -> List[str]:
         ans = []
         c = Counter(s)
+        odd = 0
+        for key,val in c.items():
+            if len(s) % 2 == 0:
+                if val % 2:
+                    return []
+            else:
+                if val % 2:
+                    odd += 1
         
+        if odd > 1:
+            return []
         def dfs(word):
             
             if len(word) == len(s):
