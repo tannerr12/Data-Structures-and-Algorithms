923f1086279f04d5f68ca15a86c1ea36cd5906b6
tannerr12 <tanner.01@hotmail.com>
2024-02-18 14:44:01 -0500

    Time: 1183 ms (72.70%), Space: 62.5 MB (73.33%) - LeetHub

--- 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
+++ 2402-meeting-rooms-iii/2402-meeting-rooms-iii.py
@@ -1,61 +1,24 @@
 class Solution:
     def mostBooked(self, n: int, meetings: List[List[int]]) -> int:
         
-        meetings.sort()
-        
-        #heap for meeting rooms time, meeting room number
-        #going through each meeting we can try to allocate a meeting to a room
-        #or if we cannot we can push the meeting onto a waiting heap which would cover [starttime, endtime/duration]
-        score = defaultdict(int)
-        mheap = []
-        aheap = [i for i in range(n)]
-        wdeq = deque()
-        
-        
-        index = 0
-        
-        for i in range(10**6):
-            while mheap and mheap[0][0] <= i:
-                val, idx = heappop(mheap)
-                heappush(aheap, idx)
-                
-            #handle all currently waiting rooms
-            while wdeq and aheap:
-                idx = heappop(aheap)
-                s,e = wdeq.popleft()
-                score[idx] += 1
-                heappush(mheap, (i + (e - s), idx))
-                
-            
-            if index < len(meetings) and aheap and meetings[index][0] <= i:
-                idx = heappop(aheap)
-                score[idx] += 1
-                heappush(mheap, (meetings[index][1], idx))
-                index += 1
-
-            elif index < len(meetings) and meetings[index][0] <= i:
-                wdeq.append((meetings[index][0], meetings[index][1]))
-                index += 1
-    
-        
-        
-        #print(score)
-        while wdeq:
-            val,idx = heappop(mheap)
-            s,e = wdeq.popleft()
-            score[idx] += 1
-            heappush(mheap, (val + (e - s), idx)) 
-        
-        
-        mx = max(score.values())
-        mn = float('inf')
-        
-        for x,y in score.items():
-            if y == mx:
-                mn = min(mn, x)
-                
-        
-        return mn
+        unused_rooms, used_rooms = list(range(n)), []
+        heapify(unused_rooms)
+        meeting_count = [0] * n
+        for start, end in sorted(meetings):
+            while used_rooms and used_rooms[0][0] <= start:
+                _, room = heappop(used_rooms)
+                heappush(unused_rooms, room)
+            if unused_rooms:
+                room = heappop(unused_rooms)
+                heappush(used_rooms, [end, room])
+            else:
+                room_availability_time, room = heappop(used_rooms)
+                heappush(
+                    used_rooms,
+                    [room_availability_time + end - start, room]
+                )
+            meeting_count[room] += 1
+        return meeting_count.index(max(meeting_count))
 
                 
                 
