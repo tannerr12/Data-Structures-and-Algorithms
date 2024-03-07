069408a47f40fe24b01fae71547410e1094b4e2d
tannerr12 <tanner.01@hotmail.com>
2024-03-06 20:22:11 -0500

    Time: 2526 ms (18.34%), Space: 383 MB (5.22%) - LeetHub

---
+++ 0818-race-car/0818-race-car.py
@@ -0,0 +1,36 @@
+class Solution:
+    def racecar(self, target: int) -> int:
+        
+        
+        level = 0
+        q = deque([[0,1]])
+        seen = set()
+        seen.add((0,1))
+        while q:
+            
+            for i in range(len(q)):
+                
+                pos,speed = q.popleft()
+                flipSpeed = -1 if speed > 0 else 1
+                
+                if pos == target:
+                    return level
+                
+                if (pos + speed, speed * 2) not in seen:
+                    seen.add((pos + speed, speed * 2))
+                    q.append((pos + speed, speed * 2))
+                if (pos, flipSpeed) not in seen:
+                    seen.add((pos,flipSpeed))
+                    q.append((pos, flipSpeed))
+            
+            
+            level += 1
+        
+        
+        return -1
+        
+        
+                
+                
+                
+                
