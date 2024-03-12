ada13efbaff24c39d834a1f9e7eae919625570e5
tannerr12 <tanner.01@hotmail.com>
2024-03-12 17:56:02 -0400

    Time: 36 ms (43.97%), Space: 16.6 MB (21.00%) - LeetHub

--- 1328-break-a-palindrome/1328-break-a-palindrome.py
+++ 1328-break-a-palindrome/1328-break-a-palindrome.py
@@ -1,19 +1,21 @@
 class Solution:
     def breakPalindrome(self, pal: str) -> str:
         
-        pos = -1
+        if len(pal) == 1:
+            return ''
+        
+        pos = 0
         
         for i in range(len(pal)):
             
             #dont flip the middle character
             if i == len(pal) // 2 and len(pal) % 2:
                 continue
-            if pal[i] == 'a':
-                pos = i
-            else:
+                
+            if pal[i] != 'a':
                 new = pal[:i] + 'a' + pal[i+1:]
                 return new
         
         
-        return pal[:pos] + 'b' + pal[pos+1:] if pos >= 0 else '' 
+        return pal[:-1] + 'b' if pos >= 0 else '' 
             
