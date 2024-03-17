2efdda2b6d1f2e31e40a546479d1a667ed259a3f
tannerr12 <tanner.01@hotmail.com>
2024-03-17 13:21:29 -0400

    Time: 1978 ms (9.90%), Space: 40.6 MB (9.90%) - LeetHub

---
+++ 1606-find-servers-that-handled-most-number-of-requests/1606-find-servers-that-handled-most-number-of-requests.py
@@ -0,0 +1,59 @@
+from sortedcontainers import SortedList
+class Solution:
+    def busiestServers(self, k: int, arrival: List[int], load: List[int]) -> List[int]:
+        
+        #keep track of the most used server
+        score = defaultdict(int)
+        
+        #keep track of busy servers
+        heap = []
+        
+        #maintain the status of the servers
+        busy = SortedList([i for i in range(k)])
+        
+        for i in range(len(arrival)):
+            
+            at  = arrival[i]
+            ld = load[i]
+            
+            #if the servers are done set them back to available
+            while heap and heap[0][0] <= at:
+                _,s = heappop(heap)
+                busy.add(s)        
+            
+            if len(busy) == 0:
+                continue
+            
+            find = i % k
+            idxr = bisect_left(busy, find)
+            
+            #the server requested is available
+            if idxr < len(busy) and busy[idxr] == find:
+                busy.pop(idxr)
+                heappush(heap, [at + ld, i % k])
+                score[i % k] += 1
+            
+            elif idxr < len(busy):
+                heappush(heap, [at + ld, busy[idxr]])  
+                score[busy[idxr]] += 1
+                busy.pop(idxr)
+            
+            else:
+                heappush(heap, [at + ld, busy[0]])  
+                score[busy[0]] += 1
+                busy.pop(0)
+              
+            
+        
+        
+        #print(score)
+        
+        ans = []
+        mx = max(score.values())
+        for key,val in score.items():
+            if val == mx:
+                ans.append(key)
+                
+        
+        return ans
+        
