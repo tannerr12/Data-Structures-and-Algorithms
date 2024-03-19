5a35f9cc2cf612f22f212a80848e30a3565a5f0e
tannerr12 <tanner.01@hotmail.com>
2024-03-19 18:47:31 -0400

    Time: 875 ms (79.83%), Space: 210.2 MB (73.95%) - LeetHub

---
+++ 3003-maximize-the-number-of-partitions-after-operations/3003-maximize-the-number-of-partitions-after-operations.py
@@ -0,0 +1,41 @@
+class Solution:
+    def maxPartitionsAfterOperations(self, s: str, k: int) -> int:
+        
+        #k = 26 means the answer is always 1
+        #the initial partitions are fixed
+        
+        c = Counter(s)
+        if k > len(c) or k == 26:
+            return 1
+
+        
+        #10,000 * 2 * 26
+        @cache
+        def dfs(i,used, mask):
+            
+            if i == len(s):
+                return 1
+            
+            total = mask.bit_count()
+            cur = ord(s[i]) - ord('a')
+            res = 0
+            if not used:
+                for j in range(26):
+                    #ch = chr(ord('a') + j)
+                    if mask & (1 << j) == 0 and j != cur:
+                        if total == k:
+                            res = max(res, dfs(i+1, 1, 0 | (1 << j)) + 1)
+                        else:
+                            res = max(res, dfs(i+1, 1, mask | (1 << j)))
+            
+            
+
+            if total == k and mask & (1 << cur) == 0:
+                res = max(res, dfs(i+1, used, 0 | (1 << cur)) + 1)
+            else:
+                res = max(res, dfs(i+1, used, mask | (1 << cur)))    
+            
+            return res
+        
+        
+        return dfs(0,0,0)
