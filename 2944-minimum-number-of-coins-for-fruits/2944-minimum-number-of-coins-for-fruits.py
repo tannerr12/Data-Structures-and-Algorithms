dadf808f8a2826885760f6fbebd6caf846c1e563
tannerr12 <tanner.01@hotmail.com>
2024-03-19 20:49:16 -0400

    Time: 2271 ms (5.27%), Space: 473.4 MB (5.26%) - LeetHub

---
+++ 2944-minimum-number-of-coins-for-fruits/2944-minimum-number-of-coins-for-fruits.py
@@ -0,0 +1,23 @@
+class Solution:
+    def minimumCoins(self, prices: List[int]) -> int:
+        
+        @cache
+        def dfs(i,free):
+            
+            if i == len(prices):
+                return 0
+            
+            res = float('inf')
+            
+            #take 
+            res = min(res, dfs(i+1, i+1) + prices[i])
+            
+            #skip
+            if free > 0:
+                res = min(res, dfs(i+1, max(0, free - 1)))
+                
+            
+            return res
+        
+        
+        return dfs(0,0)
