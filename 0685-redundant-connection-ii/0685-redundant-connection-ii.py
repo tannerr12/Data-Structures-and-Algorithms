77a08b6d327c976f97e42e92db5c7a1606ce05a7
tannerr12 <tanner.01@hotmail.com>
2024-03-28 20:44:53 -0400

    Time: 48 ms (98.89%), Space: 17.1 MB (52.37%) - LeetHub

--- 0685-redundant-connection-ii/0685-redundant-connection-ii.py
+++ 0685-redundant-connection-ii/0685-redundant-connection-ii.py
@@ -1,60 +1,72 @@
 class Solution:
     def findRedundantDirectedConnection(self, edges: List[List[int]]) -> List[int]:
         
-        nodes = len(edges)
         
-        graph = defaultdict(set)
-        parent = set([i for i in range(1, len(edges) + 1)])
+        rank = [1] * (len(edges)+1)
+        par = [i for i in range(len(edges) +1)]
+
+        
+        def find(p):
+            
+            if par[p] == p:
+                return p
+            
+            par[p] = find(par[p])
+            
+            return par[p]
+        
+        
+        def union(n1,n2):
+            
+            p1,p2 = find(n1), find(n2)
+            
+            if p1 == p2:
+                return True
+            
+            if rank[p1] > rank[p2]:
+                par[p2] = p1
+                
+       
+            elif rank[p2] > rank[p1]:
+                par[p1] = p2
+            
+            else:
+                par[p2] = p1
+                rank[p1] += 1
+            
+
+            return False
+        
+        
         count = defaultdict(int)
+        parent = defaultdict(int)
+        bad = None
         for x,y in edges:
-            graph[x].add(y)
-            count[y] += 1
-            if y in parent:
-                parent.remove(y)
-        
-        par = list(parent)
-        
-        
-        def scan(par):
-            nonlocal nodes
-            q = deque([par])
-            seen = set()
-            seen.add(par)
-            while q:
+            count[y] +=1
+            parent[x] +=1
+            if count[y] == 2:
+                bad = y
                 
-                node = q.popleft()
-                
-                for val in graph[node]:
-                    
-                    if val in seen:
-                        return False
-                    
-                    seen.add(val)
-                    q.append(val)
+        
+        
+        if not bad:    
+            for x,y in edges:
+                if union(x,y):
+                    return [x,y]
             
-            return len(seen) == nodes
-                
+        connections = set()
+        for x,y in edges:
+            if x == bad:
+                connections.add((y,x))
+        for x,y in edges:
             
-        
-        for i in range(len(edges)-1,-1,-1):
-            x,y = edges[i]
-            #remove the edge
-            graph[x].remove(y)
-            count[y] -= 1
-            if (count[y] != 0 and len(par) > 0) or (len(par) == 0 and count[y] == 0):
-                #scan and make sure we dont hit a cycle and hit all nodes
-                if len(par) > 0:
-                    if scan(par[0]):
-                        return edges[i]
-                else:
-                    if scan(y):
-                        return edges[i]
-                    
-            #add it back
-            graph[x].add(y)
-            count[y] += 1
-        
-        
-        
+            if y == bad:
+                if (x,y) in connections:
+                    return [x,y]
+                
+                if parent[x] > 1 or count[x] > 0:
+                    ans = [x,y]
+    
+        return ans
         
         
