4179ec9ec166d26d42ac59885680402554a83b40
tannerr12 <tanner.01@hotmail.com>
2024-04-01 16:22:48 -0400

    Time: 3893 ms (9.87%), Space: 387.1 MB (23.46%) - LeetHub

--- 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.py
+++ 2920-maximum-points-after-collecting-coins-from-all-nodes/2920-maximum-points-after-collecting-coins-from-all-nodes.py
@@ -2,43 +2,30 @@ class Solution:
     def maximumPoints(self, edges: List[List[int]], coins: List[int], k: int) -> int:
         
         adj = defaultdict(list)
-        parents = defaultdict(int)
+   
         for x,y in edges:
             adj[x].append(y)
             adj[y].append(x)
         
         
-        def dfs2(node,par):
-            
-            parents[node] = par
-            for edge in adj[node]:
-                if edge != par:
-                    dfs2(edge,node)
-        
-        dfs2(0,-1)
-        #print(parents)
-        
         @cache
-        def dfs(node,mult):
-            
-            #if len(adj[node]) == 1 and adj[node][0] == par:
-            #    return 0
+        def dfs(node,par,mult):
             
             res = 0
             if mult >= 14:
                 return res
             
             for edge in adj[node]:
-                if edge == parents[node]:
+                if edge == par:
                     continue
                 
                 cur = float('-inf')
                 #Option 1
-                cur = max(cur, dfs(edge, mult) + (coins[edge] >> (mult)) - k)
+                cur = max(cur,dfs(edge,node, mult) + (coins[edge] >> (mult)) - k)
                 
                 #Option 2
                 if mult < 14:
-                    cur = max(cur, dfs(edge, mult + 1) + (coins[edge] >> ((mult + 1))))
+                    cur = max(cur,dfs(edge,node, mult + 1) + (coins[edge] >> ((mult + 1))))
                 
                 res += cur
             
@@ -47,10 +34,10 @@ class Solution:
         
         res = float('-inf')
         #Option 1
-        res = max(res, dfs(0, 0) + coins[0] - k)
+        res = max(res, dfs(0,-1, 0) + coins[0] - k)
 
         #Option 2
-        res = max(res, dfs(0, 1) + (coins[0] >> (0 + 1)))
+        res = max(res, dfs(0,-1, 1) + (coins[0] >> (0 + 1)))
 
         
 
