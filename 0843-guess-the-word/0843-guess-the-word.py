52e983fa7e1492696e2dffba4a11777963425ef1
tannerr12 <tanner.01@hotmail.com>
2024-04-09 21:29:44 -0400

    Time: 33 ms (87.30%), Space: 16.6 MB (48.32%) - LeetHub

---
+++ 0843-guess-the-word/0843-guess-the-word.py
@@ -0,0 +1,48 @@
+# """
+# This is Master's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+# class Master:
+#     def guess(self, word: str) -> int:
+
+class Trie:
+    def __init__(self,val):
+        self.val = val
+        self.adj = {}
+    
+class Solution:
+    def findSecretWord(self, wordlist: List[str], master: 'Master') -> None:	
+        def pair_matches(a, b):         # count the number of matching characters
+            return sum(c1 == c2 for c1, c2 in zip(a, b))
+
+        def most_overlap_word():
+            counts = [[0 for _ in range(26)] for _ in range(6)]     # counts[i][j] is nb of words with char j at index i
+            for word in candidates:
+                for i, c in enumerate(word):
+                    counts[i][ord(c) - ord("a")] += 1
+
+            best_score = 0
+            for word in candidates:
+                score = 0
+                for i, c in enumerate(word):
+                    score += counts[i][ord(c) - ord("a")]           # all words with same chars in same positions
+                if score > best_score:
+                    best_score = score
+                    best_word = word
+
+            return best_word
+
+        candidates = wordlist[:]        # all remaining candidates, initially all words
+        while candidates:
+
+            s = most_overlap_word()     # guess the word that overlaps with most others
+            matches = master.guess(s)
+
+            if matches == 6:
+                return
+
+            candidates = [w for w in candidates if pair_matches(s, w) == matches]   # filter words with same matches
+                
+        
+        
+        
