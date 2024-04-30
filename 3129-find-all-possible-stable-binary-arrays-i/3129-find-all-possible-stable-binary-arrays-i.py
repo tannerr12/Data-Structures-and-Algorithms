07f901746cbd04e6ea40408eda0a073720e27eee
tannerr12 <tanner.01@hotmail.com>
2024-04-30 18:11:22 -0400

    Time: 3445 ms (69.83%), Space: 92.4 MB (36.29%) - LeetHub

---
+++ 3129-find-all-possible-stable-binary-arrays-i/3129-find-all-possible-stable-binary-arrays-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def numberOfStableArrays(self, zero: int, one: int, limit: int) -> int:
+        MOD = 10 ** 9 + 7
+        
+        @cache
+        def dfs(z,o,turn):
+            
+            if z == 0 and o == 0:
+                return 1
+            
+            res = 0
+            if turn:
+                for i in range(1, min(limit, z) + 1):
+                    res += dfs(z - i, o, not turn)
+            else:
+                for i in range(1, min(limit, o) + 1):
+                    res += dfs(z, o - i, not turn)
+            
+            return res % MOD
+        
+        
+        return (dfs(zero, one, True) + dfs(zero, one, False)) % MOD
